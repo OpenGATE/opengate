14c4942afbb3df82900ad6308921f6eefb9b0c59
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2020-10-16 17:46:30 +0200

    voxels

--- gam_lib/GamImageNestedParameterisation.h
+++ gam_lib/GamImageNestedParameterisation.h
@@ -18,8 +18,11 @@ class GamImageNestedParameterisation : public G4VNestedParameterisation {
 
 public:
 
+    // Slice position in mm
     std::vector<G4double> fpZ;
+    // List of pixel value <-> material
     std::vector<G4Material *> fMaterials;
+    // The image, float for the moment. Short later.
     typedef itk::Image<float, 3> ImageType;
     ImageType::Pointer cpp_image;
 
@@ -33,25 +36,19 @@ public:
         // the following must be int, not auto (will be negative)
         int size_z = cpp_image->GetLargestPossibleRegion().GetSize()[2];
         auto spacing_z = cpp_image->GetSpacing()[2];
-        std::cout << "init image " << size_z << " " << spacing_z << std::endl;
         double hs = spacing_z / 2.0; // half pixel along Z dimension
         for (int iz = 0; iz < size_z; iz++) {
             auto zp = (-size_z + 1 + 2 * iz) * hs;
             fpZ.push_back(zp * CLHEP::mm);
-            std::cout << "iz " << iz << " " << zp * CLHEP::mm << std::endl;
         }
-        std::cout << "img " << cpp_image->GetLargestPossibleRegion().GetSize() << std::endl;
-        std::cout << "img " << cpp_image->GetSpacing() << std::endl;
     }
 
     void initialize_material(std::vector<std::string> materials) {
         fMaterials.resize(0);
         for (auto s:materials) {
-            std::cout << "mat " << s << " " << std::endl;
-            //auto m = G4NistManager::Instance()->FindOrBuildMaterial(s);
             auto m = G4Material::GetMaterial(s);
             if (m == nullptr) {
-                std::cout << "Cannot find the material " << s << std::endl;
+                std::cout << "GamImageNestedParameterisation: cannot find the material " << s << std::endl;
                 exit(0);
             }
             fMaterials.push_back(m);
@@ -61,51 +58,37 @@ public:
     virtual G4Material *ComputeMaterial(G4VPhysicalVolume * /*currentVol*/,
                                         const G4int repNo,
                                         const G4VTouchable *parentTouch = nullptr) {
-        //std::cout << "I am in ComputeMaterial " << std::endl;
-        //std::cout << currentVol << std::endl;
-        //std::cout << repNo << std::endl;
-        //std::cout << parentTouch << std::endl;
-
         if (parentTouch == nullptr) {
-            std::cout << "Compute Material without parentTouch " << repNo << std::endl;
             return G4NistManager::Instance()->FindOrBuildMaterial("G4_AIR");
             // FIXME AIR ?? really ?
         }
 
+        // Get voxel index
         G4int ix = parentTouch->GetReplicaNumber(0);
         G4int iy = parentTouch->GetReplicaNumber(1);
         G4int iz = repNo;
-        //std::cout << "Index " << ix << " " << iy << " " << iz << std::endl;
-
         ImageType::IndexType index;
         index[0] = ix;
         index[1] = iy;
         index[2] = iz;
+
+        // Check if inside
         if (cpp_image->GetLargestPossibleRegion().IsInside(index)) {
             int i = int(cpp_image->GetPixel(index));
-            //std::cout << "i = " << i << std::endl;
             auto mat = fMaterials[i];
-            //std::cout << "m " << i << " " << mat->GetName() << " " << mat->GetDensity() << std::endl;
             return mat;
         } else {
-            //std::cout << "outside " << index << std::endl;
+            // Outside the image. Should almost never be here (except rounding issue)
             auto m = G4NistManager::Instance()->FindOrBuildMaterial("G4_AIR");
             return m;
         }
-
-        //auto water = G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER");
-        //return water;
     }
 
     virtual G4int GetNumberOfMaterials() const {
-        std::cout << "GetNumberOfMaterials " << fMaterials.size() << std::endl;
         return fMaterials.size();
     }
 
     virtual G4Material *GetMaterial(G4int idx) const {
-        std::cout << "GetMaterial " << idx << " " << fMaterials[idx]->GetName() << std::endl;
-        //auto water = G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER");
-        //return water;
         return fMaterials[idx];
     }
 
