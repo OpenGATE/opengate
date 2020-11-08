a329001fde870429ab978112927027899f91b864
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2020-11-08 18:37:57 +0100

    sps source

--- gam_lib/GamVActor.cpp
+++ gam_lib/GamVActor.cpp
@@ -67,6 +67,8 @@ void GamVActor::ProcessHitsPerBatch(bool force) {
 }
 
 void GamVActor::EndOfRunAction(const G4Run * /*run*/) {
+    //std::cout << "EndOfRunAction , processHistperBatch "
+    //<< batch_step_count << " " << batch_size << std::endl;
     // Needed to process the remaining information in current non-terminated batch
     ProcessHitsPerBatch(true);
 }
