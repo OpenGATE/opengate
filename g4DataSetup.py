e89df7ea74947a90b3510924932cdc9c9b133bf5
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2021-05-19 16:49:04 +0200

    Replace Azure pipeline by Github actions
    Install qt5 On Mac with conda
    Update Geant4 version to 10.7.0 and docker version
    Rename bash file to compile gam_g4 in docker
    Use c++17
    Remove 3.5 python version, not supported anymore by manylinux

--- g4DataSetup.py
+++ g4DataSetup.py
@@ -6,17 +6,18 @@ import sys
 
 # Data for Geant4
 data_packages = [
-    "https://cern.ch/geant4-data/datasets/G4NDL.4.6.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4EMLOW.7.9.1.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4PhotonEvaporation.5.5.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4RadioactiveDecay.5.4.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4SAIDDATA.2.0.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4PARTICLEXS.2.1.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4ABLA.3.1.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4INCL.1.0.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4PII.1.3.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4ENSDFSTATE.2.2.tar.gz",
-    "https://cern.ch/geant4-data/datasets/G4RealSurface.2.1.1.tar.gz"]
+     "https://cern.ch/geant4-data/datasets/G4NDL.4.6.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4EMLOW.7.13.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4PhotonEvaporation.5.7.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4RadioactiveDecay.5.6.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4PARTICLEXS.3.1.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4PII.1.3.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4RealSurface.2.2.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4SAIDDATA.2.0.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4ABLA.3.1.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4INCL.1.0.tar.gz",
+     "https://cern.ch/geant4-data/datasets/G4ENSDFSTATE.2.3.tar.gz"
+     ]
 
 
 # Check and download Geant4 data if not present:
