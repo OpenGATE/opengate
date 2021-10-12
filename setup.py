1c138b265f4b7ee9364d39bd271e96082b674427
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2021-10-12 11:41:32 +0200

    Change gam name for gam-gate for pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gam",
+    name="gam-gate",
     version="0.2",
     author="Opengate collaboration",
     author_email="david.sarrut@creatis.insa-lyon.fr",
