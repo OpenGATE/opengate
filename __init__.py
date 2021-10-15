b1e5e71bc607346e81568c13883f612cc59018a6
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2021-10-15 08:41:47 +0200

    Add library path for gam-g4 during import
    Only for linux systems becaus it already worked on mac without this
    set LD_LIBRARY_PATH and LD_PRELOAD to the correct values and restart
    python

--- __init__.py
+++ __init__.py
@@ -1,3 +1,39 @@
+
+import site
+import os
+import sys
+
+def get_site_packages_dir():
+    site_package = [p for p  in site.getsitepackages()
+                    if "site-packages" in p][0]
+    return(site_package)
+
+def get_libG4_path(lib):
+    for element in os.listdir(os.path.join(get_site_packages_dir(), "gam_g4.libs")):
+        if "libG4" + lib in element:
+            return(os.path.join(get_site_packages_dir(), "gam_g4.libs", element))
+
+if sys.platform == "linux" or sys.platform == "linux2":
+    reloadPython = False
+    if 'LD_LIBRARY_PATH' not in os.environ:
+        os.environ['LD_LIBRARY_PATH'] = ""
+    if os.path.join(get_site_packages_dir(), "gam_g4.libs") not in os.environ['LD_LIBRARY_PATH']:
+        os.environ['LD_LIBRARY_PATH'] = os.path.join(get_site_packages_dir(), "gam_g4.libs") + ":" + os.environ['LD_LIBRARY_PATH']
+        reloadPython = True
+
+    if 'LD_PRELOAD' not in os.environ:
+        os.environ['LD_PRELOAD'] = ""
+    if get_libG4_path("processes") + ":" + get_libG4_path("geometry") not in os.environ['LD_PRELOAD']:
+        os.environ['LD_PRELOAD'] = get_libG4_path("processes") + ":" + get_libG4_path("geometry") + ":" + os.environ['LD_PRELOAD']
+        reloadPython = True
+
+    if reloadPython:
+        try:
+            os.execv(sys.argv[0], sys.argv)
+        except Exception:
+            print('Failed re-exec:')
+            sys.exit(1)
+
 from .gam_g4 import *
 from .g4DataSetup import *
 from .qt5Setup import *
