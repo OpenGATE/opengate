31b3d8cc5c143ce20b6f461466ee2c9e0dd27d81
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2021-10-19 15:17:57 +0200

    Debug library path in init.py
    For developper mode, before this commit, it failed because the path is not in site-package.
    Now, if developper mode (ie. pip install -e .), it does not look if the path are correct
    
    For LD_PRELOAD, check if the 2 librairies are loaded but now it does not care about the which library is loaded first in the path

--- __init__.py
+++ __init__.py
@@ -13,19 +13,22 @@ def get_libG4_path(lib):
         if "libG4" + lib in element:
             return(os.path.join(get_site_packages_dir(), "gam_g4.libs", element))
 
+pathCurrentFile = os.path.abspath(__file__)
 if sys.platform == "linux" or sys.platform == "linux2":
-    reloadPython = False
-    if 'LD_LIBRARY_PATH' not in os.environ or os.path.join(get_site_packages_dir(), "gam_g4.libs") not in os.environ['LD_LIBRARY_PATH']:
-        reloadPython = True
+    if "site-packages" in pathCurrentFile: #gam-g4 is installed using wheel (for "pip install -e .", the paths are different)
+        reloadPython = False
+        if 'LD_LIBRARY_PATH' not in os.environ or os.path.join(get_site_packages_dir(), "gam_g4.libs") not in os.environ['LD_LIBRARY_PATH']:
+            reloadPython = True
 
-    if 'LD_PRELOAD' not in os.environ or get_libG4_path("processes") + ":" + get_libG4_path("geometry") not in os.environ['LD_PRELOAD']:
-        reloadPython = True
+        if 'LD_PRELOAD' not in os.environ or get_libG4_path("processes") not in os.environ['LD_PRELOAD'] or get_libG4_path("geometry") not in os.environ['LD_PRELOAD']:
+            reloadPython = True
 
-    if reloadPython:
-        print("gam-g4 is not detected. Be sure to execute these lines before to run python:")
-        print("export LD_LIBRARY_PATH=" + os.path.join(get_site_packages_dir(), "gam_g4.libs") + ":${LD_LIBRARY_PATH}")
-        print("export LD_PRELOAD=" + get_libG4_path("processes") + ":" + get_libG4_path("geometry") + ":${LD_PRELOAD}")
-        sys.exit(-1)
+        if reloadPython:
+            print("gam-g4 is not detected. Be sure to execute these lines before to run python:")
+            print("export LD_LIBRARY_PATH=" + os.path.join(get_site_packages_dir(), "gam_g4.libs") + ":${LD_LIBRARY_PATH}")
+            print("export LD_PRELOAD=" + get_libG4_path("processes") + ":" + get_libG4_path("geometry") + ":${LD_PRELOAD}")
+            sys.exit(-1)
+
 
 from .gam_g4 import *
 from .g4DataSetup import *
