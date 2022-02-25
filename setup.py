f5b977a113dfccebc2465a0b3391b48be7a65f3e
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2022-02-25 12:48:05 +0100

    Towards version 0.2.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="gam-gate",
-    version="0.2.3",
+    version="0.2.4",
     author="Opengate collaboration",
     author_email="david.sarrut@creatis.insa-lyon.fr",
     description="Simulation for Medical Physics",
