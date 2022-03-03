72f33903b5135f1372b68156cb1eb2630cf5ccf2
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2022-03-03 09:38:08 +0100

    Move gam_g4 inside a new directory called gam_g4 We need to do two wheels for gam_gate and gam_g4, so for gam_g4 I need to respect the tree of a python module

--- .github/workflows/createWheelLinux.sh
+++ .github/workflows/createWheelLinux.sh
@@ -8,9 +8,9 @@ cd /home/gam_g4/
 export PATH=/software/cmake/cmake-3.18.2-Linux-x86_64/bin/:${PATH}
 source /software/geant4/bin/geant4make.sh
 export CMAKE_PREFIX_PATH=/software/geant4/bin:/software/itk/bin/:${CMAKE_PREFIX_PATH}
-mkdir plugins
-cp -r /lib64/qt5/plugins/platforms plugins
-cp -r /lib64/qt5/plugins/imageformats plugins
+mkdir gam_g4/plugins
+cp -r /lib64/qt5/plugins/platforms gam_g4/plugins
+cp -r /lib64/qt5/plugins/imageformats gam_g4/plugins
 /opt/python/${PYTHONFOLDER}/bin/pip install wget colored
 /opt/python/${PYTHONFOLDER}/bin/python setup.py sdist bdist_wheel
 auditwheel repair /home/gam_g4/dist/*.whl -w /software/wheelhouse/ --plat "manylinux2014_x86_64"
