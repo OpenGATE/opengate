5973078b2718f82518cbcc4365873d08f08d7067
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2022-11-03 16:22:57 +0100

    warning

--- core/opengate_core/opengate_lib/GateHitsAdderActor.cpp
+++ core/opengate_core/opengate_lib/GateHitsAdderActor.cpp
@@ -106,6 +106,10 @@ void GateHitsAdderActor::InitializeComputation() {
   l.fInputIter = fInputHitsCollection->NewIterator();
   l.fInputIter.TrackAttribute("TotalEnergyDeposit", &l.edep);
   l.fInputIter.TrackAttribute("PostPosition", &l.pos);
+  // Should probably be PreStep instead of PostStep here
+  // However, it was Pre in Gate legacy, and does not change because when a step
+  // terminate at the end of a volume, this is 'transportation' with edep==0, so
+  // ignored
   l.fInputIter.TrackAttribute("PostStepUniqueVolumeID", &l.volID);
   l.fInputIter.TrackAttribute("GlobalTime", &l.time);
 }
