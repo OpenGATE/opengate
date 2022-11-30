ed0720ec26f4f2ae65a871e3070e9b690abe691e
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2022-11-30 14:52:29 +0100

    pairs with ZE

--- opengate/tests/src/test040_gan_phsp_pet_gan.py
+++ opengate/tests/src/test040_gan_phsp_pet_gan.py
@@ -121,6 +121,7 @@ gsource.weight_key = None
 gsource.backward_distance = 10 * cm
 # if the kinetic E is below this threshold, we set it to 0
 gsource.energy_threshold = 0.1 * keV
+gsource.skip_mode = "ZeroEnergy"
 gsource.batch_size = 1e5
 gsource.verbose_generator = True
 # set the generator and the condition generator
