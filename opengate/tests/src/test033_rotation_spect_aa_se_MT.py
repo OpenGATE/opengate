ae53b7048b183afadc96709e0a5ed85e6b5314ff
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2023-01-19 11:16:54 +0100

    adjust tolerance and correct str filename

--- opengate/tests/src/test033_rotation_spect_aa_se_MT.py
+++ opengate/tests/src/test033_rotation_spect_aa_se_MT.py
@@ -17,6 +17,6 @@ for source in sources:
 output = sim.start()
 
 # check
-is_ok = evaluate_test(output, sources, 20, 1968330)
+is_ok = evaluate_test(output, sources, 21, 1968330)
 
 gate.test_ok(is_ok)
