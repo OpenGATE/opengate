bc98566e169bca2bdd1130ab71964408e0946e2f
Nils Krah <krah.nils@gmail.com>
2023-03-22 16:07:40 +0100

    Extended Decorators.py

--- opengate/Decorators.py
+++ opengate/Decorators.py
@@ -1,20 +1,32 @@
 from functools import wraps
 
-from opengate.physics.helpers_physics import available_additional_physics_lists
+from .helpers import fatal, warning
 
 
-# decorator to check if class attribute is None
-def requires(attribute):
+# decorator template to check if class attribute is None
+def requires(attribute, mode="fatal"):
     def decorator(func):
         @wraps(func)
         def _with_check(self, *args, **kwargs):
             if getattr(self, decorator.attribute) is None:
-                raise Exception(
-                    f"Method {func.__name__} of class {type(self)} requires {decorator.attribute} to be set, but it is None."
-                )
+                msg = f"Method {func.__name__} of class {type(self)} requires {decorator.attribute} to be set, but it is None."
+                if decorator.mode == "fatal":
+                    fatal(msg)
+                elif decorator.mode == "warning":
+                    warning(msg)
             return func(self, *args, **kwargs)
 
         return _with_check
 
     decorator.attribute = attribute
+    decorator.mode = mode
+
     return decorator
+
+
+def requires_fatal(attribute):
+    return requires(attribute, mode="fatal")
+
+
+def requires_warning(attribute):
+    return requires(attribute, mode="warning")
