c61dcc74aa3a21ed4552537e74def7b1e74abe97
Nils Krah <krah.nils@gmail.com>
2023-03-23 22:39:36 +0100

    Extended and improved test053_production_cuts

--- opengate/tests/src/test053_production_cuts.py
+++ opengate/tests/src/test053_production_cuts.py
@@ -4,7 +4,9 @@
 import opengate as gate
 import opengate_core as g4
 
-paths = gate.get_default_test_paths(__file__)
+# Physics list default range cut for protons, e+, e-, gamma
+# defined in GEANT4/source/run/src/G4VUserPhysicsList.cc
+DEFAULT_CUT = 1.0
 
 
 def simulate():
@@ -24,10 +26,13 @@ def simulate():
     mm = gate.g4_units("mm")
     MeV = gate.g4_units("MeV")
 
-    requested_cuts_proton = {}
-    # requested_minekine = {}
+    sim.physics_manager.physics_list_name = "G4EmStandardPhysics"
+    global_cut = 1.34 * mm
+    sim.physics_manager.global_production_cuts.electron = global_cut
 
-    # *** Step size in a single volume ***
+    requested_cuts_proton = {}
+
+    # *** Production cuts in a single volume ***
     waterbox_A = sim.add_volume("Box", "waterbox_A")
     waterbox_A.size = [10 * cm, 10 * cm, 10 * cm]
     waterbox_A.translation = [0 * cm, 0 * cm, 11 * cm]
@@ -40,7 +45,7 @@ def simulate():
     sim.set_production_cut(waterbox_A.name, "proton", cut_proton)
     requested_cuts_proton[waterbox_A.name] = cut_proton
 
-    # *** Step sizes in individual volumes in nested volume structure ***
+    # *** Production cuts in individual volumes in nested volume structure ***
     waterbox_B = sim.add_volume("Box", "waterbox_B")
     waterbox_B.size = waterbox_A.size
     waterbox_B.translation = [
@@ -54,7 +59,6 @@ def simulate():
     for i in range(6):
         new_insert = sim.add_volume("Box", f"insert_B_{i}")
         new_insert.size = [0.9 * s for s in previous_mother.size]
-        # new_insert.translation = waterbox_B.translation
         new_insert.material = waterbox_B.material
         new_insert.mother = previous_mother.name
         previous_mother = new_insert
@@ -64,7 +68,7 @@ def simulate():
             sim.set_production_cut(new_insert.name, "proton", cut_proton)
             requested_cuts_proton[new_insert.name] = cut_proton
 
-    # *** Step sizes propagated to nested volumes ***
+    # *** Production cuts propagated to nested volumes ***
     waterbox_C = sim.add_volume("Box", "waterbox_C")
     waterbox_C.size = waterbox_A.size
     waterbox_C.translation = [
@@ -89,7 +93,7 @@ def simulate():
         previous_mother = new_insert
         requested_cuts_proton[new_insert.name] = cut_proton
 
-    # *** Step size set via region object ***
+    # *** Production cuts set via region object ***
     region_D = sim.add_region("region_D")
     region_D.production_cuts.proton = 4.87 * mm
 
@@ -115,6 +119,8 @@ def simulate():
     source.direction.momentum = [0, 0, 1]
     source.n = 1e1
 
+    print(sim.physics_manager.dump_production_cuts())
+
     se = gate.SimulationEngine(sim)
     # Set the hook function user_fct_after_init
     # to the function defined below
@@ -122,28 +128,40 @@ def simulate():
     output = se.start()
 
     print("Checking step limits:")
-    global_cut_proton = None
+    retrieved_global_cut_proton = None
     for item in output.hook_log:
         if item[0] == "world":
-            global_cut_proton = item[1]["proton"]
+            retrieved_global_cut_proton = item[1]["proton"]
+            retrieved_global_cut_positron = item[1]["positron"]
+            retrieved_global_cut_electron = item[1]["electron"]
+            retrieved_global_cut_gamma = item[1]["gamma"]
+
+            print("retrieved_global_cut_positron")
+            print(retrieved_global_cut_positron)
+
+            # Verify that cuts are global cuts in volumes beloging to DefaultWorldRegion
+            # global cut has been set for electron
+            assert retrieved_global_cut_electron == global_cut
+            # but not for the others, so the physics list default is applied
+            assert retrieved_global_cut_proton == DEFAULT_CUT
+            assert retrieved_global_cut_positron == DEFAULT_CUT
+            assert retrieved_global_cut_gamma == DEFAULT_CUT
 
     for item in output.hook_log:
         print(f"Volume {item[0]}:")
         value_dict = item[1]
-        if item[0] == "world":
-            print(f"Found cut for protons in World: {value_dict['proton']}")
-        else:
-            try:
+        if item[0] != "world":
+            if item[0] in requested_cuts_proton.keys():
                 print(
                     f"Requested production cut for protons: {requested_cuts_proton[item[0]]}"
                 )
                 print(f"Found: {value_dict['proton']}")
                 assert requested_cuts_proton[item[0]] == value_dict["proton"]
-            except KeyError:
+            else:
                 print(
                     f"Found production cut for protons, {value_dict['proton']}, but no requested cut."
                 )
-                if value_dict["proton"] == global_cut_proton:
+                if value_dict["proton"] == retrieved_global_cut_proton:
                     print(
                         "... but don't worry, this is just the global cut as expected."
                     )
@@ -151,7 +169,15 @@ def simulate():
                     print(
                         "... and that is strange because it does not match the global cut. "
                     )
-                    raise Exception("Found unexpected production cut")
+                    # raise Exception("Found unexpected production cut")
+            # region cuts were only set for protons
+            # and should be the user-specified global cut for electrons
+            assert value_dict["electron"] == global_cut
+            # and the physics list default (= 1.0) for the others
+            assert value_dict["positron"] == DEFAULT_CUT
+            assert value_dict["gamma"] == DEFAULT_CUT
+            print(value_dict)
+
     print("Test passed")
 
     # return RunManager to avoid garbage collection before the other objects
@@ -179,18 +205,23 @@ def check_user_limit(simulation_engine):
             print(f"In hook: found UserLimit {user_limits}")
 
             pc = g4_volume.g4_region.GetProductionCuts()
-            # UserLimit is None for the DefaultWorldRegion
-            if pc is not None:
-                cut_proton = pc.GetProductionCut("proton")
-                simulation_engine.hook_log.append(
-                    (
-                        volume_name,
-                        {"proton": cut_proton},
-                    )
+            cut_proton = pc.GetProductionCut("proton")
+            # Note: G4 particle names != Gate names for e+ and e-
+            cut_positron = pc.GetProductionCut("e+")
+            cut_electron = pc.GetProductionCut("e-")
+            cut_gamma = pc.GetProductionCut("gamma")
+            simulation_engine.hook_log.append(
+                (
+                    volume_name,
+                    {
+                        "proton": cut_proton,
+                        "positron": cut_positron,
+                        "electron": cut_electron,
+                        "gamma": cut_gamma,
+                    },
                 )
-                print(f"In hook: found production cut for protons {cut_proton}")
-            else:
-                print("ProductionCuts is None")
+            )
+            print(f"In hook: found production cut for protons {cut_proton}")
 
 
 # --------------------------------------------------------------------------
