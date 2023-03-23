501f0f2841c8a29b49a89c3cf4f72232e2077aed
Nils Krah <krah.nils@gmail.com>
2023-03-23 22:36:03 +0100

    Made PhysicsManager compatible with new implementation of production cuts

--- opengate/physics/PhysicsManager.py
+++ opengate/physics/PhysicsManager.py
@@ -57,12 +57,12 @@ class PhysicsManager:
         self.default_physic_list = "QGSP_BERT_EMV"
         ui.physics_list_name = self.default_physic_list
         ui.enable_decay = False
-        ui.production_cuts.world = Box()
-        ui.production_cuts.world.gamma = -1  # -1 means = will be the phys list default
-        ui.production_cuts.world.proton = -1
-        ui.production_cuts.world.electron = -1
-        ui.production_cuts.world.positron = -1
-        ui.production_cuts.world.propagate_to_daughters = True
+        # ui.production_cuts.world = Box()
+        # ui.production_cuts.world.gamma = -1  # -1 means = will be the phys list default
+        # ui.production_cuts.world.proton = -1
+        # ui.production_cuts.world.electron = -1
+        # ui.production_cuts.world.positron = -1
+        # ui.production_cuts.world.propagate_to_daughters = True
         """
         FIXME Energy range not clear : does not work in mono-thread mode
         Ignored for the moment (keep them to None)
@@ -78,6 +78,20 @@ class PhysicsManager:
         ui.energy_range_max = None
         ui.apply_cuts = True
 
+    # define this as property for convenience
+    # will become one automatically after refactoring into GateObject
+    @property
+    def global_production_cuts(self):
+        return self.user_info.global_production_cuts
+
+    @property
+    def physics_list_name(self):
+        return self.user_info.physics_list_name
+
+    @physics_list_name.setter
+    def physics_list_name(self, name):
+        self.user_info.physics_list_name = name
+
     def dump_available_physics_lists(self):
         factory = g4.G4PhysListFactory()
         s = (
@@ -87,27 +101,41 @@ class PhysicsManager:
         )
         return s
 
+    # alias for back-compatibility
     def dump_cuts(self):
-        s = ""
-        for c in self.user_info.production_cuts:
-            s += f"{c} : {self.user_info.production_cuts[c]}\n"
+        gate.warning(
+            "Deprecation warning: Old version dump_cuts called. Update implementation to use dump_production_cuts()."
+        )
+        return self.dump_production_cuts()
+
+    def dump_production_cuts(self):
+        s = "*** Production cuts for World: ***\n"
+        for k, v in self.user_info.global_production_cuts.items():
+            s += f"{k}: {v}\n"
+        if len(self.regions.keys()) > 0:
+            s += f"*** Production cuts per regions ***\n"
+            for region in self.regions.values():
+                s += f"In region {region.name}:\n"
+                s += region.dump_production_cuts()
+        else:
+            s += "*** No cuts per region defined. ***\n"
         return s
 
-    def set_cut(self, volume_name, particle_name, value):
-        cuts = self.user_info.production_cuts
-        if volume_name not in cuts:
-            s = f'Cannot find the volume "{volume_name}" to define its cut.'
-            gate.fatal(s)
-        if particle_name == "all":
-            cuts[volume_name]["gamma"] = value
-            cuts[volume_name]["electron"] = value
-            cuts[volume_name]["positron"] = value
-            cuts[volume_name]["proton"] = value
-            return
-        if particle_name not in cuts[volume_name]:
-            s = f'Cannot find the particle named "{particle_name}" to define its cut in the volume "{volume_name}".'
-            gate.fatal(s)
-        cuts[volume_name][particle_name] = value
+    # def set_cut(self, volume_name, particle_name, value):
+    #     cuts = self.user_info.production_cuts
+    #     if volume_name not in cuts:
+    #         s = f'Cannot find the volume "{volume_name}" to define its cut.'
+    #         gate.fatal(s)
+    #     if particle_name == "all":
+    #         cuts[volume_name]["gamma"] = value
+    #         cuts[volume_name]["electron"] = value
+    #         cuts[volume_name]["positron"] = value
+    #         cuts[volume_name]["proton"] = value
+    #         return
+    #     if particle_name not in cuts[volume_name]:
+    #         s = f'Cannot find the particle named "{particle_name}" to define its cut in the volume "{volume_name}".'
+    #         gate.fatal(s)
+    #     cuts[volume_name][particle_name] = value
 
     def create_region(self, region_name):
         if region_name in self.regions.keys():
@@ -123,6 +151,17 @@ class PhysicsManager:
         else:
             region = self.volumes_regions_lut[volume_name]
         return region
+
+    # keep 'old' function name for compatibility
+    def set_cut(self, volume_name, particle_name, value):
+        self.set_production_cut(volume_name, particle_name, value)
+
+    # New name, more specific
+    def set_production_cut(
+        self, volume_name, particle_name, value, propagate_to_daughters=False
+    ):
+        region = self.find_or_create_region(volume_name, propagate_to_daughters)
+        region.production_cuts[particle_name] = value
 
     # set methods for the user_info parameters
     # logic: every volume with user_infos must be associated
@@ -156,8 +195,3 @@ class PhysicsManager:
     def set_user_limits_particles(self, volume_name, particle_names):
         region = self.find_or_create_region(volume_name)
         region.user_limits["particles"] = particle_names
-
-    # This method is here for completeness. Not used yet
-    def set_production_cut(self, volume_name, particle_name, value):
-        region = self.find_or_create_region(volume_name)
-        region.production_cuts[particle_name] = value
