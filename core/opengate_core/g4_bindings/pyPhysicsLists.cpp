d5d59fc84e0a3f3526fd9cbf6a6cbfb055825e5c
Nils Krah <krah.nils@gmail.com>
2023-03-23 22:31:52 +0100

    modified comment in pyPhysicsList

--- core/opengate_core/g4_bindings/pyPhysicsLists.cpp
+++ core/opengate_core/g4_bindings/pyPhysicsLists.cpp
@@ -74,7 +74,7 @@ namespace py = pybind11;
 // (nodelete is needed because it is deleted in cpp side (runmanager?)
 // NK: Yes, the RunManager destructor calls the destructors of all
 // G4VPhysicsConstructor objects in a physics list
-// then also on py side, so seg fault at destruction)
+// then also on py side, so seg fault during garbage collection)
 #define ADD_PHYSICS_CONSTRUCTOR(plname)                                        \
   py::class_<plname, G4VPhysicsConstructor,                                    \
              std::unique_ptr<plname, py::nodelete>>(m, #plname)                \
