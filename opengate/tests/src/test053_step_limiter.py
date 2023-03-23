81670d483398f019728df055acb8d620aef98f72
Nils Krah <krah.nils@gmail.com>
2023-03-23 14:17:37 +0100

    Extend test053 to cover nested geometries and UserSpecialCuts

--- opengate/tests/src/test053_step_limiter.py
+++ opengate/tests/src/test053_step_limiter.py
@@ -24,17 +24,100 @@ def simulate():
     mm = gate.g4_units("mm")
     MeV = gate.g4_units("MeV")
 
-    # add a simple volume
-    waterbox = sim.add_volume("Box", "waterbox")
-    waterbox.size = [40 * cm, 40 * cm, 40 * cm]
-    waterbox.translation = [0 * cm, 0 * cm, 21 * cm]
-    waterbox.material = "G4_WATER"
+    requested_stepsizes = {}
+    requested_minekine = {}
+
+    # *** Step size in a single volume ***
+    waterbox_A = sim.add_volume("Box", "waterbox_A")
+    waterbox_A.size = [10 * cm, 10 * cm, 10 * cm]
+    waterbox_A.translation = [0 * cm, 0 * cm, 11 * cm]
+    waterbox_A.material = "G4_WATER"
+
+    # Choose an "awkward" step size
+    # which does not corrispond to any of Geant4's
+    # defaults to make the assertion (below) significant
+    stepsize = 1.47 * mm
+    min_ekine = 10.7 * MeV
+    sim.set_max_step_size(waterbox_A.name, stepsize)
+    sim.set_min_ekine(waterbox_A.name, min_ekine)
+    requested_stepsizes[waterbox_A.name] = stepsize
+    requested_minekine[waterbox_A.name] = min_ekine
+
+    # *** Step sizes in individual volumes in nested volume structure ***
+    waterbox_B = sim.add_volume("Box", "waterbox_B")
+    waterbox_B.size = waterbox_A.size
+    waterbox_B.translation = [
+        0 * cm,
+        0 * cm,
+        waterbox_A.translation[2] + 1.1 * waterbox_B.size[2],
+    ]
+    waterbox_B.material = "G4_WATER"
+
+    previous_mother = waterbox_B
+    for i in range(6):
+        new_insert = sim.add_volume("Box", f"insert_B_{i}")
+        new_insert.size = [0.9 * s for s in previous_mother.size]
+        # new_insert.translation = waterbox_B.translation
+        new_insert.material = waterbox_B.material
+        new_insert.mother = previous_mother.name
+        previous_mother = new_insert
+        # Set step in every second insert
+        if i % 2 == 0:
+            stepsize = 2.1 + i / 100.0 * mm
+            min_ekine = 20.1 + i / 100.0 * MeV
+            sim.set_max_step_size(new_insert.name, stepsize)
+            sim.set_min_ekine(new_insert.name, min_ekine)
+            requested_stepsizes[new_insert.name] = stepsize
+            requested_minekine[new_insert.name] = min_ekine
+
+    # *** Step sizes propagated to nested volumes ***
+    waterbox_C = sim.add_volume("Box", "waterbox_C")
+    waterbox_C.size = waterbox_A.size
+    waterbox_C.translation = [
+        0 * cm,
+        0 * cm,
+        waterbox_B.translation[2] + 1.1 * waterbox_C.size[2],
+    ]
+    waterbox_C.material = "G4_WATER"
+
+    stepsize_C = 3.39 * mm
+    min_ekine = 30.39 * MeV
+    sim.set_max_step_size(waterbox_C.name, stepsize_C, propagate_to_daughters=True)
+    sim.set_min_ekine(waterbox_C.name, min_ekine, propagate_to_daughters=True)
+    requested_stepsizes[waterbox_C.name] = stepsize_C
+    requested_minekine[waterbox_C.name] = min_ekine
+
+    previous_mother = waterbox_C
+    for i in range(6):
+        new_insert = sim.add_volume("Box", f"insert_C_{i}")
+        new_insert.size = [0.9 * s for s in previous_mother.size]
+        new_insert.material = waterbox_C.material
+        new_insert.mother = previous_mother.name
+        previous_mother = new_insert
+        requested_stepsizes[new_insert.name] = stepsize_C
+        requested_minekine[new_insert.name] = min_ekine
+
+    # *** Step size set via region object ***
+    region_D = sim.add_region("region_D")
+    region_D.user_limits.max_step_size = 4.87 * mm
+    region_D.user_limits.min_ekine = 40.87 * MeV
+
+    for i in range(4):
+        new_box = sim.add_volume("Box", f"waterbox_D{i}")
+        new_box.size = [1 * mm, 1 * mm, 1 * mm]
+        new_box.translation = [
+            i * 2 * cm,
+            0 * cm,
+            waterbox_C.translation[2] + 1.1 * waterbox_C.size[2],
+        ]
+        new_box.material = "G4_WATER"
+        region_D.associate_volume(new_box)
+        requested_stepsizes[new_box.name] = region_D.user_limits.max_step_size
+        requested_minekine[new_box.name] = region_D.user_limits.min_ekine
 
     # Arbritrary source because we do not really need
     # the simulation, only the initialization
-    source = sim.add_source(
-        "GenericSource", "Default"
-    )  # FIXME warning ref not OK (cppSource not the same)
+    source = sim.add_source("GenericSource", "Default")
     source.particle = "proton"
     source.energy.mono = energy * MeV
     source.position.radius = 1 * mm
@@ -42,23 +125,22 @@ def simulate():
     source.direction.momentum = [0, 0, 1]
     source.n = 1e1
 
-    # Choose an "awkward" step size
-    # which does not corrispond to any of Geant4's
-    # defaults to make the assertion (below) significant
-    stepsize = 1.47
-    sim.set_max_step_size("waterbox", stepsize * mm)
-
     se = gate.SimulationEngine(sim)
     # Set the hook function user_fct_after_init
     # to the function defined below
     se.user_fct_after_init = check_user_limit
     output = se.start()
 
-    # Assert that the retrieved step size
-    # equals the requested one
-    print(f"Requested step limit in {waterbox.name} was {stepsize}.")
-    print(f"Geant4 reports {output.hook_log[0]}.")
-    assert stepsize == output.hook_log[0]
+    print("Checking step limits:")
+    for item in output.hook_log:
+        print(f"Volume {item[0]}:")
+        value_dict = item[1]
+        print(f"Requested max_step_size: {requested_stepsizes[item[0]]}")
+        print(f"Found: {value_dict['max_step_size']}")
+        print(f"Requested min_ekine: {requested_minekine[item[0]]}")
+        print(f"Found: {value_dict['min_ekine']}")
+        assert requested_stepsizes[item[0]] == value_dict["max_step_size"]
+        assert requested_minekine[item[0]] == value_dict["min_ekine"]
     print("Test passed")
 
     # return RunManager to avoid garbage collection before the other objects
@@ -77,18 +159,32 @@ def check_user_limit(simulation_engine):
 
     """
     print(f"Entered hook")
-    g4_volume = simulation_engine.volume_engine.g4_volumes["waterbox"]
-    if g4_volume.g4_region is not None:
-        name = g4_volume.g4_region.GetName()
-        print(f"In hook: found region {name}")
-        user_limits = g4_volume.g4_region.GetUserLimits()
-        print(f"In hook: found UserLimit {user_limits}")
+    for volume_name, g4_volume in simulation_engine.volume_engine.g4_volumes.items():
+        # print(volume_name, g4_volume.g4_region)
+        if g4_volume.g4_region is not None:
+            region_name = g4_volume.g4_region.GetName()
+            print(f"In hook: found volume {volume_name} with region {region_name}")
+            user_limits = g4_volume.g4_region.GetUserLimits()
+            print(f"In hook: found UserLimit {user_limits}")
 
-        max_step_size = g4_volume.g4_region.GetUserLimits().GetMaxAllowedStep(
-            g4.G4Track()
-        )
-        simulation_engine.hook_log.append(max_step_size)
-        print(f"In hook: found max_step_size {max_step_size}")
+            ul = g4_volume.g4_region.GetUserLimits()
+            # UserLimit is None for the DefaultWorldRegion
+            if ul is not None:
+                max_step_size = g4_volume.g4_region.GetUserLimits().GetMaxAllowedStep(
+                    g4.G4Track()
+                )
+                min_ekine = g4_volume.g4_region.GetUserLimits().GetUserMinEkine(
+                    g4.G4Track()
+                )
+                simulation_engine.hook_log.append(
+                    (
+                        volume_name,
+                        {"max_step_size": max_step_size, "min_ekine": min_ekine},
+                    )
+                )
+                print(f"In hook: found max_step_size {max_step_size}")
+            else:
+                print("UserLimits is None")
 
 
 # --------------------------------------------------------------------------
