baadeda77538d83c405c0c3ef6aaa11ea198870c
Nils Krah <krah.nils@gmail.com>
2023-06-13 09:16:31 +0200

    clean up and em_parameters

--- opengate/physics/PhysicsEngine.py
+++ opengate/physics/PhysicsEngine.py
@@ -1,8 +1,9 @@
 import opengate as gate
 import opengate_core as g4
-from anytree import LevelOrderIter
 
-from ..Decorators import requires_fatal
+# from anytree import LevelOrderIter
+
+from ..Decorators import requires_fatal, requires_warning
 from .PhysicsConstructors import UserLimitsPhysics
 from opengate_core import G4ApplicationState
 from .helpers_physics import translate_particle_name_gate2G4
@@ -50,7 +51,7 @@ class PhysicsEngine(gate.EngineBase):
         self.g4_em_parameters = None
 
     @requires_fatal("simulation_engine")
-    @requires_fatal("g4_physics_list")
+    @requires_warning("g4_physics_list")
     def close_physics_constructors(self):
         """This method removes PhysicsConstructors defined in python from the physics list.
 
@@ -63,7 +64,6 @@ class PhysicsEngine(gate.EngineBase):
         self.simulation_engine.g4_state = G4ApplicationState.G4State_PreInit
         for pc in self.gate_physics_constructors:
             self.g4_physics_list.RemovePhysics(pc)
-            # pc.close()
         self.simulation_engine.g4_state = current_state
 
     # make this a property so the communication between
@@ -82,6 +82,7 @@ class PhysicsEngine(gate.EngineBase):
         self.initialize_decay()
         self.initialize_em_options()
         self.initialize_user_limits_physics()
+        # self.initialize_g4_em_parameters()
 
     def initialize_after_runmanager(self):
         """Initialize methods to be called *after*
@@ -92,29 +93,38 @@ class PhysicsEngine(gate.EngineBase):
         """
         self.initialize_global_cuts()
         self.initialize_regions()
+        self.initialize_g4_em_parameters()
 
     def initialize_physics_list(self):
         """
         Create a Physic List from the Factory
         """
-        ui = self.physics_manager.user_info
-        pl_name = ui.physics_list_name
-        # Select the Physic List: check if simple ones
-        if pl_name.startswith("G4"):
-            self.g4_physics_list = gate.create_modular_physics_list(pl_name)
-        else:
-            # If not, select the Physic List from the Factory
-            factory = g4.G4PhysListFactory()
-            if not factory.IsReferencePhysList(pl_name):
-                s = (
-                    f"Cannot find the physic list : {pl_name}\n"
-                    f"Known list are : {factory.AvailablePhysLists()}\n"
-                    f"With EM : {factory.AvailablePhysListsEM()}\n"
-                    f"Default is {self.physics_manager.default_physic_list}\n"
-                    f"Help : https://geant4-userdoc.web.cern.ch/UsersGuides/PhysicsListGuide/html/physicslistguide.html"
-                )
-                gate.fatal(s)
-            self.g4_physics_list = factory.GetReferencePhysList(pl_name)
+        physics_list_name = self.physics_manager.user_info.physics_list_name
+        self.g4_physics_list = (
+            self.physics_manager.physics_list_manager.get_physics_list(
+                physics_list_name
+            )
+        )
+
+    # OLD CODE, LEFT HERE FOR NOW FOR REFERENCE
+    # # Select the Physic List: check if simple ones
+    # if pl_name.startswith("G4"):
+    #     print(f"Creating the physics list object named {pl_name} dynamically.")
+    #     self.g4_physics_list = gate.create_modular_physics_list(pl_name)
+    # else:
+    #     print(f"Using G4PhysListFactory to create the physics list object named {pl_name}.")
+    #     # If not, select the Physic List from the Factory
+    #     factory = g4.G4PhysListFactory()
+    #     if not factory.IsReferencePhysList(pl_name):
+    #         s = (
+    #             f"Cannot find the physic list : {pl_name}\n"
+    #             f"Known list are : {factory.AvailablePhysLists()}\n"
+    #             f"With EM : {factory.AvailablePhysListsEM()}\n"
+    #             f"Default is {self.physics_manager.default_physic_list}\n"
+    #             f"Help : https://geant4-userdoc.web.cern.ch/UsersGuides/PhysicsListGuide/html/physicslistguide.html"
+    #         )
+    #         gate.fatal(s)
+    #     self.g4_physics_list = factory.GetReferencePhysList(pl_name)
 
     def initialize_decay(self):
         """
@@ -126,6 +136,7 @@ class PhysicsEngine(gate.EngineBase):
             return
         # check if decay/radDecay already exist in the physics list
         # (keep p and pp in self to prevent destruction)
+        # NOTE: can we use Replace() method from G4VModularPhysicsList?
         self.g4_decay = self.g4_physics_list.GetPhysics("Decay")
         if not self.g4_decay:
             self.g4_decay = g4.G4DecayPhysics(1)
@@ -160,12 +171,9 @@ class PhysicsEngine(gate.EngineBase):
         if ui.global_production_cuts.all is not None:
             # calls SetCutValue for all relevant particles,
             # i.e. proton, gamma, e+, e-
-            # self.g4_physics_list.SetDefaultCutValue(ui.global_production_cuts.all)
-            for pname in self.physics_manager.cut_particle_names.keys():
-                self.g4_physics_list.SetCutValue(
-                    ui.global_production_cuts.all,
-                    translate_particle_name_gate2G4(pname),
-                )
+            for pname in self.physics_manager.cut_particle_names.values():
+                self.g4_physics_list.SetCutValue(ui.global_production_cuts.all, pname)
+
         else:
             for pname, value in ui.global_production_cuts.items():
                 # ignore 'all', as that's already treated above
@@ -176,20 +184,24 @@ class PhysicsEngine(gate.EngineBase):
                         value, translate_particle_name_gate2G4(pname)
                     )
 
-        # global cuts
+    def initialize_g4_em_parameters(self):
+        ui = self.physics_manager.user_info
         self.g4_em_parameters = g4.G4EmParameters.Instance()
         self.g4_em_parameters.SetApplyCuts(ui.apply_cuts)
-        if ui.apply_cuts is False:
-            s = f"No production cuts (apply_cuts is False)"
-            gate.warning(s)
-            gate.fatal(
-                "Not implemented: currently it crashes when apply_cuts is False. To be continued ..."
-            )
-            return
 
-        # # production cuts by region
-        # for region in ui.production_cuts:
-        #     self.set_region_cut(region)
+        self.g4_em_parameters.SetFluo(ui.em_parameters["fluo"])
+        self.g4_em_parameters.SetAuger(ui.em_parameters["auger"])
+        self.g4_em_parameters.SetAugerCascade(ui.em_parameters["auger_cascade"])
+        self.g4_em_parameters.SetPixe(ui.em_parameters["pixe"])
+        self.g4_em_parameters.SetDeexcitationIgnoreCut(
+            ui.em_parameters["deexcitation_ignore_cut"]
+        )
+
+        # Switches need to be implemented in Region class
+        # -> after PR is merged.
+        # self.g4_em_parameters.SetDeexActiveRegion(
+        #     "DefaultRegionForTheWorld", True, True, True
+        # )
 
     @requires_fatal("physics_manager")
     def initialize_user_limits_physics(self):
@@ -206,50 +218,3 @@ class PhysicsEngine(gate.EngineBase):
             user_limits_physics.physics_engine = self
             self.g4_physics_list.RegisterPhysics(user_limits_physics)
             self.gate_physics_constructors.append(user_limits_physics)
-
-            # Use StepLimiterPhysics from Geant4 for now
-            # The costum constructor still segfaults
-            # self.user_limits_physics = g4.G4StepLimiterPhysics()
-
-    # def propagate_cuts_to_child(self, tree):
-    #     """This method is kept for legacy compatibility
-    #     because the method name was changed to
-    #     propagate_cuts_to_children.
-    #     Should be deleted soon.
-
-    #     """
-    #     print(10 * "*" + 10 * "#" + 10 * "*")
-    #     print("Old propagate_cuts_to_child method invoked. Please update the code.")
-    #     print(10 * "*" + 10 * "#" + 10 * "*")
-    #     self.propagate_cuts_to_children(tree)
-
-    # def propagate_cuts_to_children(self, tree):
-    #     ui = self.physics_manager.user_info
-    #     pc = ui.production_cuts
-    #     # loop on the tree, level order
-    #     for node in LevelOrderIter(tree[gate.__world_name__]):
-    #         if not node.parent:
-    #             # this is the world, do nothing
-    #             continue
-    #         # get the user cuts for this region
-    #         if node.name in pc:
-    #             cuts = pc[node.name]
-    #         else:
-    #             gate.fatal(f"Cannot find region {node.name} in the cuts list: {pc}")
-    #         # get the cuts for the parent
-    #         if node.parent.name in pc:
-    #             pcuts = pc[node.parent.name]
-    #         else:
-    #             gate.fatal(
-    #                 f"Cannot find parent region {node.parent.name} in the cuts list: {pc}"
-    #             )
-    #         for p in self.physics_manager.cut_particle_names:
-    #             # set the cut for the current region like the parent
-    #             # but only if it was not set by user
-    #             if p not in cuts:
-    #                 if p in pcuts:
-    #                     cuts[p] = pcuts[p]
-    #                 else:
-    #                     # special case when the parent=world, with default cuts
-    #                     cuts[p] = -1
-    #                     pcuts[p] = -1
