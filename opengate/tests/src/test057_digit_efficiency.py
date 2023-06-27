247d083f54d63d30300fda676447b900d0cab728
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2023-06-27 12:52:25 +0200

    Change physics cuts

--- opengate/tests/src/test057_digit_efficiency.py
+++ opengate/tests/src/test057_digit_efficiency.py
@@ -61,11 +61,10 @@ crystal.color = [1, 1, 0, 1]
 p = sim.get_physics_user_info()
 p.physics_list_name = "G4EmStandardPhysics_option4"
 p.enable_decay = False
-cuts = p.production_cuts
-cuts.world.gamma = 0.01 * mm
-cuts.world.electron = 0.01 * mm
-cuts.world.positron = 1 * mm
-cuts.world.proton = 1 * mm
+sim.physics_manager.global_production_cuts.gamma = 0.01 * mm
+sim.physics_manager.global_production_cuts.electron = 0.01 * mm
+sim.physics_manager.global_production_cuts.positron = 1 * mm
+sim.physics_manager.global_production_cuts.proton = 1 * mm
 
 # default source for tests
 source = sim.add_source("GenericSource", "Default")
