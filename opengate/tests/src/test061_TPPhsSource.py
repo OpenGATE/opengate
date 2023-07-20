0f262353a1eedc53a98c28d668e96a922f1d5c20
Hermann Fuchs <hermann.fuchs@meduniwien.ac.at>
2023-07-20 16:06:49 +0200

    Test for TPPhsSource without treatment plan

---
+++ opengate/tests/src/test061_TPPhsSource.py
@@ -0,0 +1,75 @@
+import test061_TPPhsSource_helpers as t
+import opengate as gate
+from scipy.spatial.transform import Rotation
+
+
+paths = gate.get_default_test_paths(
+    __file__, "test061_TPPhsSource", output_folder="test061"
+)
+
+ref_path = paths.data / "test061_ref"
+
+# units
+m = gate.g4_units("m")
+mm = gate.g4_units("mm")
+cm = gate.g4_units("cm")
+nm = gate.g4_units("nm")
+Bq = gate.g4_units("Bq")
+MeV = gate.g4_units("MeV")
+deg: float = gate.g4_units("deg")
+
+
+def main():
+    # print("create reference PhS file")
+    # t.create_test_Phs(
+    #     particle="proton",
+    #     phs_name=paths.output / "test_proton",
+    #     number_of_particles=1,
+    #     translation=[0 * cm, 0 * cm, 0 * mm],
+    # )
+    print("Testing TPPhS source rotations")
+
+    t.test_source_rotation_A(
+        plan_file_name=ref_path / "PlanSpot.txt",
+        phs_list_file_name=ref_path / "PhsList.txt",
+        phs_file_name_out=paths.output / "output.root",
+    )
+
+    a = t.check_value_from_root_file(
+        file_name_root=paths.output / "output.root",
+        key="KineticEnergy",
+        ref_value=150 * MeV,
+    )
+    b = t.check_value_from_root_file(
+        file_name_root=paths.output / "output.root",
+        key="PrePositionLocal_X",
+        ref_value=-60,
+    )
+    c = t.check_value_from_root_file(
+        file_name_root=paths.output / "output.root",
+        key="PrePositionLocal_Y",
+        ref_value=50,
+    )
+    d = t.check_value_from_root_file(
+        file_name_root=paths.output / "output.root",
+        key="PrePositionLocal_Z",
+        ref_value=0,
+    )
+
+    e = t.check_value_from_root_file(
+        file_name_root=paths.output / "output.root",
+        key="PreDirectionLocal_X",
+        ref_value=-0.012,
+    )
+    f = t.check_value_from_root_file(
+        file_name_root=paths.output / "output.root",
+        key="PreDirectionLocal_Y",
+        ref_value=0.01,
+    )
+
+    # this is the end, my friend
+    gate.test_ok(a and b and c and d and e and f)
+
+
+if __name__ == "__main__":
+    main()
