b39d1fd5ec956c47426f271bccf398e4f702c7ce
Nils Krah <krah.nils@gmail.com>
2023-07-25 03:12:45 +0200

    Update test016_bool_volumes to refactored Volumes

--- opengate/tests/src/test016_bool_volumes.py
+++ opengate/tests/src/test016_bool_volumes.py
@@ -32,38 +32,45 @@ world.size = [1 * m, 1 * m, 1 * m]
 
 # create a union of several volumes
 
-# first create the solids
-b = sim.new_solid("Box", "box")
+# first create the boolean volumes
+# use the create_volume() method, *not* the create_and_add_volume() method
+b = sim.volume_manager.create_volume("Box", "box")
 b.size = [10 * cm, 10 * cm, 10 * cm]
-s = sim.new_solid("Sphere", "sphere")
+s = sim.volume_manager.create_volume("Sphere", "test_sph")
 s.rmax = 5 * cm
-t = sim.new_solid("Tubs", "t")
+t = sim.volume_manager.create_volume("Tubs", "t")
 t.rmin = 0
 t.rmax = 2 * cm
 t.dz = 15 * cm
 
 # bool operations
-a = gate.solid_union(b, s, [0, 1 * cm, 5 * cm])
-a = gate.solid_subtraction(a, t, [0, 1 * cm, 5 * cm])
-a = gate.solid_union(a, b, [0, -1 * cm, -5 * cm])  # strange but ok
-b = gate.solid_intersection(t, s, [3 * cm, 0, 0])
-a = gate.solid_union(a, b, [0, -7 * cm, -5 * cm])
+a = s.add_to(b, translation=[0, 1 * cm, 5 * cm])
+a = t.subtract_from(a, translation=[0, 1 * cm, 5 * cm])
+a = b.add_to(a, translation=[0, -1 * cm, -5 * cm])
+b = s.intersect_with(t, translation=[3 * cm, 0, 0])
+u = b.add_to(a, translation=[0, -7 * cm, -5 * cm], new_name="my_stuff")
+
+# a = gate.solid_union(b, s, [0, 1 * cm, 5 * cm])
+# a = gate.solid_subtraction(a, t, [0, 1 * cm, 5 * cm])
+# a = gate.solid_union(a, b, [0, -1 * cm, -5 * cm])  # strange but ok
+# b = gate.solid_intersection(t, s, [3 * cm, 0, 0])
+# a = gate.solid_union(a, b, [0, -7 * cm, -5 * cm])
 
 # then add them to a Union, with translation/rotation
-rot = Rotation.from_euler("x", 33, degrees=True).as_matrix()
-u = sim.add_volume_from_solid(a, "my_stuff")
+# u = sim.add_volume_from_solid(a, "my_stuff")
+sim.volume_manager.add_volume(u)
 u.translation = [5 * cm, 5 * cm, 5 * cm]
-u.rotation = rot
+u.rotation = Rotation.from_euler("x", 33, degrees=True).as_matrix()
 u.mother = "world"
 u.material = "G4_WATER"
 u.color = [0, 1, 0, 1]
 
 # create a volume from a solid (not really useful)
-u = sim.add_volume_from_solid(s, "test_sph")
-u.translation = [-5 * cm, -5 * cm, 1 - 5 * cm]
-u.mother = "world"
-u.material = "G4_WATER"
-u.color = [0, 1, 1, 1]
+sim.volume_manager.add_volume(s)
+s.translation = [-5 * cm, -5 * cm, 1 - 5 * cm]
+s.mother = "world"
+s.material = "G4_WATER"
+s.color = [0, 1, 1, 1]
 
 # default source for tests
 source = sim.add_source("GenericSource", "Default")
