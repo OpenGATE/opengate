added option to correct for bias in standard error calculation from the mean

opengate/helpers_tests.py
@@ -1420,14 +1420,21 @@ def assert_img_sum(img1, img2, sum_tolerance=5):
     return b
 
 
-def assert_images_ratio(expected_ratio, mhd_1, mhd_2, abs_tolerance=0.1):
+def assert_images_ratio(
+    expected_ratio, mhd_1, mhd_2, abs_tolerance=0.1, fn_to_apply=None
+):
     img1 = itk.imread(str(mhd_1))
     img2 = itk.imread(str(mhd_2))
     data1 = itk.GetArrayViewFromImage(img1).ravel()
     data2 = itk.GetArrayViewFromImage(img2).ravel()
 
-    sum1 = np.sum(data1)
-    sum2 = np.sum(data2)
+    if fn_to_apply is None:
+        fn_to_apply = lambda x: np.sum(x)
+    sum2 = fn_to_apply(data2)
+    sum1 = fn_to_apply(data1)
+    # if mode.lower() in [ "sum", "cumulative"]:
+    # sum1 = np.sum(data1)
+    # sum2 = np.sum(data2)
     ratio = sum2 / sum1
 
     print("\nSum energy dep for phantom 1: ", sum1)
@@ -1456,9 +1463,18 @@ def assert_images_ratio_per_voxel(expected_ratio, mhd_1, mhd_2, abs_tolerance=0.
     N_within_tolerance = np.sum(within_tolerance_M)
     fraction_within_tolerance = N_within_tolerance / np.array(data1).size
     fraction_within_tolerance = N_within_tolerance / np.sum(data2 != 0)
+
+    mean = np.mean(ratio)
+    std = np.std(ratio)
     print("Ratio is: ", ratio)
     print("Expected ratio is: ", expected_ratio)
     print(f"{fraction_within_tolerance =}")
+    print(f"Mean {mean} \nStd {std}")
+
+    data1_mean = np.mean(data1[:])
+    data2_mean = np.mean(data2[:])
+    print(f"{data1_mean =}")
+    print(f"{data2_mean =}")
     is_ok = False
     if fraction_within_tolerance > 0.999:
         is_ok = True

opengate/tests/src/test041_dose_actor_MT_cp_images_Nthreads.py
@@ -4,6 +4,7 @@
 import opengate as gate
 from scipy.spatial.transform import Rotation
 import matplotlib.pyplot as plt
+import numpy as np
 
 paths = gate.get_default_test_paths(__file__, "gate_test041_dose_actor_dose_to_water")
 
@@ -16,7 +17,9 @@ ui.g4_verbose = False
 ui.g4_verbose_level = 1
 ui.visu = False
 # ui.random_seed = 123456789
-ui.number_of_threads = 20
+ui.number_of_threads = 8
+Ntotal = 100000 * 10
+N_per_trhead = Ntotal / ui.number_of_threads
 # units
 m = gate.g4_units("m")
 cm = gate.g4_units("cm")
@@ -48,7 +51,7 @@ sim.set_cut("world", "all", 1000 * km)
 
 # default source for tests
 source = sim.add_source("GenericSource", "mysource")
-source.energy.mono = 100 * MeV
+source.energy.mono = 80 * MeV
 source.particle = "proton"
 source.position.type = "disc"
 source.position.rotation = Rotation.from_euler("y", 90, degrees=True).as_matrix()
@@ -57,10 +60,10 @@ source.position.sigma_y = 2 * mm
 source.position.translation = [0, 0, 0]
 source.direction.type = "momentum"
 source.direction.momentum = [-1, 0, 0]
-source.n = 100000
+source.n = N_per_trhead
 
-dose_size = [50, 1, 1]
-dose_spacing = [1 * mm, 100.0 * mm, 100.0 * mm]
+dose_size = [100, 1, 1]
+dose_spacing = [0.5 * mm, 100.0 * mm, 100.0 * mm]
 doseActorName_IDD_singleImage = "IDD_singleImage"
 doseActor = sim.add_actor("DoseActor", doseActorName_IDD_singleImage)
 doseActor.output = paths.output / ("test041-" + doseActorName_IDD_singleImage + ".mhd")
@@ -90,13 +93,28 @@ doseActor.ste_of_mean = True
 doseActor.uncertainty = False
 doseActor.square = False
 
+doseActorName_IDD_NthreadImages_Unbiased = "IDD_NthreadImages_Unbiased"
+doseActor = sim.add_actor("DoseActor", doseActorName_IDD_NthreadImages_Unbiased)
+doseActor.output = paths.output / (
+    "test041-" + doseActorName_IDD_NthreadImages_Unbiased + ".mhd"
+)
+doseActor.mother = phantom.name
+doseActor.size = dose_size
+doseActor.spacing = dose_spacing
+doseActor.hit_type = "random"
+doseActor.dose = False
+# doseActor.use_more_RAM = True
+doseActor.ste_of_mean_unbiased = True
+doseActor.uncertainty = False
+doseActor.square = False
 
 # add stat actor
 s = sim.add_actor("SimulationStatisticsActor", "stats")
 s.track_types_flag = True
 
+
 # start simulation
-sim.n = 20000
+sim.n = int(N_per_trhead)
 output = sim.run()
 # output = sim.run(start_new_process=True)
 
@@ -123,6 +141,10 @@ doseFpath_IDD_NthreadImages_uncer = str(
     sim.output.get_actor(doseActorName_IDD_NthreadImages).user_info.output
 ).replace(".mhd", "-Uncertainty.mhd")
 
+doseFpath_IDD_NthreadImages_uncer_unbias = str(
+    sim.output.get_actor(doseActorName_IDD_NthreadImages_Unbiased).user_info.output
+).replace(".mhd", "-Uncertainty.mhd")
+
 
 unused = gate.assert_images(
     doseFpath_IDD_singleImage,
@@ -142,11 +164,23 @@ is_ok = gate.assert_images_ratio(
 )
 
 is_ok = (
-    gate.assert_images_ratio_per_voxel(
+    gate.assert_images_ratio(
         expected_ratio,
         doseFpath_IDD_singleImage_uncert,
         doseFpath_IDD_NthreadImages_uncer,
         abs_tolerance=0.05,
+        fn_to_apply=lambda x: np.mean(x),
+    )
+    and is_ok
+)
+
+is_ok = (
+    gate.assert_images_ratio(
+        expected_ratio,
+        doseFpath_IDD_singleImage_uncert,
+        doseFpath_IDD_NthreadImages_uncer_unbias,
+        abs_tolerance=0.05,
+        fn_to_apply=lambda x: np.mean(x),
     )
     and is_ok
 )