5eaef4e6e5e418f5393b8ac21dda832536b22af3
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2023-08-30 22:02:12 +0200

    new test MT

---
+++ opengate/tests/src/test033_rotation_spect_noaa_MT.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from test033_rotation_spect_aa_helpers import *
+
+if __name__ == "__main__":
+    # create the simulation
+    sim = gate.Simulation()
+    sources = create_test(sim, nb_thread=4)
+
+    # AA mode
+    for source in sources:
+        source.direction.acceptance_angle.intersection_flag = False
+        source.direction.acceptance_angle.normal_flag = False
+
+    # go
+    sim.run()
+
+    # check
+    is_ok = evaluate_test(sim.output, sources, 10, 0)
+
+    gate.test_ok(is_ok)
