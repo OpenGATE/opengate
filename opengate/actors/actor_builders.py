2bd2f56e737226de0afafb45b9ea02a598e7d32a
Nils Krah <krah.nils@gmail.com>
2023-09-20 00:54:42 +0300

    update imports in actor_builders.py

--- opengate/actors/actor_builders.py
+++ opengate/actors/actor_builders.py
@@ -1,21 +1,24 @@
-from .arfactors import ARFActor
-from .arfactors import ARFTrainingDatasetActor
-from .DoseActor import DoseActor
-from .LETActor import LETActor
-from .DigitizerAdderActor import DigitizerAdderActor
-from .DigitizerReadoutActor import DigitizerReadoutActor
-from .DigitizerHitsCollectionActor import DigitizerHitsCollectionActor
-from .DigitizerEnergyWindowsActor import DigitizerEnergyWindowsActor
-from .DigitizerProjectionActor import DigitizerProjectionActor
-from .DigitizerBlurringActor import DigitizerBlurringActor
-from .DigitizerSpatialBlurringActor import DigitizerSpatialBlurringActor
-from .DigitizerEfficiencyActor import DigitizerEfficiencyActor
-from .MotionVolumeActor import MotionVolumeActor
-from .PhaseSpaceActor import PhaseSpaceActor
-from .SimulationStatisticsActor import SimulationStatisticsActor
-from .SourceInfoActor import SourceInfoActor
-from .TestActor import TestActor
+from .arfactors import ARFActor, ARFTrainingDatasetActor
+from .doseactors import DoseActor, LETActor
+from .digitizers import (
+    DigitizerAdderActor,
+    DigitizerReadoutActor,
+    DigitizerHitsCollectionActor,
+    DigitizerEnergyWindowsActor,
+    DigitizerProjectionActor,
+    DigitizerBlurringActor,
+    DigitizerSpatialBlurringActor,
+    DigitizerEfficiencyActor,
+    PhaseSpaceActor,
+)
+from .misc import (
+    MotionVolumeActor,
+    SimulationStatisticsActor,
+    SourceInfoActor,
+    TestActor,
+)
 from ..helpers import make_builders
+
 
 actor_type_names = {
     SimulationStatisticsActor,
