ccb85850c7e6c0a9710257e12ba92d7da30bf346
Nils Krah <krah.nils@gmail.com>
2023-09-19 15:28:12 +0300

    remove builder mechanism from helpers_source.py

--- opengate/source/helpers_source.py
+++ opengate/source/helpers_source.py
@@ -1,32 +1,7 @@
 import pathlib
 import numpy as np
+from ..helpers import g4_units
 
-from .GenericSource import GenericSource
-from .VoxelsSource import VoxelsSource
-from .GANSource import GANSource
-from .GANPairsSource import GANPairsSource
-from .PencilBeamSource import PencilBeamSource
-from .TemplateSource import TemplateSource
-from .PhaseSpaceSource import PhaseSpaceSource
-from ..helpers import make_builders, g4_units
-
-
-"""
-    List of source types: Generic, Voxels etc
-
-    Energy spectra for beta+ emitters
-"""
-
-source_type_names = {
-    GenericSource,
-    VoxelsSource,
-    GANSource,
-    GANPairsSource,
-    PencilBeamSource,
-    TemplateSource,
-    PhaseSpaceSource,
-}
-source_builders = make_builders(source_type_names)
 
 gate_source_path = pathlib.Path(__file__).parent.resolve()
 
