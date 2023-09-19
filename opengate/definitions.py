803bb7f6706f08a9300e387cb10db5e60314f9cc
Nils Krah <krah.nils@gmail.com>
2023-09-19 18:31:07 +0300

    move FLOAT_MAX and elements_name_symbol to definitions.py

--- opengate/definitions.py
+++ opengate/definitions.py
@@ -1,4 +1,31 @@
+import sys
+
 # This files contains definitions of common variables used throughout opengate
 
 """ Global name for the world volume"""
 __world_name__ = "world"
+
+
+FLOAT_MAX = sys.float_info.max
+
+
+# correspondence element names <> symbol
+elements_name_symbol = {
+    "Hydrogen": "H",
+    "Carbon": "C",
+    "Nitrogen": "N",
+    "Oxygen": "O",
+    "Sodium": "Na",
+    "Magnesium": "Mg",
+    "Phosphor": "P",
+    "Sulfur": "S",
+    "Chlorine": "Cl",
+    "Argon": "Ar",
+    "Potassium": "K",
+    "Calcium": "Ca",
+    "Titanium": "Ti",
+    "Copper": "Cu",
+    "Zinc": "Zn",
+    "Silver": "Ag",
+    "Tin": "Sn",
+}
