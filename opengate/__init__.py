7e7da068445eb14ca41e6b09a706410579e4a75f
Nils Krah <krah.nils@gmail.com>
2023-09-19 23:40:51 +0300

    add imports to /opengate/__ini__.py

--- opengate/__init__.py
+++ opengate/__init__.py
@@ -2,3 +2,18 @@
 # import them here as shortcuts
 # from .Simulation import Simulation
 # from .physics.Region import Region
+
+
+# __all__ = [
+#     'actor',
+#     'geometry',
+#     'physics',
+#     'source'
+# ]
+
+from .managers import Simulation
+
+import opengate.geometry
+import opengate.physics
+import opengate.sources
+import opengate.actors
