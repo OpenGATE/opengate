5d1895c758eb483352ffea21ab3ed81381a43e73
Nils Krah <krah.nils@gmail.com>
2023-09-19 15:24:42 +0300

    remove builder mechanism from helpers_actor.py

--- opengate/actor/helpers_actor.py
+++ opengate/actor/helpers_actor.py
@@ -1,42 +1,4 @@
-from .ARFActor import ARFActor
-from .ARFTrainingDatasetActor import ARFTrainingDatasetActor
-from .DoseActor import DoseActor
-from .LETActor import LETActor
-from .DigitizerAdderActor import DigitizerAdderActor
-from .DigitizerReadoutActor import DigitizerReadoutActor
-from .DigitizerHitsCollectionActor import DigitizerHitsCollectionActor
-from .DigitizerEnergyWindowsActor import DigitizerEnergyWindowsActor
-from .DigitizerProjectionActor import DigitizerProjectionActor
-from .DigitizerBlurringActor import DigitizerBlurringActor
-from .DigitizerSpatialBlurringActor import DigitizerSpatialBlurringActor
-from .DigitizerEfficiencyActor import DigitizerEfficiencyActor
-from .MotionVolumeActor import MotionVolumeActor
-from .PhaseSpaceActor import PhaseSpaceActor
-from .SimulationStatisticsActor import SimulationStatisticsActor
-from .SourceInfoActor import SourceInfoActor
-from .TestActor import TestActor
-from ..helpers import fatal, g4_units, make_builders
-
-actor_type_names = {
-    SimulationStatisticsActor,
-    DoseActor,
-    LETActor,
-    SourceInfoActor,
-    PhaseSpaceActor,
-    DigitizerHitsCollectionActor,
-    DigitizerAdderActor,
-    DigitizerEnergyWindowsActor,
-    DigitizerProjectionActor,
-    DigitizerReadoutActor,
-    DigitizerBlurringActor,
-    DigitizerSpatialBlurringActor,
-    DigitizerEfficiencyActor,
-    MotionVolumeActor,
-    ARFActor,
-    ARFTrainingDatasetActor,
-    TestActor,
-}
-actor_builders = make_builders(actor_type_names)
+from ..helpers import fatal, g4_units
 
 
 def get_simplified_digitizer_channels_Tc99m(spect_name, scatter_flag):
