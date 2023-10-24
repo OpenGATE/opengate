6e72248691e3ea6ef3e0b8330177ef163143c97e
Nils Krah <krah.nils@gmail.com>
2023-10-24 14:12:35 +0200

    Enhance decorators.py - add requires_attribute

--- opengate/decorators.py
+++ opengate/decorators.py
@@ -13,7 +13,7 @@ from .exception import fatal, warning
 # decorator for class methods which throws a fatal error when the class's attribute
 # physics_engine is None. Obviously, this could also be achieved via an if-statement
 # in the method. Using the decorator avoids boiler-plate code and increases readibility.
-def requires(attribute, mode="fatal"):
+def requires_not_none(attribute, mode="fatal"):
     def decorator(func):
         # @wraps decorator copies the doc_string, function name, etc.
         # of the method 'func' over to the wrapper '_with_check'.
@@ -33,15 +33,48 @@ def requires(attribute, mode="fatal"):
 
     decorator.attribute = attribute
     decorator.mode = mode
+    return decorator
 
+
+def requires_attribute(attribute, mode="fatal", msg=None):
+    def decorator(func):
+        # @wraps decorator copies the doc_string, function name, etc.
+        # of the method 'func' over to the wrapper '_with_check'.
+        # So: It informs python that 'func' is just wrapped,
+        # but should still be treated as if it were 'func'
+        @wraps(func)
+        def _with_check(self, *args, **kwargs):
+            if not hasattr(self, decorator.attribute):
+                if decorator.msg is None:
+                    msg = (
+                        f"Method {func.__name__} of class {type(self)} requires {decorator.attribute} to exist, "
+                        f"but it is part of this class."
+                    )
+                else:
+                    msg = decorator.msg
+                if decorator.mode == "fatal":
+                    fatal(msg)
+                elif decorator.mode == "warning":
+                    warning(msg)
+            return func(self, *args, **kwargs)
+
+        return _with_check
+
+    decorator.attribute = attribute
+    decorator.mode = mode
+    decorator.msg = msg
     return decorator
 
 
 # Different flavors of the "requires" decorator
 # with preset modes
 def requires_fatal(attribute):
-    return requires(attribute, mode="fatal")
+    return requires_not_none(attribute, mode="fatal")
 
 
 def requires_warning(attribute):
-    return requires(attribute, mode="warning")
+    return requires_not_none(attribute, mode="warning")
+
+
+def requires_attribute_fatal(attribute, msg=None):
+    return requires_attribute(attribute, mode="fatal", msg=msg)
