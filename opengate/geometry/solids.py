96bba8060a8879befb774e0c8ee40f8bc3975071
Nils Krah <krah.nils@gmail.com>
2024-01-12 16:14:30 +0100

    Implement ImageSolid.__getstate__()

--- opengate/geometry/solids.py
+++ opengate/geometry/solids.py
@@ -463,6 +463,13 @@ class ImageSolid(SolidBase):
         self.g4_solid_y = None
         self.g4_solid_z = None
 
+    def __getstate__(self):
+        return_dict = super().__getstate__()
+        return_dict["g4_solid_x"] = None
+        return_dict["g4_solid_y"] = None
+        return_dict["g4_solid_z"] = None
+        return return_dict
+
     def close(self):
         self.release_g4_references()
         super().close()
