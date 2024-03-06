fa78494ea50ce84727e85dac3b5b9743313bf66d
Nils Krah <krah.nils@gmail.com>
2024-03-06 14:49:09 +0100

    Extend ActorOutput

--- opengate/actors/actoroutput.py
+++ opengate/actors/actoroutput.py
@@ -2,6 +2,8 @@ from ..exception import warning, fatal
 from ..base import GateObject
 from ..image import sum_itk_images
 from pathlib import Path
+from ..image import write_itk_image
+from ..utility import ensure_filename_is_str
 
 
 def _setter_hook_belongs_to(self, belongs_to):
@@ -35,6 +37,12 @@ class ActorOutput(GateObject):
                 "set via the Simulation.output_path option. ",
             },
         ),
+        "write_to_disk": (
+            True,
+            {
+                "doc": "Should the data be written to disk?",
+            },
+        ),
         "keep_data_in_memory": (
             True,
             {
@@ -63,7 +71,7 @@ class ActorOutput(GateObject):
         # Store the user_info dictionary of the associated actor for convenience:
         self.actor_user_input = actor_user_input
 
-        self.data_per_run = None  # holds the data per run in memory
+        self.data_per_run = {}  # holds the data per run in memory
         self.merged_data = None  # holds the data merged from multiple runs in memory
 
     @property
@@ -77,11 +85,14 @@ class ActorOutput(GateObject):
             )
         return self.data_per_run[0]
 
-    def merge_data_from_runs(self):
+    def merge_data_runs(self):
         raise NotImplementedError(
             f"Your are calling this method from the base class {type(self).__name__}, "
             f"but it should be implemented in the specific derived class"
         )
+
+    def store_data(self, data, run_index=0):
+        self.data_per_run[run_index] = data
 
     def load_data(self):
         raise NotImplementedError(
@@ -89,19 +100,26 @@ class ActorOutput(GateObject):
             f"but it should be implemented in the specific derived class"
         )
 
+    def write_data(self):
+        raise NotImplementedError(
+            f"Your are calling this method from the base class {type(self).__name__}, "
+            f"but it should be implemented in the specific derived class"
+        )
+
     def get_output_path(self, run_index=None):
+        full_data_path = self.simulation.get_output_path(self.data_path)
         if run_index is None:
-            return self.data_path
+            return full_data_path
         else:
-            return self.data_path.with_name(
-                self.data_path.stem + f"_run{run_index:03f}" + self.data_path.suffix
+            return full_data_path.with_name(
+                full_data_path.stem + f"_run{run_index:03f}" + full_data_path.suffix
             )
 
     def close(self):
         if self.keep_data_per_run is False:
-            self.data_per_run = None
+            self.data_per_run = {}
         if self.keep_data_in_memory is False:
-            self.data_per_run = None
+            self.data_per_run = {}
             self.merged_data = None
         super().close()
 
@@ -117,9 +135,15 @@ class ActorOutputImage(ActorOutput):
         ),
     }
 
-    def merge_data_from_runs(self):
+    def merge_data_runs(self):
         if self.merge_method == "sum":
             self.merged_data = sum_itk_images(self.data_per_run)
+
+    def write_data(self):
+        if self.write_to_disk is True:
+            for i, image in self.data_per_run.items():
+                write_itk_image(image, ensure_filename_is_str(self.get_output_path(i)))
+        # FIXME: add code for merged data
 
 
 class ActorOutputRoot(ActorOutput):
@@ -133,6 +157,9 @@ class ActorOutputRoot(ActorOutput):
         ),
     }
 
-    def merge_data_from_runs(self):
+    def merge_data_runs(self):
         if self.merge_method == "append":
             raise NotImplementedError("Appending ROOT files not yet implemented.")
+
+
+actor_output_classes = {"root": ActorOutputRoot, "image": ActorOutputImage}
