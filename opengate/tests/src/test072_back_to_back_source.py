59200546affaeada46a2086cd221dda2e842eaa5
Maxime Toussaint <maxime.toussaint@usherbrooke.ca>
2024-04-01 15:29:58 -0400

    Pre-commit things

--- opengate/tests/src/test072_back_to_back_source.py
+++ opengate/tests/src/test072_back_to_back_source.py
@@ -1,9 +1,97 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#########################################################################################
+# Imports
+#########################################################################################
 import opengate as gate
 from opengate.tests import utility
 
+import uproot
+import numpy as np
+
+
+#########################################################################################
+# Simulations configuration that may be relevant to change
+#########################################################################################
+# Number of back-to-back to generate
+nbEvents = 100
+
+
+#########################################################################################
+# Constants
+#########################################################################################
+# Units
+MeV = gate.g4_units.MeV
+keV = gate.g4_units.keV
+Bq = gate.g4_units.Bq
+deg = gate.g4_units.deg
+mm = gate.g4_units.mm
+m = gate.g4_units.m
+cm = gate.g4_units.cm
+
+
+#########################################################################################
+# Methods used in this test
+#########################################################################################
+def test_backToBack(_pathToRootFile, _nbB2b):
+    """
+    Def.: zxc FIXME
+    """
+    # get data from root file
+    b2b_root = uproot.open(_pathToRootFile)["phsp;1"].arrays(library="numpy")
+
+    # Assuming that trackId is relative to a eventId, it provides use a way to check
+    # that everything start with two gammas.
+    # It does not take into account 3+ gamma cases but I do not know how to do better
+    b2b_eventId_adamEve = b2b_root["TrackID"] <= 2
+
+    b2b_emissionEnergy = b2b_root["EventKineticEnergy"][b2b_eventId_adamEve]
+
+    b2b_particleName = b2b_root["ParticleName"][b2b_eventId_adamEve]
+
+    b2b_dir = (
+        np.stack(
+            (
+                b2b_root["PreDirection_X"],
+                b2b_root["PreDirection_Y"],
+                b2b_root["PreDirection_Z"],
+            )
+        ).T
+    )[b2b_eventId_adamEve]
+    # For easier manipulation
+    b2b_dir = b2b_dir.reshape((_nbB2b, 2, 3))
+
+    # Note: We assumes that thing that are not specific to back-to-back were already
+    # tested, for example the number of particle
+    # FIXME Would be better to check fAccolinearityFlag but might not be accessible?
+    is_b2b = source.particle == "back_to_back"
+    is_monoEnergy = source.energy.type == "mono"
+    is_def511kev = source.energy.mono == 511 * keV
+    is_defNoAcolin = source.direction.accolinearity_flag == False
+    is_allB2b511keV = np.all(np.isclose(b2b_emissionEnergy, 0.511, atol=10**-3))
+    is_allEmissionGamme = np.all(b2b_particleName == "gamma")
+    is_b2bColin = np.all(
+        np.isclose(
+            np.sum(b2b_dir[:, 0, :] * b2b_dir[:, 1, :], axis=-1), -1.0, atol=10**-3
+        )
+    )
+    # FIXME: Confirm it stay in the sphere?
+
+    return (
+        is_b2b
+        & is_monoEnergy
+        & is_def511kev
+        & is_defNoAcolin
+        & is_allB2b511keV
+        & is_allEmissionGamme
+        & is_b2bColin
+    )
+
+
+#########################################################################################
+# Main : We use this to launch the test
+#########################################################################################
 if __name__ == "__main__":
     paths = utility.get_default_test_paths(__file__, output_folder="test072")
 
@@ -18,15 +106,6 @@ if __name__ == "__main__":
     sim.number_of_threads = 1
     # sim.random_seed = 123456
 
-    # useful units
-    MeV = gate.g4_units.MeV
-    keV = gate.g4_units.keV
-    Bq = gate.g4_units.Bq
-    deg = gate.g4_units.deg
-    mm = gate.g4_units.mm
-    m = gate.g4_units.m
-    cm = gate.g4_units.cm
-
     # set the world size like in the Gate macro
     world = sim.world
     world.size = [2 * m, 2 * m, 2 * m]
@@ -34,7 +113,7 @@ if __name__ == "__main__":
     # test sources
     source = sim.add_source("GenericSource", "b2b")
     source.particle = "back_to_back"
-    source.n = 100
+    source.n = nbEvents
     source.position.type = "sphere"
     source.position.radius = 5 * mm
     source.direction.type = "iso"
@@ -46,12 +125,14 @@ if __name__ == "__main__":
     stats_actor = sim.add_actor("SimulationStatisticsActor", "Stats")
 
     # store phsp
-    phsp = sim.add_actor("PhaseSpaceActor", "phsp")
-    phsp.attributes = [
+    phsp_actor = sim.add_actor("PhaseSpaceActor", "phsp")
+    phsp_actor.attributes = [
         "EventID",
         "TrackID",
         "EventPosition",
         "EventDirection",
+        "Direction",
+        "EventKineticEnergy",
         "KineticEnergy",
         "ParticleName",
         "TimeFromBeginOfEvent",
@@ -59,9 +140,9 @@ if __name__ == "__main__":
         "LocalTime",
         "PDGCode",
         "PostPosition",
-        "PostDirection",
+        "PreDirection",
     ]
-    phsp.output = paths.output / "b2b.root"
+    phsp_actor.output = paths.output / "b2b.root"
 
     # verbose
     # sim.g4_verbose = True
@@ -73,10 +154,14 @@ if __name__ == "__main__":
     # start simulation
     sim.run()
 
-    # get results
+    # get results FIXME I might need to do something with this?
     stats = sim.output.get_actor("Stats")
-    print(stats)
+    # print(stats)
 
-    # FIXME : test something
-    is_ok = False
-    utility.test_ok(is_ok)
+    is_ok = test_backToBack(phsp_actor.output, nbEvents)
+    # FIXME confirm acolin when activated
+    # FIXME Other tests?
+
+    # zxc: Hack to see print
+    print("!!!!!!!!!!!!!!! Do all tests passes?", is_ok)
+    utility.test_ok(False)
