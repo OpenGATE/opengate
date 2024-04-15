870c274e4f71ee0705b141b6e84e2609c6b763c1
Nils Krah <krah.nils@gmail.com>
2024-04-15 15:33:18 +0200

    Refactor user info in DigitizerEfficiencyActor

--- opengate/actors/digitizers.py
+++ opengate/actors/digitizers.py
@@ -571,6 +571,9 @@ class DigitizerSpatialBlurringActor(g4.GateDigitizerSpatialBlurringActor, ActorB
         if self.blur_sigma is None:
             fatal(f"Error, use blur_sigma or blur_fwhm")
 
+    def initialize(self):
+        self.initialize_blurring_parameters()
+
     def StartSimulationAction(self):
         g4.GateDigitizerSpatialBlurringActor.StartSimulationAction(self)
 
@@ -583,35 +586,69 @@ class DigitizerEfficiencyActor(g4.GateDigitizerEfficiencyActor, ActorBase):
     Digitizer module for simulating efficiency.
     """
 
+    user_info_defaults = {
+        "attributes": (
+            [],
+            {
+                "doc": "Attributes to be considered. ",
+            },
+        ),
+        "output": (
+            "singles.root",
+            {
+                "doc": "FIXME",
+            },
+        ),
+        "input_digi_collection": (
+            "Hits",
+            {
+                "doc": "FIXME",
+            },
+        ),
+        "skip_attributes": (
+            [],
+            {
+                "doc": "FIXME",
+            },
+        ),
+        "clear_every": (
+            1e5,
+            {
+                "doc": "FIXME",
+            },
+        ),
+        "efficiency": (
+            1.0,
+            {
+                "doc": "FIXME",
+            },
+        ),
+    }
+
     type_name = "DigitizerEfficiencyActor"
 
-    @staticmethod
-    def set_default_user_info(user_info):
-        ActorBase.set_default_user_info(user_info)
-        user_info.attributes = []
-        user_info.output = "efficiency.root"
-        user_info.input_digi_collection = "Hits"
-        user_info.skip_attributes = []
-        user_info.clear_every = 1e5
-        user_info.efficiency = 1.0  # keep everything
+    # @staticmethod
+    # def set_default_user_info(user_info):
+    #     ActorBase.set_default_user_info(user_info)
+    #     user_info.attributes = []
+    #     user_info.output = "efficiency.root"
+    #     user_info.input_digi_collection = "Hits"
+    #     user_info.skip_attributes = []
+    #     user_info.clear_every = 1e5
+    #     user_info.efficiency = 1.0  # keep everything
 
-    def __init__(self, user_info):
-        # check and adjust parameters
-        self.set_param(user_info)
+    def __init__(self, *args, **kwargs):
         # base classes
-        ActorBase.__init__(self, user_info)
-        g4.GateDigitizerEfficiencyActor.__init__(self, user_info.__dict__)
-        actions = {"StartSimulationAction", "EndSimulationAction"}
-        self.AddActions(actions)
+        ActorBase.__init__(self, *args, **kwargs)
+        g4.GateDigitizerEfficiencyActor.__init__(self, self.user_info)
+        self.AddActions({"StartSimulationAction", "EndSimulationAction"})
 
-    def set_param(self, user_info):
-        efficiency = user_info.efficiency
-        if not (0.0 <= efficiency <= 1.0):
-            warning(f"Efficency set to {efficiency}, which is not in [0;1].")
+    def initialize_blurring_parameters(self):
+        if not (0.0 <= self.efficiency <= 1.0):
+            warning(f"Efficency set to {self.efficiency}, which is not in [0;1].")
 
-    def __str__(self):
-        s = f"DigitizerEfficiencyActor {self.user_info.name}"
-        return s
+    def initialize(self):
+        self.initialize_blurring_parameters()
 
     def StartSimulationAction(self):
         g4.GateDigitizerEfficiencyActor.StartSimulationAction(self)
