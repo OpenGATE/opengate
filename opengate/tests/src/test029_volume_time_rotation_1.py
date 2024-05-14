4ef1be78261bdc473945dc454d472399ab00ad51
Nils Krah <krah.nils@gmail.com>
2024-05-14 23:00:43 +0200

    Update test029_volume_time_rotation_1.py to refactored actors

--- opengate/tests/src/test029_volume_time_rotation_1.py
+++ opengate/tests/src/test029_volume_time_rotation_1.py
@@ -16,6 +16,10 @@ if __name__ == "__main__":
     # create sim without AA
     test029.create_simulation(sim, False, paths)
 
+    # for later reference, get the actors that were created by the helper function above
+    proj_actor = sim.actor_manager.get_actor('Projection')
+    stats = sim.actor_manager.get_actor('Stats')
+
     # initialize & start
     sim.run()
 
@@ -31,7 +35,6 @@ if __name__ == "__main__":
 
     # -------------------------
     gate.exception.warning("Compare stats")
-    stats = sim.output.get_actor("Stats")
     print(stats)
     stats_ref = utility.read_stat_file(paths.output_ref / "stats029.txt")
     print(
@@ -46,7 +49,7 @@ if __name__ == "__main__":
     is_ok = (
         utility.assert_images(
             paths.output_ref / "proj029.mhd",
-            paths.output / "proj029.mhd",
+            proj_actor.get_output_path(),  # get the path by asking the actor; better than hard-code the path
             stats,
             tolerance=59,
             ignore_value=0,
