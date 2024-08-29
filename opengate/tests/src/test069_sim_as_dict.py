99b9a104ca6e9b01dc1b651cc1aa01d26767ea64
David <david.sarrut@gmail.com>
2024-08-29 14:36:06 +0200

    there is no test069, renaming those two

--- opengate/tests/src/test069_sim_as_dict.py
+++ opengate/tests/src/test069_sim_as_dict.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
     # store a json archive
     sim.store_json_archive = True
     sim.store_input_files = True
-    sim.json_archive_filename = "simu_test065.json"
-    sim.output_dir = paths.output / "test065"
+    sim.json_archive_filename = "simu_test069.json"
+    sim.output_dir = paths.output / "test069"
 
     # add a material database
     sim.volume_manager.add_material_database(
@@ -100,8 +100,8 @@ if __name__ == "__main__":
     sim.run()
 
     # test the file content -> NO, there are some abs filenames ...
-    fn1 = paths.output / "test065" / sim.json_archive_filename
-    """fn2 = paths.output_ref / "test065" / sim.json_archive_filename
+    fn1 = paths.output / "test069" / sim.json_archive_filename
+    """fn2 = paths.output_ref / "test069" / sim.json_archive_filename
     f1 = open(fn1)
     j1 = json.load(f1)
     f2 = open(fn2)
--- opengate/tests/src/test069_sim_from_dict.py
+++ opengate/tests/src/test069_sim_from_dict.py
@@ -7,7 +7,7 @@ import pathlib
 import os
 import subprocess
 
-# This test is to be run after test065_sim_as_dict.py
+# This test is to be run after test069_sim_as_dict.py
 # It checks whether the simulation is recreated correctly from the JSON file,
 # but does not actually run any simulation.
 
@@ -15,20 +15,20 @@ if __name__ == "__main__":
     pathFile = pathlib.Path(__file__).parent.resolve()
     paths = utility.get_default_test_paths(__file__)
 
-    # the test065_sim_as_dict.py is needed first
-    if not os.path.isfile(paths.output / "simu_test065.json"):
-        print(f"Running test065_sim_as_dict.py")
-        subprocess.call(["python", paths.current / "test065_sim_as_dict.py"])
+    # the test069_sim_as_dict.py is needed first
+    if not os.path.isfile(paths.output / "simu_test069.json"):
+        print(f"Running test069_sim_as_dict.py")
+        subprocess.call(["python", paths.current / "test069_sim_as_dict.py"])
 
     # create the simulation
-    sim = gate.create_sim_from_json(paths.output / "test065" / "simu_test065.json")
+    sim = gate.create_sim_from_json(paths.output / "test069" / "simu_test069.json")
 
     # Assert that objects have been read back correctly
     assert "rod" in sim.volume_manager.volumes
     m = gate.g4_units.m
     assert sim.volume_manager.world_volume.size == [1.5 * m, 1.5 * m, 1.5 * m]
     assert "rod_region" in sim.physics_manager.regions
-    assert pathlib.Path(paths.output / "test065" / "patient-4mm.mhd").exists()
+    assert pathlib.Path(paths.output / "test069" / "patient-4mm.mhd").exists()
     assert (
         sim.volume_manager.get_volume("waterbox_with_hole").creator_volumes[0].name
         == "Waterbox"
