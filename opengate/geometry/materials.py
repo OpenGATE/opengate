1784b9c088e4185d6ade3391422204746cf03212
Nils Krah <krah.nils@gmail.com>
2024-08-29 02:11:06 +0200

    Fix and improve materials.py: call open(...) via with ... as structure to make sure no filehandle remains dangling

--- opengate/geometry/materials.py
+++ opengate/geometry/materials.py
@@ -11,25 +11,25 @@ from ..definitions import elements_name_symbol
 
 def read_voxel_materials(filename, def_mat="G4_AIR"):
     p = os.path.abspath(filename)
-    f = open(p, "r")
     current = 0
     materials = []
-    for line in f:
-        for word in line.split():
-            if word[0] == "#":
-                break
-            if current == 0:
-                start = float(word)
-                current = 1
-            else:
-                if current == 1:
-                    stop = float(word)
-                    current = 2
+    with open(p, "r") as f:
+        for line in f:
+            for word in line.split():
+                if word[0] == "#":
+                    break
+                if current == 0:
+                    start = float(word)
+                    current = 1
                 else:
-                    if current == 2:
-                        mat = word
-                        current = 0
-                        materials.append([start, stop, mat])
+                    if current == 1:
+                        stop = float(word)
+                        current = 2
+                    else:
+                        if current == 2:
+                            mat = word
+                            current = 0
+                            materials.append([start, stop, mat])
 
     # sort according to starting interval
     materials = sorted(materials)
@@ -58,56 +58,56 @@ def read_voxel_materials(filename, def_mat="G4_AIR"):
 
 def HU_read_materials_table(file_mat):
     p = os.path.abspath(file_mat)
-    f = open(p, "r")
     elements = ["HU"]
     materials = []
     current_section = None
     current_material = None
-    for line in f:
-        i = 0
-        for word in line.split():
-            if word[0] == "#":
-                break
-            if word == "[Elements]":
-                current_section = "element"
-                break
-            if word == "[/Elements]":
-                elements.append("name")
-                current_section = "table"
-                break
-            if current_section is None:
-                break
-            if current_section == "element":
-                elements.append(word)
-            if current_section == "table":
-                if current_material is None:
-                    current_material = {}
-                if i == 0:
-                    current_material[elements[i]] = int(word)
-                else:
-                    if i == len(elements) - 1:
-                        current_material[elements[i]] = word
+    with open(p, "r") as f:
+        for line in f:
+            i = 0
+            for word in line.split():
+                if word[0] == "#":
+                    break
+                if word == "[Elements]":
+                    current_section = "element"
+                    break
+                if word == "[/Elements]":
+                    elements.append("name")
+                    current_section = "table"
+                    break
+                if current_section is None:
+                    break
+                if current_section == "element":
+                    elements.append(word)
+                if current_section == "table":
+                    if current_material is None:
+                        current_material = {}
+                    if i == 0:
+                        current_material[elements[i]] = int(word)
                     else:
-                        current_material[elements[i]] = float(word)
-            i += 1
-        if current_material:
-            materials.append(current_material)
-        current_material = None
+                        if i == len(elements) - 1:
+                            current_material[elements[i]] = word
+                        else:
+                            current_material[elements[i]] = float(word)
+                i += 1
+            if current_material:
+                materials.append(current_material)
+            current_material = None
     return materials, elements
 
 
 def HU_read_density_table(file_density):
     p = os.path.abspath(file_density)
-    f = open(p, "r")
     densities = []
-    for line in f:
-        words = line.split()
-        if len(words) < 1:
-            continue
-        if words[0][0] == "#":
-            continue
-        d = {"HU": int(words[0]), "density": float(words[1])}
-        densities.append(d)
+    with open(p, "r") as f:
+        for line in f:
+            words = line.split()
+            if len(words) < 1:
+                continue
+            if words[0][0] == "#":
+                continue
+            d = {"HU": int(words[0]), "density": float(words[1])}
+            densities.append(d)
     return densities
 
 
@@ -156,7 +156,7 @@ def HounsfieldUnit_to_material(simulation, density_tolerance, file_mat, file_den
     created_materials = []
     gcm3 = g4_units.g_cm3
 
-    elems = elements[1 : len(elements) - 1]
+    elems = elements[1:len(elements) - 1]
     elems_symbol = [elements_name_symbol[x] for x in elems]
 
     i = 0
@@ -655,12 +655,12 @@ class MaterialDatabase:
         self.current_filename = filename
         self.element_builders_by_filename[self.current_filename] = {}
         self.material_builders_by_filename[self.current_filename] = {}
-        f = open(filename, "r")
-        line = f.readline()
-        while line:
-            line = line.strip().replace("\t", " ")
-            self.read_one_item(f, line)
+        with open(filename, "r") as f:
             line = f.readline()
+            while line:
+                line = line.strip().replace("\t", " ")
+                self.read_one_item(f, line)
+                line = f.readline()
 
     def read_one_item(self, f, line):
         # skip empty lines
