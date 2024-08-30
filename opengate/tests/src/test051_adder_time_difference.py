0a8327d8d64ad3016a60cae00a2d6836c2b2efbe
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2024-08-30 14:34:55 +0200

    test051 is ok

--- opengate/tests/src/test051_adder_time_difference.py
+++ opengate/tests/src/test051_adder_time_difference.py
@@ -48,8 +48,8 @@ if __name__ == "__main__":
 
     # spect digitizer
     hc = sim.add_actor("DigitizerHitsCollectionActor", f"Hits")
-    hc.mother = crystal.name
-    hc.output = ""  # No output
+    hc.attached_to = crystal.name
+    hc.output_filename = ""  # No output
     hc.attributes = [
         "PostPosition",
         "TotalEnergyDeposit",
@@ -57,11 +57,11 @@ if __name__ == "__main__":
         "GlobalTime",
     ]
     sc = sim.add_actor("DigitizerAdderActor", f"Singles")
-    sc.mother = hc.mother
+    sc.attached_to = hc.attached_to
     sc.input_digi_collection = hc.name
     sc.time_difference = True
     sc.number_of_hits = True
-    sc.output = paths.output / "test051_singles.root"
+    sc.output_filename = paths.output / "test051_singles.root"
 
     # physics
     sim.physics_manager.physics_list_name = "G4EmStandardPhysics_option3"
@@ -79,9 +79,9 @@ if __name__ == "__main__":
     s1.activity = 1e4 * Bq / sim.number_of_threads
 
     # add stat actor
-    s = sim.add_actor("SimulationStatisticsActor", "stats")
-    s.track_types_flag = True
-    s.output = paths.output / "test051_stats.txt"
+    stats = sim.add_actor("SimulationStatisticsActor", "stats")
+    stats.track_types_flag = True
+    stats.output_filename = paths.output / "test051_stats.txt"
 
     # go
     sim.run(start_new_process=True)
@@ -89,7 +89,6 @@ if __name__ == "__main__":
     # check stats
     print()
     gate.exception.warning("Check stats")
-    stats = sim.get_actor("stats")
     stats_ref = utility.read_stat_file(paths.output_ref / "test051_stats.txt")
     is_ok = utility.assert_stats(stats, stats_ref, tolerance=0.05)
 
@@ -99,7 +98,7 @@ if __name__ == "__main__":
     root1, n1 = utility.open_root_as_np(
         paths.output_ref / "test051_singles.root", "Singles"
     )
-    root2, n2 = utility.open_root_as_np(sc.output, "Singles")
+    root2, n2 = utility.open_root_as_np(sc.get_output_path(), "Singles")
 
     # time difference
     td1 = root1["TimeDifference"]
