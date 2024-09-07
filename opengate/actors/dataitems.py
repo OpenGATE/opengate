ec47757ae3d9dae3248b69ca7c64ad102f119b06
Nils Krah <krah.nils@gmail.com>
2024-09-08 01:44:04 +0200

    fix DataItemContainer.set_data()

--- opengate/actors/dataitems.py
+++ opengate/actors/dataitems.py
@@ -398,6 +398,7 @@ class DataItemContainer(DataContainer):
                 fatal(f"Inconsistent input to set_data method: "
                       f"{len(data)} data items provided, "
                       f"but {len(item)} items specified in the 'item' keyword argument. ")
+        else:
             item = [i for i in range(len(data))]
         processed_data = []
         for i, d in zip(item, data):
