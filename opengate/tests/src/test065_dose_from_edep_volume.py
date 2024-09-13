e907a415ff7d9aa3d62eb83161cc40c75e6aa686
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2024-09-13 12:31:22 +0200

    set output in a subfolder

--- opengate/tests/src/test065_dose_from_edep_volume.py
+++ opengate/tests/src/test065_dose_from_edep_volume.py
@@ -8,10 +8,10 @@ from opengate.contrib.beamlines.ionbeamline import BeamlineModel
 from opengate.contrib.tps.ionbeamtherapy import spots_info_from_txt
 
 if __name__ == "__main__":
-    paths = utility.get_default_test_paths(__file__, "gate_test044_pbs")
+    paths = utility.get_default_test_paths(__file__, "gate_test044_pbs", "test065_dose_from_edep_volume")
 
     output_path = paths.output / "output_test059_rtp"
-    ref_path = paths.output_ref / "test059"
+    ref_path = paths.output_ref / '..' / "test059"
 
     # create output dir, if it doesn't exist
     output_path.mkdir(parents=True, exist_ok=True)
