687669b793005d2f5a057312a4ed58352bae43c5
Nils Krah <krah.nils@gmail.com>
2024-09-27 21:42:37 +0200

    Start implementing test079_check_initcpp_exists.py

---
+++ opengate/tests/src/test079_check_initcpp_exists.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from opengate.devtools import check_classes_in_current_package
+from opengate.base import GateObject
+from opengate.exception import GateImplementationError
+
+if __name__ == "__main__":
+    warnings = check_classes_in_current_package('__initcpp__', package_name='opengate',
+                                                sub_package_name='actors', inherits_from='opengate.actors.base.ActorBase')
+    print(warnings)
+    if len(warnings):
+        raise GateImplementationError("Some GateObjects do not implement a '__finalize_init__' method: \n"
+                                      f"{warnings}")
