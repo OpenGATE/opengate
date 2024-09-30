0b553acf5b12e2d6156dfefde4fe788f7007689b
Nils Krah <krah.nils@gmail.com>
2024-09-30 12:17:30 +0200

    In devtools.py: split class crawling and class check; improve code

--- opengate/devtools.py
+++ opengate/devtools.py
@@ -2,15 +2,47 @@ import inspect
 import pkgutil
 import importlib
 
-from .base import GateObject
+
+def get_attribute_type(attribute):
+    if callable(attribute):
+        return "method"
+    elif isinstance(attribute, property):
+        return "property"
+    else:
+        return "plain"
 
 
-def check_classes_in_current_package(
-    attribute_name,
+def check_if_class_has_attribute(cls, attribute_name=None, attribute_type=None):
+    """Check if the class has the desired attribute
+    """
+    if attribute_name is None:
+        raise ValueError("kwarg 'attribute_name' is required. ")
+    warning = None
+    if hasattr(cls, attribute_name):
+        attribute = getattr(cls, attribute_name)
+        if attribute_type is not None:
+            found_attribute_type = get_attribute_type(attribute)
+            if found_attribute_type != attribute_type:
+                base_msg = (
+                    f"Class {cls.__name__} in module {cls.__module__} "
+                    f"has the attribute '{attribute_name}', "
+                    "but it is not a "
+                )
+                if found_attribute_type in ("method", "property"):
+                    warning = base_msg + f"{attribute_type}."
+                elif found_attribute_type == "plain":
+                    warning = base_msg + "plain attribute."
+    else:
+        warning = (f"Class {cls.__name__} in module {cls.__module__} "
+                   f"does NOT have the attribute '{attribute_name}'.")
+    return warning
+
+def apply_class_check_to_package(
+    check_func,
     package_name=None,
-    attribute_type=None,
     sub_package_name=None,
     inherits_from=None,
+    func_kwargs=None
 ):
     """
     Checks for the presence of a certain attribute type (attribute, property, or method)
@@ -22,6 +54,8 @@ def check_classes_in_current_package(
     :param inherits_from: Restrict the chck to classes that inherit from the class provided as inherits_from.
         Needs to be a qualified name, e.g. inherits_from=opengate.actors.ActorBase
     """
+    if func_kwargs is None:
+        func_kwargs = {}
     if package_name is None:
         # Get the current package's name
         package_name = __package__
@@ -63,33 +97,7 @@ def check_classes_in_current_package(
                 if inspect.isclass(obj):
                     if inherits_from is not None and instance_of_class not in obj.mro():
                         continue
-                    # Check if the class has the desired attribute
-                    if hasattr(obj, attribute_name):
-                        attribute = getattr(obj, attribute_name)
-                        if attribute_type is not None:
-                            found_attribute_type = get_attribute_type(attribute)
-                            if found_attribute_type != attribute_type:
-                                base_msg = (
-                                    f"Class {obj.__name__} in module {module_name} "
-                                    f"has the attribute '{attribute_name}', "
-                                    "but it is not a "
-                                )
-                                if found_attribute_type in ("method", "property"):
-                                    warnings.append(base_msg + f"{attribute_type}.")
-                                elif found_attribute_type == "plain":
-                                    warnings.append(base_msg + "plain attribute.")
-                    else:
-                        warnings.append(
-                            f"Class {obj.__name__} in module {module_name} "
-                            f"does NOT have the attribute '{attribute_name}'."
-                        )
+                    w = check_func(obj, **func_kwargs)
+                    if w is not None:
+                        warnings.append(w)
     return warnings
-
-
-def get_attribute_type(attribute):
-    if callable(attribute):
-        return "method"
-    elif isinstance(attribute, property):
-        return "property"
-    else:
-        return "plain"
