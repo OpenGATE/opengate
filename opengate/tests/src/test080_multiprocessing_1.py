a7434e55d4d3012ffd2537281cadd359cbea648a
Nils Krah <krah.nils@gmail.com>
2024-10-10 01:49:36 +0200

    Add test080_multiprocessing_1.py

---
+++ opengate/tests/src/test080_multiprocessing_1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from opengate.utility import g4_units
+import opengate as gate
+from opengate.tests.utility import get_default_test_paths
+
+
+
+if __name__ == "__main__":
+    paths = get_default_test_paths(
+        __file__, output_folder="test080"
+    )
+
+    s = g4_units.s
+
+    sim = gate.Simulation()
+    sim.run_timing_intervals = [[0 * s, 1 * s], [1 * s, 3 * s], [10 * s, 15 * s]]
+    sim.output_dir = paths.output
+
+    box1 = sim.add_volume('BoxVolume', 'box1')
+    box1.add_dynamic_parametrisation(translation=[[i, i, i] for i in range(len(sim.run_timing_intervals))])
+
+    n_proc = 4 * len(sim.run_timing_intervals)
+    run_timing_interval_map = sim.generate_run_timing_interval_map(n_proc)
+    print(run_timing_interval_map)
+
+    output = sim.run(number_of_sub_processes=n_proc)
+
+
+    print("*** output ***")
+    for o in output:
+        print(o)
+
+    print(f"ID of the main sim: {id(sim)}")
+
+    ids = [e[2] for e in output]
+    assert id(sim) not in ids
+
+
