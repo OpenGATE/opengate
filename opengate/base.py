dcebecc95d1863a6049c778eb97927969c6cb684
Nils Krah <krah.nils@gmail.com>
2024-10-13 02:36:49 +0200

    Update GateObject to rename property 'warnings' in Simulation

--- opengate/base.py
+++ opengate/base.py
@@ -642,7 +642,7 @@ class GateObject:
             self._temporary_warning_cache.append(message)
         # if possible, register the warning directly
         else:
-            self.simulation._user_warnings.append(message)
+            self.simulation.warnings.append(message)
         warning(message)
 
 
