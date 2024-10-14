956be1cbf90b9a0165365ce6ab4fc1486276ca4c
Nils Krah <krah.nils@gmail.com>
2024-10-14 08:47:05 +0200

    Implement explicit inplace_merge_with in ItkImageDataItem

--- opengate/actors/dataitems.py
+++ opengate/actors/dataitems.py
@@ -268,6 +268,12 @@ class ItkImageDataItem(DataItem):
             self.set_data(divide_itk_images(self.data, other.data))
         return self
 
+    def inplace_merge_with(self, *other):
+        data_to_merge = [self.data] + [o.data for o in other]
+        if self.data is not None:
+            data_to_merge += [self.data]
+        self.data = sum_itk_images(data_to_merge)
+
     def set_image_properties(self, **properties):
         if not self.data_is_none:
             if "spacing" in properties and properties["spacing"] is not None:
