92bb82f8388b11fd0d66c35d232b689ce2164190
Nils Krah <krah.nils@gmail.com>
2024-10-18 03:53:08 +0200

    Update test023_filters_iec_phantom.py to changes in assert_images()

--- opengate/tests/src/test023_filters_iec_phantom.py
+++ opengate/tests/src/test023_filters_iec_phantom.py
@@ -87,6 +87,8 @@ if __name__ == "__main__":
         stat,
         tolerance=102,
         sum_tolerance=28,
+        ignore_value_data2=0,
+        apply_ignore_mask_to_sum_check=False  # force legacy behavior
     )
 
     utility.test_ok(is_ok)
