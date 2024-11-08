1ae349369eefd61fbe23b5090059aa4283eeb34f
majacquet <maxime.jacquet@creatis.insa-lyon.fr>
2024-11-08 14:23:31 +0100

    Add C++ code of the kill actor

--- core/opengate_core/opengate_core.cpp
+++ core/opengate_core/opengate_core.cpp
@@ -309,6 +309,8 @@ void init_GateARFTrainingDatasetActor(py::module &m);
 
 void init_GateKillActor(py::module &);
 
+void init_GateKillNonInteractingParticleActor(py::module &);
+
 void init_itk_image(py::module &);
 
 void init_GateImageNestedParameterisation(py::module &);
@@ -586,6 +588,7 @@ PYBIND11_MODULE(opengate_core, m) {
   init_GateARFActor(m);
   init_GateARFTrainingDatasetActor(m);
   init_GateKillActor(m);
+  init_GateKillNonInteractingParticleActor(m);
   init_GateDigiAttributeManager(m);
   init_GateVDigiAttribute(m);
   init_GateExceptionHandler(m);
