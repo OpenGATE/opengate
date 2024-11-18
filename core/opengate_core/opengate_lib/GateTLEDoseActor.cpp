2c8a2942a24ee36064501d06a0172ecf100b3ed3
majacquet <maxime.jacquet@creatis.insa-lyon.fr>
2024-11-18 17:52:53 +0100

    Correction of TLE mechanism

--- core/opengate_core/opengate_lib/GateTLEDoseActor.cpp
+++ core/opengate_core/opengate_lib/GateTLEDoseActor.cpp
@@ -37,50 +37,88 @@ void GateTLEDoseActor::InitializeUserInfo(py::dict &user_info) {
   fMaterialMuHandler = GateMaterialMuHandler::GetInstance(database, fEnergyMax);
 }
 
+
+void GateTLEDoseActor::BeginOfEventAction(const G4Event* event) {
+  auto &l = fThreadLocalData.Get();
+  l.fIsTLESecondary = false;
+  l.fSecNbWhichDeposit.clear();
+  GateDoseActor::BeginOfEventAction(event);
+
+
+}
 void GateTLEDoseActor::PreUserTrackingAction(const G4Track *track) {
   auto &l = fThreadLocalData.Get();
+
+
   if (track->GetDefinition()->GetParticleName() == "gamma") {
     l.fIsTLEGamma = false;
-    l.fLastTrackId = 1;
-  } else {
-    auto track_id = track->GetTrackID();
-    if (track_id < l.fLastTrackId) {
-      // if the track_id is lower than the lastTrack, it means all the following
-      // tracks will be without TLE mode (tracks are processed LIFO), so we
-      // reset fLastTrackId to allow all tertiary (or more) tracks
-      l.fIsTLEGamma = false;
-      l.fLastTrackId = 1;
+    l.fIsTLESecondary = false;
+    l.fSecNbWhichDeposit[track->GetTrackID()] = 0;
+  }
+
+
+  //if the particle
+  else {
+    auto parent_id = track->GetParentID();
+    //std::cout<<"begin"<<std::endl;
+    for (auto it = l.fSecNbWhichDeposit.begin(); it != l.fSecNbWhichDeposit.end(); ++it) {
+      //std::cout <<it->first<<"  "<<it->second<<std::endl;
+        if (parent_id  == it->first){
+          if (it->second > 0){
+            l.fIsTLESecondary = true;
+          }
+          if (it->second == 0){
+            l.fSecNbWhichDeposit.erase(it);
+            l.fIsTLESecondary = false;
+            break;
+          }
+          it->second --;
+        }
     }
+    //std::cout<<"end"<<std::endl;
   }
 }
 
+
 void GateTLEDoseActor::SteppingAction(G4Step *step) {
   auto &l = fThreadLocalData.Get();
-  // Update the track id if there is secondary in the current non TLE particle
-  if (!l.fIsTLEGamma)
-    l.fLastTrackId += step->GetSecondaryInCurrentStep()->size();
 
-  // For non-gamma particle, no TLE
-  if (step->GetTrack()->GetDefinition()->GetParticleName() != "gamma") {
-    auto track_id = step->GetTrack()->GetTrackID();
-    if (track_id > l.fLastTrackId) {
-      // If the current track id is larger than the stored one: this is a
-      // track from a TLE gamma, so we ignore all other particles.
-      return;
-    } else {
-      // If not, the track count as a conventional DoseActor
+  auto pre_step = step->GetPreStepPoint();
+  double energy = 0;
+  if (pre_step !=0)
+    energy =pre_step->GetKineticEnergy();
+  if (step->GetTrack()->GetDefinition()->GetParticleName() == "gamma") {
+
+  // For too high energy, no TLE
+    if (energy > fEnergyMax) {
+      l.fIsTLEGamma = false;
+      l.fIsTLESecondary = false;
       return GateDoseActor::SteppingAction(step);
+   }
+    else {
+      l.fIsTLEGamma = true;
+    }
+  // Update the number of secondaries if there is secondary in the current non TLE particle
+    if (l.fIsTLEGamma==true){
+      auto nbSec = step->GetSecondaryInCurrentStep()->size();
+      if (nbSec > 0){
+        l.fIsTLESecondary = true;
+        l.fSecNbWhichDeposit[step->GetTrack()->GetTrackID()] += nbSec;
+      }
+      //l.fLastTrackId += step->GetSecondaryInCurrentStep()->size();
     }
   }
 
-  auto pre_step = step->GetPreStepPoint();
-  double energy = pre_step->GetKineticEnergy();
 
-  // For too high energy, no TLE
-  if (energy > fEnergyMax) {
+
+  // For non-gamma particle, no TLE
+  if (step->GetTrack()->GetDefinition()->GetParticleName() != "gamma") {
+    if (l.fIsTLESecondary == true){
+      return;
+    }
     return GateDoseActor::SteppingAction(step);
   }
-  l.fIsTLEGamma = true;
+
 
   auto weight = step->GetTrack()->GetWeight();
   auto step_length = step->GetStepLength();
@@ -103,11 +141,24 @@ void GateTLEDoseActor::SteppingAction(G4Step *step) {
   bool isInside;
   Image3DType::IndexType index;
   GetVoxelPosition(step, position, isInside, index);
+  auto event_id =
+      G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
   if (isInside) {
     G4AutoLock mutex(&SetPixelTLEMutex); // mutex is bound to the if-scope
     if (fDoseFlag) {
       ImageAddValue<Image3DType>(cpp_dose_image, index, dose);
     }
     ImageAddValue<Image3DType>(cpp_edep_image, index, edep);
+
+    if (fEdepSquaredFlag || fDoseSquaredFlag) {
+      if (fEdepSquaredFlag) {
+        ScoreSquaredValue(fThreadLocalDataEdep.Get(), cpp_edep_squared_image,
+                          edep, event_id, index);
+      }
+      if (fDoseSquaredFlag) {
+        ScoreSquaredValue(fThreadLocalDataDose.Get(), cpp_dose_squared_image,
+                          dose, event_id, index);
+      }
+    }
   }
 }
