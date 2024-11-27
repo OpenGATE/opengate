4936f3b7fc68091cb15665d0c4e2b84d43db67e8
Alex Vergara Gil <alex.vergara@biomediqa.com>
2024-11-27 15:40:20 +0100

    Add icrp107 database and functionalities

--- opengate/sources/base.py
+++ opengate/sources/base.py
@@ -3,6 +3,7 @@ import os
 import pathlib
 import numpy as np
 import json
+import re
 
 from ..actors.base import _setter_hook_attached_to
 from ..base import GateObject, process_cls
@@ -23,6 +24,44 @@ all_beta_plus_radionuclides = [
     "O15",
     "Rb82",
 ]
+
+icrp107_emissions = [
+    "alpha",
+    "beta-",
+    "beta+",
+    "gamma",
+    "X",
+    "neutron",
+    "auger",
+    "IE",
+    "alpha recoil",
+    "anihilation",
+    "fission",
+    "betaD",
+    "b-spectra",
+]
+
+
+def gate_radname_to_icrp107(rad_name: str) -> str:
+    """
+    Convert a radionuclide name from GATE to ICRP 107 format.
+
+    GATE format is for example "F18" and ICRP 107 format is "F-18"
+
+    Parameters
+    ----------
+    rad_name : str
+        radionuclide name in GATE format
+
+    Returns
+    -------
+    str
+        radionuclide name in ICRP 107 format
+    """
+    at_num = re.findall(r"\d+", rad_name)[0]  # Extract atomic number
+    elem = rad_name.replace(at_num, "").replace("-", "")  # Find element code
+    elem = elem[0].upper() + elem[1:]  # Fix
+    return f"{elem}-{at_num}"
 
 
 def read_beta_plus_spectra(rad_name):
@@ -73,6 +112,53 @@ def compute_cdf_and_total_yield(data, bins):
     total = p.sum()
     cdf = np.cumsum(p) / total
     return cdf, total
+
+
+def get_icrp107_spectrum(rad_name: str, spectrum_type="gamma") -> Box:
+    """
+    Get the spectrum of a given radionuclide according to ICRP107 recommendations.
+
+    Parameters
+    ----------
+    rad_name : str
+        The name of the radionuclide in Gate format, e.g. "Tc99m", "Lu177"
+
+    spectrum_type : str
+        The type of spectrum to retrieve. Must be one of "gamma", "beta-", "beta+", "alpha", "X", "neutron", "auger", "IE", "alpha recoil", "anihilation", "fission", "betaD", "b-spectra"
+
+    Returns
+    -------
+    Box
+        A Box with two keys: "energies" and "weights". The first contains the energy of each emission, the second contains the weight of each emission (summing to 1).
+
+    Raises
+    ------
+    fatal
+        If the radionuclide or spectrum type is not valid.
+    """
+    path = pathlib.Path(os.path.dirname(__file__)).parent / "data" / "icrp107.json"
+
+    if spectrum_type not in icrp107_emissions: #Convert particle name to spectrum type
+        spectrum_type = spectrum_type.lower().replace("e-", "beta-").replace("e+", "beta+")
+
+    if spectrum_type not in icrp107_emissions:
+        fatal(f"get_icrp107_spectrum: {spectrum_type} is not valid")
+
+    with open(path, "r") as f:
+        data = json.load(f)
+        rad = gate_radname_to_icrp107(rad_name)
+        if rad not in data:
+            fatal(
+                f"get_icrp107_spectrum: {path} does not contain data for isotope {rad}"
+            )
+
+        # convert to Box
+        gate_data = {}
+        gate_data["energies"] = [
+            v[0] * g4_units.MeV for v in data[rad]["Emissions"][spectrum_type]
+        ]
+        gate_data["weights"] = [v[1] for v in data[rad]["Emissions"][spectrum_type]]
+        return Box(gate_data)
 
 
 def get_rad_gamma_spectrum(rad):
@@ -132,6 +218,34 @@ def set_source_rad_energy_spectrum(source, rad):
     rad_spectrum = get_rad_gamma_spectrum(rad)
 
     source.particle = "gamma"
+    source.energy.type = "spectrum_discrete"
+    source.energy.spectrum_weights = rad_spectrum.weights
+    source.energy.spectrum_energies = rad_spectrum.energies
+
+def set_source_icrp107_energy_spectrum(source, rad):
+    """
+    Set the energy spectrum of a source according to the ICRP107 recommendations.
+
+    Parameters
+    ----------
+    source : a source
+        The source to set the energy spectrum for
+    rad : str
+        The name of the radionuclide to use
+    
+    Notes
+    -----
+    The source particle must be set before calling this function.
+    If the source particle is "beta-" or "e-", the function will use the
+    "b-spectra" spectrum from the ICRP107 data.
+    Otherwise, the function will use the discrete spectrum for the given particle.
+
+    """
+    if source.particle == "beta-" or source.particle == "e-":
+        rad_spectrum = get_icrp107_spectrum(rad, "b-spectra")
+    else:
+        rad_spectrum = get_icrp107_spectrum(rad, source.particle)
+
     source.energy.type = "spectrum_discrete"
     source.energy.spectrum_weights = rad_spectrum.weights
     source.energy.spectrum_energies = rad_spectrum.energies
