b2af2d7a77c5a5b775bc86b8b296c6735d569250
Nils Krah <krah.nils@gmail.com>
2024-11-29 18:24:33 +0100

    Put methods back in order after merge

--- opengate/actors/actoroutput.py
+++ opengate/actors/actoroutput.py
@@ -490,57 +490,6 @@ class ActorOutputUsingDataItemContainer(ActorOutputBase):
         ),
     }
 
-    def merge_into_merged_data(self, data):
-        if self.merged_data is None:
-            self.merged_data = data
-        else:
-            self.merged_data = merge_data([self.merged_data, data])
-
-    def end_of_run(self, run_index):
-        if self.merge_data_after_simulation is True:
-            self.merge_into_merged_data(self.data_per_run[run_index])
-        if self.keep_data_per_run is False:
-            self.data_per_run.pop(run_index)
-
-    def end_of_simulation(self, **kwargs):
-        try:
-            self.write_data_if_requested(which="all", **kwargs)
-        except NotImplementedError:
-            raise GateImplementationError(
-                "Unable to run end_of_simulation "
-                f"in user_output {self.name} of actor {self.belongs_to_actor.name}"
-                f"because the class does not implement a write_data_if_requested() "
-                f"and/or write_data() method. "
-                f"A developer needs to fix this. "
-            )
-
-    def merge_data_from_actor_output(
-        self, *actor_output, discard_existing_data=True, **kwargs
-    ):
-        run_indices_to_import = set()
-        for ao in actor_output:
-            run_indices_to_import.union(ao.data_per_run.keys())
-        which_output_per_run_index = dict(
-            [
-                (r, [ao for ao in actor_output if r in ao.data_per_run])
-                for r in run_indices_to_import
-            ]
-        )
-        for r in run_indices_to_import:
-            data_to_import = [
-                ao.data_per_run[r] for ao in which_output_per_run_index[r]
-            ]
-            if discard_existing_data is False and r in self.data_per_run:
-                data_to_import.append(self.data_per_run[r])
-            self.data_per_run[r] = merge_data(data_to_import)
-        merged_data_to_import = [
-            ao.merged_data for ao in actor_output if ao.merged_data is not None
-        ]
-        if discard_existing_data is False and self.merged_data is not None:
-            merged_data_to_import.append(self.merged_data)
-        if len(merged_data_to_import) > 0:
-            self.merged_data = merge_data(merged_data_to_import)
-
     # this intermediate base class defines a class attribute data_container_class,
     # but leaves it as None. Specific classes need to set it to the correct class or tuple of classes
     data_container_class = None
@@ -855,6 +804,45 @@ class ActorOutputUsingDataItemContainer(ActorOutputBase):
     def end_of_simulation(self, item="all", **kwargs):
         self.write_data_if_requested(which="all", item=item)
 
+    def merge_into_merged_data(self, data):
+        if self.merged_data is None:
+            self.merged_data = data
+        else:
+            self.merged_data = merge_data([self.merged_data, data])
+
+    def end_of_run(self, run_index):
+        if self.merge_data_after_simulation is True:
+            self.merge_into_merged_data(self.data_per_run[run_index])
+        if self.keep_data_per_run is False:
+            self.data_per_run.pop(run_index)
+
+    def merge_data_from_actor_output(
+        self, *actor_output, discard_existing_data=True, **kwargs
+    ):
+        run_indices_to_import = set()
+        for ao in actor_output:
+            run_indices_to_import.union(ao.data_per_run.keys())
+        which_output_per_run_index = dict(
+            [
+                (r, [ao for ao in actor_output if r in ao.data_per_run])
+                for r in run_indices_to_import
+            ]
+        )
+        for r in run_indices_to_import:
+            data_to_import = [
+                ao.data_per_run[r] for ao in which_output_per_run_index[r]
+            ]
+            if discard_existing_data is False and r in self.data_per_run:
+                data_to_import.append(self.data_per_run[r])
+            self.data_per_run[r] = merge_data(data_to_import)
+        merged_data_to_import = [
+            ao.merged_data for ao in actor_output if ao.merged_data is not None
+        ]
+        if discard_existing_data is False and self.merged_data is not None:
+            merged_data_to_import.append(self.merged_data)
+        if len(merged_data_to_import) > 0:
+            self.merged_data = merge_data(merged_data_to_import)
+
 
 class ActorOutputImage(ActorOutputUsingDataItemContainer):
 
@@ -942,22 +930,6 @@ class ActorOutputStatisticsActor(ActorOutputUsingDataItemContainer):
                 "allowed_values": ("json", "legacy"),
             },
         ),
-        # "output_filename": (
-        #     "auto",
-        #     {
-        #         "doc": "Filename for the data represented by this actor output. "
-        #         "Relative paths and filenames are taken "
-        #         "relative to the global simulation output folder "
-        #         "set via the Simulation.output_dir option. ",
-        #         "setter_hook": _setter_hook_stats_actor_output_filename,
-        #     },
-        # ),
-        # "write_to_disk": (
-        #     False,
-        #     {
-        #         "doc": "Should the output be written to disk, or only kept in memory? ",
-        #     },
-        # ),
     }
 
     def __init__(self, *args, **kwargs):
