a1b3024f59daf268961df1bf00874e6cc13f10c2
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2024-12-17 10:57:06 +0100

    default verbose

--- opengate/managers.py
+++ opengate/managers.py
@@ -1500,6 +1500,7 @@ class Simulation(GateObject):
         """
         # The Simulation instance should not hold a reference to itself (cycle)
         kwargs.pop("simulation", None)
+        setter_hook_verbose_level(self, "INFO")
         super().__init__(name=name, **kwargs)
 
         # list to store warning messages issued somewhere in the simulation
