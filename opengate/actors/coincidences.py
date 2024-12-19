8a765de40c64d5b4b9230b3f871d77696d81359c
kochebina <kochebina@gmail.com>
2024-12-19 18:15:50 +0100

    Coin sorter V2 corrected with tests

--- opengate/actors/coincidences.py
+++ opengate/actors/coincidences.py
@@ -1,10 +1,15 @@
 import awkward as ak
+
+# import itertools
+# import collections
+import opengate as gate
 from tqdm import tqdm
 from ..exception import fatal
-
+import numpy as np
+from collections import Counter
 
 def coincidences_sorter(
-    singles_tree, time_window, minSecDiff, policy, chunk_size=10000
+    singles_tree, time_window, policy, minDistanceXY, maxDistanceZ, chunk_size=10000
 ):
     """
     Consider the singles and sort them according to coincidences
@@ -16,14 +21,16 @@ def coincidences_sorter(
     Chunk size is important for very large root file to avoid loading everything in memory
 
     DEV NOTES:
-    1) potential bug while having several chunks: couple of coincidences too much
-    2) filtering during sorting or after? so far done after but maybe better do during for execution speed
-    3) removeMultiples policy is applied on EventID maybe should be done differently: checking while time window is open
-
+    1) potential bug while having several chunks: couple of coincidecnes too much
+    2) potential acceleration is possible 
+    3) TODO : add check that all necessary attibutes are saved for Singles
+    4) TODO: add option allDigiOpenCoincGate=false, so far only for allDigiOpenCoincGate=true
 
     """
+
     # prepare the "coincidences" tree (dict)
     # with the same branches as the singles
+    # Prepare the "coincidences" tree (dict) with the same branches as the singles
     coincidences = {}
     for k in singles_tree.keys():
         coincidences[f"{k}1"] = []
@@ -39,72 +46,208 @@ def coincidences_sorter(
 
         # Process the current combined chunk
         current_chunk_tail = process_chunk(
-            singles_tree.keys(), chunk, coincidences, time_window, policy
+            singles_tree.keys(),
+            chunk,
+            coincidences,
+            time_window,
+            minDistanceXY,
+            maxDistanceZ,
+            policy,
         )
 
         # Store the tail of the current chunk for the next iteration
         previous_chunk_tail = current_chunk_tail
 
-    # prepare coincidences_filtered
-    coincidences_filtered = {}
-    for k in coincidences.keys():
-        coincidences_filtered[f"{k}"] = []
-
-    if policy == "removeMultiples":
-        ids_to_keep = remove_multiples(coincidences)
-    # TODO add other filters
-
-    elif policy == "keepAll":
-        return coincidences
-    else:
-        fatal(f"Error in Coincidence Sorter" f" {policy} is unknown")
-
-    print("-----Filtering------")
-    for i in tqdm(range(len(coincidences["EventID1"]))):
-        for j in ids_to_keep:
-            if coincidences["EventID1"][i] == j:
-                for k in coincidences.keys():
-                    coincidences_filtered[f"{k}"].append(coincidences[f"{k}"][i])
-
-    return coincidences_filtered
+    return coincidences
 
 
-def process_chunk(keys, chunk, coincidences, time_window, policy):
+def process_chunk(
+    keys, chunk, coincidences, time_window, minDistanceXY, maxDistanceZ, policy
+):
     singles = chunk
-    coinc_events = []
     nsingles = len(singles)
-
+ 
+    # Define policy functions
+    policy_functions = {
+        "removeMultiples": remove_multiples,
+        "takeAllGoods": take_all_goods,
+        "takeWinnerOfGoods": take_winner_of_goods,
+        "takeIfOnlyOneGood": take_if_only_one_good,
+        "takeWinnerIfIsGood": take_winner_if_is_good,
+        "takeWinnerIfAllAreGoods": take_winner_if_all_are_goods,
+    }
+    
     print("-----Sorting------")
+    
+    #coincidences_filtered = []
+    
+    coincidences_tmp = {}
+    for k in coincidences.keys():
+        coincidences_tmp[f"{k}"] = []
+
+    # Loop over singles    
     for i in tqdm(range(nsingles - 1)):
         time_window_open = singles[i]["GlobalTime"]
+        
         for j in range(i + 1, nsingles):
-
             time = singles[j]["GlobalTime"]
-            coinc_events = []
 
             if abs(time - time_window_open) <= time_window:
-
-                # remove if in the same volume
+                # Remove if in the same volume
                 if (
                     singles[i]["PreStepUniqueVolumeID"]
                     == singles[j]["PreStepUniqueVolumeID"]
                 ):
                     break
 
-                # TODO: apply minRingDiff
-                # if distance<defined_distance:
-                #    break
-
-                coinc_events.append(singles[i])
-                coinc_events.append(singles[j])
                 for k in keys:
-                    coincidences[f"{k}1"].append(coinc_events[0][k])
-                    coincidences[f"{k}2"].append(coinc_events[1][k])
+                    coincidences_tmp[f"{k}1"].append(singles[i][k])
+                    coincidences_tmp[f"{k}2"].append(singles[j][k])
 
             else:
-                break  # if the time difference exceeds the time window, break the inner loop
+                #filter coincidences in the same window
+                
+                # skip if no coincidecnes in this window    
+                if len(coincidences_tmp["EventID1"])==0:
+                    break
+                
+                if policy in policy_functions:
+                    coincidences_filtered = policy_functions[policy](
+                        coincidences_tmp, minDistanceXY, maxDistanceZ
+                    )
+                         
+                else:
+                    fatal(f"Error in Coincidence Sorter: {policy} is unknown")
 
-    return coinc_events
+                #save the filtered coincidences 
+                if coincidences_filtered:
+                    for key in coincidences:
+                        for j in range(len(coincidences_filtered[key])):
+                            coincidences[key].append(coincidences_filtered[key][j])
+                            
+                
+                #clean temp containers
+                if coincidences_filtered:
+                    coincidences_filtered.clear()
+                for key in coincidences_tmp:
+                    coincidences_tmp[key].clear()
+                
+                break  # if the time difference exceeds the time window, break the loop
+    
+    return coincidences
+
+
+def filter_goods(coincidences, minDistanceXY, maxDistanceZ):
+    
+    indices_to_keep = []
+
+    for i in range(len(coincidences["EventID1"])):
+        #XY_diff, Z_diff = calculate_distance(coincidences)
+        x1, y1, z1 = (
+            coincidences["PostPosition_X1"][i],
+            coincidences["PostPosition_Y1"][i],
+            coincidences["PostPosition_Z1"][i]
+        )
+        x2, y2, z2 = (
+            coincidences["PostPosition_X2"][i],
+            coincidences["PostPosition_Y2"][i],
+            coincidences["PostPosition_Z2"][i]
+        )
+        
+        XY_diff = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        Z_diff = abs(z2 - z1)
+
+        
+        if XY_diff > minDistanceXY or Z_diff < maxDistanceZ:
+            indices_to_keep.append(i) 
+
+           
+    coincidences_filtered = {key: [value[i] for i in indices_to_keep] for key, value in coincidences.items()}
+     
+    return coincidences_filtered
+
+
+def filter_multi(coincidences):
+
+    if len(coincidences["EventID1"])==1:
+        return coincidences
+    else:
+        return {}
+   
+
+
+def filter_max_energy(coincidences):
+    
+    energy_sums = [
+        coincidences["TotalEnergyDeposit1"][i] + coincidences["TotalEnergyDeposit2"][i]
+        for i in range(len(coincidences["TotalEnergyDeposit1"]))
+    ]
+    # Find the index of the maximum energy sum
+    max_index = energy_sums.index(max(energy_sums))
+
+    # Filter the dictionary to include only the element at the max energy sum index
+    coincidences_filtered = {key: [value[max_index]] for key, value in coincidences.items()}
+   
+    return coincidences_filtered
+
+
+def remove_multiples(coincidences, minDistanceXY, maxDistanceZ):
+    # Remove all multiple coincidences
+    # 1) check if good
+    # 2) take only ones where one coincidence in a time window
+
+    coincidences_goods = filter_goods(coincidences, minDistanceXY, maxDistanceZ)
+    coincidences_output = filter_multi(coincidences_goods)
+    return coincidences_output    
+
+
+def take_all_goods(coincidences, minDistanceXY, maxDistanceZ):
+    # Return all good coincidences  
+    return filter_goods(coincidences, minDistanceXY, maxDistanceZ)
+
+
+def take_winner_of_goods(coincidences, minDistanceXY, maxDistanceZ):
+    #Take winner of goods
+    # 1) check if good
+    # 2) take only one with the highest energy (energy1+energy2)
+    
+    coincidences_goods=filter_goods(coincidences, minDistanceXY, maxDistanceZ)
+    
+    coincidences_output=filter_max_energy(coincidences_goods)
+   
+    return coincidences_output
+   
+def take_if_only_one_good(coincidences, minDistanceXY, maxDistanceZ):
+    # Take winner if only one good
+    # 1) check how many goods
+    # 2) if one --> keep
+    
+    coincidences_goods=filter_goods(coincidences, minDistanceXY, maxDistanceZ)
+    coincidences_output=filter_multi(coincidences_goods)
+    return coincidences_output 
+
+def take_winner_if_is_good(coincidences, minDistanceXY, maxDistanceZ):
+    # Take winner if it is good one
+    # 1) find the winner with Emax
+    # 2) if good --> keep
+    
+    coincidences_winner=filter_max_energy(coincidences)
+    coincidences_output=filter_goods(coincidences_winner, minDistanceXY, maxDistanceZ)
+    return coincidences_output 
+    
+def take_winner_if_all_are_goods(coincidences, minDistanceXY, maxDistanceZ):
+    # Take winner if all are goods
+    # 1) check if all are goods, if not -> skip
+    # 2) keep winner with Emax 
+    coincidences_goods=filter_goods(coincidences, minDistanceXY, maxDistanceZ)
+
+    if len(coincidences_goods["EventID1"]) != len(coincidences["EventID1"]):
+        coincidences_output = {}
+    else:
+        coincidences_output = filter_max_energy(coincidences_goods)
+    
+    return coincidences_output 
+
 
 
 def copy_tree_for_dump(input_tree):
@@ -112,10 +255,3 @@ def copy_tree_for_dump(input_tree):
     for key in input_tree.keys():
         branches[key] = input_tree.arrays([key], library="np")[key]
     return branches
-
-
-def remove_multiples(coincidences):
-    # return EventID1 to keep
-    ids = coincidences["EventID1"]
-    ids = [i for i in ids if ids.count(i) == 1]
-    return ids
