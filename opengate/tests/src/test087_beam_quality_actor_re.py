bbee0022478f44039826108481b306815a68ebfa
Andreas Resch <andreas.resch@medaustron.at>
2025-01-20 14:11:31 +0100

    no test yet

--- opengate/tests/src/test087_beam_quality_actor_re.py
+++ opengate/tests/src/test087_beam_quality_actor_re.py
@@ -4,13 +4,14 @@
 from scipy.spatial.transform import Rotation
 import opengate as gate
 from opengate.tests import utility
-
+import pandas as pd
 
 if __name__ == "__main__":
-    paths = utility.get_default_test_paths(__file__, "test_085")
+    paths = utility.get_default_test_paths(__file__, "test_087")
     print(paths)
-    ref_path = paths.output_ref
-
+    ref_path = paths.output_ref / "test087"
+    mkm_lq_fpath = ref_path / "mkm_nirs_LQparameters_SURVIVAL.csv"
+    df = pd.read_csv(mkm_lq_fpath)
     # create the simulation
     sim = gate.Simulation()
 
@@ -56,11 +57,6 @@ if __name__ == "__main__":
 
     # physics
     sim.physics_manager.physics_list_name = "QGSP_BIC_EMZ"
-    # sim.physics_manager.set_production_cut("world", "all", 1000 * km)
-    # FIXME need SetMaxStepSizeInRegion ActivateStepLimiter
-    # now avialable
-    # e.g.
-    # sim.physics_manager.set_max_step_size(volume_name='phantom.name', max_step_size=1*mm)
 
     # default source for tests
     source = sim.add_source("GenericSource", "mysource")
@@ -78,16 +74,12 @@ if __name__ == "__main__":
     source.n = numPartSimTest
     # source.activity = 100 * kBq
 
-    # filter : keep proton
-    # f = sim.add_filter("ParticleFilter", "f")
-    # f.particle = "proton"
-
     size = [50, 1, 1]
     spacing = [2.0 * mm, 60.0 * mm, 60.0 * mm]
 
     doseActorName_IDD_d = "IDD_d"
     doseIDD = sim.add_actor("DoseActor", doseActorName_IDD_d)
-    doseIDD.output = paths.output / ("test050-" + doseActorName_IDD_d + ".mhd")
+    doseIDD.output_filename = paths.output / ("test050-" + doseActorName_IDD_d + ".mhd")
     doseIDD.mother = phantom_off.name
     doseIDD.size = size
     doseIDD.spacing = spacing
@@ -96,7 +88,7 @@ if __name__ == "__main__":
 
     RBE = "RBE"
     RBE_act = sim.add_actor("LETActor", RBE)
-    RBE_act.output = paths.output / ("test050-" + RBE + ".mhd")
+    RBE_act.output_filename = paths.output / ("test050-" + RBE + ".mhd")
     RBE_act.mother = phantom_off.name
     RBE_act.size = size
     RBE_act.spacing = spacing
@@ -113,10 +105,11 @@ if __name__ == "__main__":
     RBE_act.lookup_table_path = (
         "/home/ideal/0_Data/21_RBE/01_Tables/NIRS_MKM_reduced_data.txt"
     )
+    RBE_act.lookup_table_path = ref_path / "RE_Alanine_RBEstyle.txt"
 
     RE = "RE"
     RE_act = sim.add_actor("LETActor", RE)
-    RE_act.output = paths.output / ("test050-" + RE + ".mhd")
+    RE_act.output_filename = paths.output / ("test050-" + RE + ".mhd")
     RE_act.mother = phantom_off.name
     RE_act.size = size
     RE_act.spacing = spacing
@@ -130,64 +123,7 @@ if __name__ == "__main__":
     RE_act.lookup_table_path = (
         "/opt/GATE/GateRTion-1.1/install/data/RE_Alanine/RE_Alanine_RBEstyle.txt"
     )
-    #
-    # usesful for looping over several options
-    # LETActor_IDD_d.track_average = True ## same as above line
 
-    # LETActorName_IDD_d = "LETActorOG_d"
-    # LETActor_IDD_d = sim.add_actor("LETActor", LETActorName_IDD_d)
-    # LETActor_IDD_d.output = paths.output / ("test050-" + LETActorName_IDD_d + ".mhd")
-    # LETActor_IDD_d.mother = phantom_off.name
-    # LETActor_IDD_d.size = size
-    # LETActor_IDD_d.spacing = spacing
-    # LETActor_IDD_d.hit_type = "random"
-    # LETActor_IDD_d.separate_output = True
-    # # both lines do the same thing,
-    # setattr(
-    #     LETActor_IDD_d, "dose_average", True
-    # )  # usesful for looping over several options
-    # # LETActor_IDD_d.track_average = True ## same as above line
-
-    # LETActorName_IDD_t = "LETActorOG_t"
-    # LETActor_IDD_t = sim.add_actor("LETActor", LETActorName_IDD_t)
-    # LETActor_IDD_t.output = paths.output / ("test050-" + LETActorName_IDD_t + ".mhd")
-    # LETActor_IDD_t.mother = phantom_off.name
-    # LETActor_IDD_t.size = size
-    # LETActor_IDD_t.spacing = spacing
-    # LETActor_IDD_t.hit_type = "random"
-    # LETActor_IDD_t.track_average = True
-
-    # LETActorName_IDD_d2w = "LETActorOG_d2w"
-    # LETActor_IDD_d2w = sim.add_actor("LETActor", LETActorName_IDD_d2w)
-    # LETActor_IDD_d2w.output = paths.output / (
-    #     "test050-" + LETActorName_IDD_d2w + ".mhd"
-    # )
-    # LETActor_IDD_d2w.mother = phantom_off.name
-    # LETActor_IDD_d2w.size = size
-    # LETActor_IDD_d2w.spacing = spacing
-    # LETActor_IDD_d2w.hit_type = "random"
-    # LETActor_IDD_d2w.other_material = "G4_WATER"
-    # setattr(LETActor_IDD_d2w, "dose_average", True)
-
-    # LET_primaries = "LETprimaries"
-    # LETActor_primaries = sim.add_actor("LETActor", LET_primaries)
-    # LETActor_primaries.output = paths.output / ("test050-" + LET_primaries + ".mhd")
-    # LETActor_primaries.mother = phantom_off.name
-    # LETActor_primaries.size = size
-    # LETActor_primaries.spacing = spacing
-    # LETActor_primaries.hit_type = "random"
-    # LETActor_primaries.dose_average = True
-
-    # # # add dose actor, without e- (to check)
-    fe = sim.add_filter("ParticleFilter", "f")
-    print(dir(fe))
-    fe.particle = "proton"
-    fe.policy = "keep"
-    RBE_act.filters.append(fe)
-    # print(dir(fe))
-
-    # fName_ref_IDD = "IDD__Proton_Energy1MeVu_RiFiout-Edep.mhd"
-    # print(paths)
     # add stat actor
     s = sim.add_actor("SimulationStatisticsActor", "stats")
     s.track_types_flag = True
@@ -209,11 +145,6 @@ if __name__ == "__main__":
     print(stat)
 
     # ----------------------------------------------------------------------------------------------------------------
-    # tests
-    print()
-    # gate.exception.warning("Tests stats file")
-    # stats_ref = utility.read_stat_file(paths.gate_output / "stats.txt")
-    # is_ok = utility.assert_stats(stat, stats_ref, 0.14)
 
     rbe_actor = sim.output.get_actor(RBE)
 
@@ -230,8 +161,10 @@ if __name__ == "__main__":
     )
 
     """
-    ref_fpath = "/home/aresch/Calculations/05_Gate9_benchmarkEx/02_RE_alanine/output/"
-    ref_fpath += "test080_REalanine__Proton_Energy80spread1MeV_PrimaryProton-relEfficiency-letToG4_ALANINE.mhd"
+    ref_fpath = (
+        ref_path
+        / "test087_REalanine__Proton_Energy80spread1MeV_PrimaryProton-relEfficiency-letToG4_ALANINE.mhd"
+    )
     is_ok = utility.assert_filtered_imagesprofile1D(
         ref_filter_filename1=ref_path / fNameIDD,
         ref_filename1=ref_fpath,
