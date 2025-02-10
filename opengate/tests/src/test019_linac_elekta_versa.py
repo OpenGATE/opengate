5db7143dcbb271ac221ae79937f4783f83c42441
Jing Zhang <jing3jour@gmail.com>
2025-02-10 11:11:49 +0100

    Update test019_linac_elekta_versa.py

--- opengate/tests/src/test019_linac_elekta_versa.py
+++ opengate/tests/src/test019_linac_elekta_versa.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     sim.visu_type = "vrml"
     sim.check_volumes_overlap = False
     sim.number_of_threads = 1
-    sim.output_dir = paths.output  # FIXME (not yet)
+    sim.output_dir = paths.output
     sim.random_seed = 123456789
     sim.check_volumes_overlap = True
     sim.output_dir = paths.output
