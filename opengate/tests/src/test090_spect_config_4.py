cd3441703383893df135b36b037dcc0026b64352
David <david.sarrut@gmail.com>
2025-05-07 16:48:46 +0200

    update to new api

--- opengate/tests/src/test090_spect_config_4.py
+++ opengate/tests/src/test090_spect_config_4.py
@@ -12,30 +12,24 @@ if __name__ == "__main__":
     # TEST 3: intevo, Lu177, 2 heads, 3 angles, with FF-FD
     sc = create_test_spect_config(paths)
 
-    # create the simulation
-    print(sc)
-    sim = gate.Simulation()
-    output = sc.setup_simulation(sim, number_of_threads=1, visu=False)
-    print(output)
-
-    # make it FF-AA primary
+    # units
     deg = g4_units.deg
-    ac = 1e3 * g4_units.Bq / sim.number_of_threads
-    crystals = sim.volume_manager.find_volumes("crystal")
-    crystal_names = [c.name for c in crystals]
-    options = Box(
-        {
-            "primary_activity": ac,
-            "angle_tolerance": 15 * deg,
-            "volume_names": crystal_names,
-            "forced_direction_flag": True,
-        }
-    )
-    spect_freeflight_initialize_primary(sim, sc, output.source, options)
+    Bq = g4_units.Bq
+    cm = g4_units.cm
 
-    # run it
+    # make it FF-FD primary
+    sc.free_flight_config.primary_activity = 1e3 * Bq
+    sc.free_flight_config.max_rejection = 10000
+    sc.free_flight_config.angle_tolerance = 15 * deg
+    sc.free_flight_config.forced_direction_flag = True
+
+    # create the simulation
+    sim = gate.Simulation()
     sim.random_seed = 987456
-    sim.run()
+    output = sc.setup_simulation_ff_primary(sim, number_of_threads=1, visu=False)
+
+    # go
+    sim.run(start_new_process=True)
 
     # we check only that the output files exist
     is_ok = True
