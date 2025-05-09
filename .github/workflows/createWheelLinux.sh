e4d8ab1ce96e074327183f137ec46d38ed53916a
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2025-05-09 10:56:35 +0200

    merge master

--- .github/workflows/createWheelLinux.sh
+++ .github/workflows/createWheelLinux.sh
@@ -6,16 +6,17 @@ cd /home/core/
 export PATH=/software/cmake/cmake-3.18.2-Linux-x86_64/bin/:${PATH}
 source /software/geant4/bin/geant4make.sh
 export CMAKE_PREFIX_PATH=/software/geant4/bin:/software/itk/bin/:${CMAKE_PREFIX_PATH}
+. /opt/rh/gcc-toolset-14/enable
 mkdir opengate_core/plugins
-cp -r /lib64/qt5/plugins/platforms opengate_core/plugins
-cp -r /lib64/qt5/plugins/imageformats opengate_core/plugins
+cp -r /lib64/qt6/plugins/platforms/* opengate_core/plugins/
+cp -r /lib64/qt6/plugins/imageformats opengate_core/plugins/
 /opt/python/${PYTHONFOLDER}/bin/pip install wget colored setuptools
 /opt/python/${PYTHONFOLDER}/bin/python setup.py sdist bdist_wheel
 archi=`uname -m`
 if [ "$(uname -m)" = "aarch64" ]; then
-  auditwheel repair /home/core/dist/*.whl -w /software/wheelhouse/ --plat "manylinux2014_aarch64"
+  auditwheel repair /home/core/dist/*.whl -w /software/wheelhouse/ --plat "manylinux_2_34_aarch64"
 else
-  auditwheel repair /home/core/dist/*.whl -w /software/wheelhouse/ --plat "manylinux2014_x86_64"
+  auditwheel repair /home/core/dist/*.whl -w /software/wheelhouse/ --plat "manylinux_2_34_x86_64"
 fi
 cp -r /software/wheelhouse /home/
 #/opt/python/${PYTHONFOLDER}/bin/pip install twine
