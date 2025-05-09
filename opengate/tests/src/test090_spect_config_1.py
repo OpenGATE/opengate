7f3af10b1dd219d0b4e21f68a4b1224686345482
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2025-05-09 10:58:43 +0200

    update spect config

--- opengate/tests/src/test090_spect_config_1.py
+++ opengate/tests/src/test090_spect_config_1.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     # create the simulation
     print(sc)
     sim = gate.Simulation()
-    output = sc.setup_simulation(sim, number_of_threads=1, visu=False)
+    output = sc.create_simulation(sim, number_of_threads=1, visu=False)
     print(output)
 
     # run it
--- opengate/tests/src/test090_spect_config_3.py
+++ opengate/tests/src/test090_spect_config_3.py
@@ -12,28 +12,35 @@ if __name__ == "__main__":
     # TEST 3: intevo, Lu177, 2 heads, 3 angles, with FF-AA, primary only
     sc = create_test_spect_config(paths)
 
-    # units
-    deg = g4_units.deg
-    Bq = g4_units.Bq
-    cm = g4_units.cm
+    # create the simulation
+    print(sc)
+    sim = gate.Simulation()
+    output = sc.create_simulation(sim, number_of_threads=1, visu=False)
+    print(output)
 
     # make it FF-AA primary
-    sc.free_flight_config.primary_activity = 1e5 * Bq
-    sc.free_flight_config.max_rejection = 10000
-    sc.free_flight_config.angle_tolerance = 15 * deg
-    sc.free_flight_config.forced_direction_flag = False
+    deg = g4_units.deg
+    ac = 1e5 * g4_units.Bq / sim.number_of_threads
+    crystals = sim.volume_manager.find_volumes("crystal")
+    crystal_names = [c.name for c in crystals]
+    print(crystal_names)
+    options = Box(
+        {
+            "max_rejection": 10000,
+            "primary_activity": ac,
+            "angle_tolerance": 15 * deg,
+            "volume_names": crystal_names,
+        }
+    )
+    spect_freeflight_initialize_primary(sim, sc, output.source, options)
 
-    # create the simulation
-    sim = gate.Simulation()
+    # run it
     sim.random_seed = 654456
-    output = sc.setup_simulation_ff_primary(sim, number_of_threads=1, visu=False)
-
-    # go
     sim.run(start_new_process=True)
 
     # we check only that the output files exist
     is_ok = True
     is_ok = check_stats_file(18960, sc, output, is_ok)
-    is_ok = check_projection_files(sim, paths, output, is_ok, tol=60, squared_flag=True)
+    is_ok = check_projection_files(sim, paths, output, is_ok, tol=46, squared_flag=True)
 
     utility.test_ok(is_ok)
