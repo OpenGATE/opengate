03f83693290c3e695e01b7934582bafa1fd6d050
David <david.sarrut@gmail.com>
2025-06-26 13:18:01 +0200

    Update free flight actor configurations for bias handling
    Replaced `ignored_volumes` with `unbiased_volumes` and added `kill_interacting_in_volumes` to control gamma interactions more explicitly. Adjusted attached volume in one test to ensure proper killing behavior outside specific regions. These changes improve bias management and consistency in simulations.

--- opengate/tests/src/test085_free_flight_spect_3_scatter_mt.py
+++ opengate/tests/src/test085_free_flight_spect_3_scatter_mt.py
@@ -27,7 +27,8 @@ if __name__ == "__main__":
     # free flight actor
     ff = sim.add_actor("ScatterSplittingFreeFlightActor", "ff")
     ff.attached_to = "world"
-    ff.ignored_volumes = ["spect_1_crystal", "spect_2_crystal"]
+    ff.unbiased_volumes = ["spect_1_crystal", "spect_2_crystal"]
+    ff.kill_interacting_in_volumes = ["spect_1_crystal", "spect_2_crystal"]
     ff.compton_splitting_factor = 50
     ff.rayleigh_splitting_factor = 10
     ff.max_compton_level = 10000
