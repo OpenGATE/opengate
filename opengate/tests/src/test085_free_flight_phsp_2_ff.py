0c9fe75329317488727f51970f51bbe29649a10c
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2025-10-03 16:45:25 +0200

    Refactor `test085` scripts: integrate advanced tree manipulation and comparison functions from `root_helpers.py`, update configurations, improve gamma process handling, and enhance test logic with detailed branch comparisons and plotting.

--- opengate/tests/src/test085_free_flight_phsp_2_ff.py
+++ opengate/tests/src/test085_free_flight_phsp_2_ff.py
@@ -3,6 +3,7 @@
 
 from opengate.tests import utility
 from test085_free_flight_helpers import *
+from opengate.contrib.root_helpers import *
 
 if __name__ == "__main__":
     paths = utility.get_default_test_paths(__file__, None, output_folder="test085_phsp")
@@ -11,11 +12,12 @@ if __name__ == "__main__":
     sim = gate.Simulation()
     sim.number_of_threads = 1
     # sim.visu = True
+    ac = 1e4
     source, actors = create_simulation_test085(
         sim,
         paths,
         simu_name="ff",
-        ac=1e4,
+        ac=ac,
         use_spect_head=False,
         use_spect_arf=False,
         use_phsp=True,
@@ -43,5 +45,39 @@ if __name__ == "__main__":
         scalings2=[1] * 5,
         img=paths.output / "test085_phsp_ff.png",
     )
+
+    # NOTE: this MUST be slightly different, as the prim does not include part of the Rayl
+    # that will come from the free flight scatter part.
+    results, b = compare_branches_chi2(
+        paths.output_ref / "phsp_sphere_ref_peak.root",
+        paths.output / "phsp_sphere_ff.root",
+        "phsp_sphere",
+        verbose=True,
+    )
+    utility.print_test(b, "Chi2 difference ? ")
+    is_ok = is_ok and b
+
+    fig = paths.output / "test085_phsp_prim.png"
+    plot_branch_comparison(
+        paths.output_ref / "phsp_sphere_ref_peak.root",
+        paths.output / "phsp_sphere_ff.root",
+        "phsp_sphere",
+        save_path=fig,
+        scaling_factor2=1e5 / ac,
+    )
+    print(f"Saved plot to {fig}")
+
+    """results = compare_branches_zscore(
+        paths.output_ref / "phsp_sphere_ref_peak.root",
+        paths.output / "phsp_sphere_ff.root",
+        "phsp_sphere",
+        # verbose=True,
+        bins=10,
+        scaling_factor2=1e5 / ac,
+    )
+    outliers = [r for r in results if "outliers" in r]
+    b = len(outliers) == 4
+    utility.print_test(b, f"Outliers ==4 ? {len(outliers)}")
+    is_ok = is_ok and b"""
 
     utility.test_ok(is_ok)
