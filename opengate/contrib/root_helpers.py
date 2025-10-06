3b5f6a93021731fa57d2d3258e73f2be5c704f46
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2025-10-06 08:04:53 +0200

    add root helpers functions

--- opengate/contrib/root_helpers.py
+++ opengate/contrib/root_helpers.py
@@ -6,16 +6,14 @@ import os
 from pathlib import Path
 from scipy.stats import chi2_contingency
 import logging
+from opengate.exception import raise_except
 
 # Get a logger for this specific module. This is the standard practice for libraries.
 logger = logging.getLogger(__name__)
 
-# --- Private Helper Functions ---
 
-
-def _open_trees_safely(paths, tree_name):
+def _root_open_trees_safely(paths, tree_name):
     """Opens one or more ROOT files and yields the TTree objects safely."""
-    # ... (content is unchanged)
     files = [uproot.open(path) for path in paths]
     try:
         trees = [f[tree_name] for f in files]
@@ -81,10 +79,108 @@ def _get_scaled_weights(branches_data, weight_branch, scaling_factor):
     return weights
 
 
-# --- Public API Functions ---
+def is_uproot_tree(tree):
+    # Check if the object has the key methods/attributes that uproot trees have
+    return hasattr(tree, "arrays") and hasattr(tree, "keys") and hasattr(tree, "items")
 
 
-def split_tree_by_branch(
+def get_array_dtype(series):
+    if isinstance(series, np.ndarray):
+        sdtype = series.dtype
+        if sdtype == "object":
+            return "string"
+        if np.issubdtype(sdtype, np.number):
+            # Get the type string (e.g., 'i4', 'f8') from the final NumPy array.
+            return sdtype.str.replace("<", "")
+        if sdtype.kind == "U":
+            return "string"
+        return sdtype.str
+    elif isinstance(series, ak.Array):
+        return series.type.content
+    else:
+        return type(series)
+
+
+def root_tree_get_branch_data(tree, library="np"):
+    branch_data = {}
+    for branch_name, series in tree.items():
+        if not is_uproot_tree(tree):
+            if library == "np":
+                series = np.array(tree[branch_name])
+            if library == "ak":
+                series = ak.Array(tree[branch_name])
+        else:
+            series = tree.arrays([branch_name], library=library)[branch_name]
+        branch_data[branch_name] = series
+    return branch_data
+
+
+def root_tree_get_branch_types(tree, verbose=False):
+    branch_types = {}
+    for branch_name, series in tree.items():
+        dtype = get_array_dtype(series)
+        branch_types[branch_name] = dtype
+        if verbose:
+            print(f"Branch '{branch_name}': {branch_types[branch_name]}")
+    return branch_types
+
+
+def root_tree_get_branch(tree, branch_name, library="np"):
+    """
+    This version is with numpy arrays, not awkward arrays.
+    Use only when the root file is small (for tests)
+    """
+    if branch_name not in tree:
+        # check if a branch with "_X" exists?
+        b = f"{branch_name}_X"
+        if b not in tree:
+            raise_except(f"Error: Branch '{branch_name}' not found in tree.")
+        return np.column_stack(
+            [
+                tree[f"{branch_name}_X"].array(library=library),
+                tree[f"{branch_name}_Y"].array(library=library),
+                tree[f"{branch_name}_Z"].array(library=library),
+            ]
+        )
+    return tree[branch_name].array(library=library)
+
+
+def root_read_tree(root_file_path, tree_name="phsp"):
+    # if not exists, raise_except
+    if not Path(root_file_path).exists():
+        raise_except(f"Error: File '{root_file_path}' not found.")
+    # open the root file
+    with uproot.open(root_file_path) as file:
+        if tree_name not in file:
+            raise_except(f"Error: TTree '{tree_name}' not found in {root_file_path}.")
+        tree = file[tree_name]
+    return tree
+
+
+def root_write_tree(output_file, tree_name, branch_types, branch_data):
+    """
+    Must be used like :
+    with uproot.recreate(output_filename) as output_file:
+        root_write_tree(output_file, "hits", hits_types, hits_data)
+        root_write_tree(output_file, "singles", singles_types, singles_data)
+    """
+
+    # Step 1: Create the empty tree using the dictionary of type strings.
+    tree = output_file.mktree(tree_name, branch_types)
+
+    # Step 2: Fill the tree using the dictionary of prepared data arrays.
+    tree.extend(branch_data)
+
+
+def root_write_trees(output_filename, trees_names, trees_data):
+    with uproot.recreate(output_filename) as output_file:
+        for tree_name, tree_data in zip(trees_names, trees_data):
+            t_data = root_tree_get_branch_data(tree_data)
+            t_types = root_tree_get_branch_types(t_data)
+            root_write_tree(output_file, tree_name, t_types, t_data)
+
+
+def root_split_tree_by_branch(
     input_path,
     high_val_path,
     low_val_path,
@@ -140,7 +236,7 @@ def split_tree_by_branch(
         raise
 
 
-def merge_trees(
+def root_merge_trees(
     file_paths,
     output_path,
     tree_name,
@@ -163,7 +259,7 @@ def merge_trees(
 
     try:
         all_data_to_merge = []
-        for trees in _open_trees_safely(file_paths, tree_name):
+        for trees in _root_open_trees_safely(file_paths, tree_name):
 
             first_branches = set(trees[0].keys())
             for i, tree in enumerate(trees[1:], 1):
@@ -202,7 +298,7 @@ def merge_trees(
         raise
 
 
-def compare_branches_chi2(
+def root_compare_branches_chi2(
     file1_path,
     file2_path,
     tree_name,
@@ -234,7 +330,7 @@ def compare_branches_chi2(
     results = []
     all_ok = True
     try:
-        for trees in _open_trees_safely([file1_path, file2_path], tree_name):
+        for trees in _root_open_trees_safely([file1_path, file2_path], tree_name):
             tree1, tree2 = trees
             branches1, branches2 = tree1.arrays(), tree2.arrays()
 
@@ -327,7 +423,7 @@ def compare_branches_chi2(
     return results, all_ok
 
 
-def plot_branch_comparison(
+def root_plot_branch_comparison(
     file1_path,
     file2_path,
     tree_name,
@@ -348,7 +444,7 @@ def plot_branch_comparison(
         )
 
     try:
-        for trees in _open_trees_safely([file1_path, file2_path], tree_name):
+        for trees in _root_open_trees_safely([file1_path, file2_path], tree_name):
             tree1, tree2 = trees
             numeric_branches = _get_common_numeric_branches(
                 trees, ignore_branches=[weight_branch]
@@ -487,7 +583,7 @@ def compare_branches_statistics(
 
     results = []
     try:
-        for trees in _open_trees_safely([file1_path, file2_path], tree_name):
+        for trees in _root_open_trees_safely([file1_path, file2_path], tree_name):
             tree1, tree2 = trees
             branches1, branches2 = tree1.arrays(), tree2.arrays()
 
@@ -585,7 +681,7 @@ def compare_branches_zscore(
 
     results = []
     try:
-        for trees in _open_trees_safely([file1_path, file2_path], tree_name):
+        for trees in _root_open_trees_safely([file1_path, file2_path], tree_name):
             tree1, tree2 = trees
             branches1, branches2 = tree1.arrays(), tree2.arrays()
 
