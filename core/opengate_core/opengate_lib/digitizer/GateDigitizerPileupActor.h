0c3a13e9b5df742583ec098c11d44d90a5db1e18
Gert Van Hoey <gert.vanhoey@xeos.care>
2025-11-09 10:31:24 +0000

    Implement pile-up

--- core/opengate_core/opengate_lib/digitizer/GateDigitizerPileupActor.h
+++ core/opengate_core/opengate_lib/digitizer/GateDigitizerPileupActor.h
@@ -11,7 +11,9 @@
 #include "GateVDigitizerWithOutputActor.h"
 #include <G4Cache.hh>
 #include <G4Navigator.hh>
+#include <map>
 #include <pybind11/stl.h>
+#include <variant>
 
 namespace py = pybind11;
 
@@ -36,13 +38,52 @@ public:
   // Called every time an Event ends (all threads)
   void EndOfEventAction(const G4Event *event) override;
 
+  // Called every time a Run ends (all threads)
+  void EndOfRunAction(const G4Run *run) override;
+
+  void SetGroupVolumeDepth(int depth);
+
 protected:
   void DigitInitialize(
       const std::vector<std::string> &attributes_not_in_filler) override;
 
+  // User parameters
+  int fGroupVolumeDepth;
+  double fTimeWindow;
+
+  // Output attribute pointers
+  GateVDigiAttribute *fOutputEdepAttribute{};
+  GateVDigiAttribute *fOutputGlobalTimeAttribute{};
+  GateVDigiAttribute *fOutputVolumeIDAttribute{};
+
+  void ProcessPileup();
+
   // During computation (thread local)
-  struct threadLocalT {};
+  struct threadLocalT {
+    double *edep;
+    double *time;
+    GateUniqueVolumeID::Pointer *volID;
+
+    // Storage for piled up singles
+    struct PileupGroup {
+      double highest_edep;
+      double total_edep;
+      double first_time;
+      double time;
+      GateUniqueVolumeID::Pointer volume_id;
+
+      using AttributeValue =
+          std::variant<double, int, int64_t, std::string, G4ThreeVector,
+                       GateUniqueVolumeID::Pointer>;
+      std::map<std::string, AttributeValue> stored_attributes;
+    };
+
+    std::map<uint64_t, std::vector<PileupGroup>> volume_groups;
+  };
   G4Cache<threadLocalT> fThreadLocalData;
+
+  void StoreAttributeValues(threadLocalT::PileupGroup &group, size_t index);
+  void FillAttributeValues(const threadLocalT::PileupGroup &group);
 };
 
 #endif // GateDigitizerPileupActor_h
