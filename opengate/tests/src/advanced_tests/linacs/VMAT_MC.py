67b916ee17cb7f610f040de9c6e11794375e244b
majacquet <maxime.jacquet@creatis.insa-lyon.fr>
2026-01-28 16:54:50 +0100

    new linac model + advanced test

---
+++ opengate/tests/src/advanced_tests/linacs/VMAT_MC.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import opengate as gate
+from opengate.tests import utility
+from opengate.utility import g4_units
+from opengate.contrib.linacs import elektaversa as versa
+from opengate.contrib.linacs import dicomrtplan as rtplan
+from scipy.spatial.transform import Rotation
+import numpy as np
+import itk
+
+
+
+
+def information_img_patient(path_img = None,open_img = True,img = None):
+    if open_img :
+        image = itk.imread(path_img)
+        offset = np.array(image.GetOrigin())
+        dim = np.array(image.GetLargestPossibleRegion().GetSize())
+        spacing = np.array(image.GetSpacing())
+    else :
+        offset = np.array(img.GetOrigin())
+        dim = np.array(img.GetLargestPossibleRegion().GetSize())
+        spacing = np.array(img.GetSpacing())
+
+    return(offset,dim,spacing)
+
+
+
+def add_dose_actor_on_patient_image(sim,patient,dim,spacing,do_tle,tle_type,tle_threshold):
+    MeV = gate.g4_units.MeV
+    mm = gate.g4_units.mm
+    if do_tle == True :
+        tle_dose_actor = sim.add_actor("TLEDoseActor", "tle_dose_actor")
+        tle_dose_actor.attached_to = patient.name
+        tle_dose_actor.dose_uncertainty.active = True
+        tle_dose_actor.edep_uncertainty.active = False
+        tle_dose_actor.density.active = False
+        tle_dose_actor.hit_type = "random"
+        tle_dose_actor.dose.active = True
+        tle_dose_actor.dose_squared.active = True
+        tle_dose_actor.edep.active = True
+        tle_dose_actor.edep.write_to_disk = False
+        tle_dose_actor.size = (np.array(np.round(dim), dtype=int))
+        tle_dose_actor.spacing = np.array(spacing, dtype=float)
+        tle_dose_actor.output_coordinate_system = 'attached_to_image'
+        # the following option is important: if TLE is used for gammas with too high energy, the
+        # resulting dose will be biased. The energy threshold depends on the voxels size of the
+        # dose actor. Here the bias is clearly visible if TLE is used above 1.2 MeV.
+        # With the threshold enabled, no acceleration for high enery gamma, but no bias.
+        tle_dose_actor.tle_threshold_type = tle_type
+        tle_dose_actor.tle_threshold = tle_threshold*mm
+        print(tle_dose_actor.tle_threshold)
+        tle_dose_actor.database = "EPDL"
+
+    dose = sim.add_actor("DoseActor", "dose")
+    dose.attached_to = patient.name
+    dose.size = (np.array(np.round(dim), dtype=int))
+    dose.spacing = np.array(spacing, dtype=float)
+    dose.output_coordinate_system = 'attached_to_image'
+    dose.dose_uncertainty.active = True
+    dose.dose_squared.active = True
+    dose.density.active = False
+    dose.translation = [0, 0, 0]
+    dose.hit_type = "random"
+    dose.dose.active = True
+    dose.edep.write_to_disk = False
+
+def add_patient_image(sim, name, path_image, img, cp_param,do_tle = False,tle_type="energy",tle_threshold= 1.6,RPL=False):
+    # OPEN IMAGE AND ASSOCIATION OF DENSITY TO EACH VOXELS
+    gcm3 = gate.g4_units.g / gate.g4_units.cm3
+    offset,dim,spacing = information_img_patient(path_image + img)
+    translation, rotation = versa.get_patient_translation_and_rotation_from_RT_plan_to_IEC(cp_param, path_image + img)
+    paths = utility.get_default_test_paths(__file__, "", output_folder="advanced_test_linac")
+
+    ### ADD PATIENT
+    patient = sim.add_volume("Image", "patient")
+    patient.image = path_image + img
+    patient.mother = name
+    patient.material = "G4_AIR"  # material used by default
+    f1 = paths.data / "Schneider2000MaterialsTable.txt"
+    f2 = paths.data / "Schneider2000DensitiesTable.txt"
+    tol = 0.05 * gcm3
+    patient.voxel_materials, materials = gate.geometry.materials.HounsfieldUnit_to_material(sim, tol, f1, f2)
+    patient.color = [1, 0, 1, 1]
+    patient.rotation =rotation
+    patient.translation = translation
+
+    # #ADD DOSE ACTOR
+    add_dose_actor_on_patient_image(sim,patient,dim,spacing,do_tle,tle_type,tle_threshold)
+    return patient
+
+
+
+def add_phase_space_actor(sim,mother_name,ssd):
+
+    m = gate.g4_units.m
+    cm = gate.g4_units.cm
+    nm = gate.g4_units.nm
+    volume_list = sim.volume_manager.volumes.keys()
+    mother = sim.volume_manager.get_volume(mother_name)
+    if not "phase_space_plan" in volume_list:
+        phsp_plan = sim.add_volume("Box", "phase_space_plan")
+        phsp_plan.size = [mother.size[0],mother.size[1], 1 * nm]
+        phsp_plan.mother = mother_name
+        mother_volume = sim.volume_manager.get_volume(mother_name)
+
+        z_pos =  100*cm -ssd - phsp_plan.size[2]/2
+        phsp_plan.translation = [0, 0, z_pos]
+    else:
+        phsp_plan = sim.volume_manager.get_volume("phase_space_plan")
+    phsp_plan.color = [0.7,0.3,0.3,0.8]
+
+
+    phsp = sim.add_actor("PhaseSpaceActor", "phase_space_actor")
+    phsp.attached_to = phsp_plan.name
+    phsp.attributes =  [
+        "KineticEnergy",
+        "Weight",
+        "PrePosition",
+        "PrePositionLocal",
+        "PreDirection",
+        "PreDirectionLocal",
+        "PDGCode",
+        "TrackID",
+    ]
+    return phsp_plan
+
+
+
+def init_simulation(dcm_RP,arc_id,path_img,img,mode=0,vis =False,tle_type=0,tle_threshold = 1.6,shielding=True,lead_thickness=4):
+    tle_types = ["energy","max range","average range"]
+    l_mode = ["Normal","TLE"]
+    mode = l_mode[mode]
+    tle_type = tle_types[tle_type]
+    sim = gate.Simulation()
+    ui = sim.user_info
+    ui.running_verbose_level = gate.logger.RUN
+    # main options
+    sim.g4_verbose = False
+
+    sim.visu = vis
+    sim.visu_type = "vrml"
+    sim.random_seed = "auto"
+    sim.check_volumes_overlap = True
+    cp_id="all_cp"
+
+
+    # unit
+    nm = gate.g4_units.nm
+    m = gate.g4_units.m
+    mm = gate.g4_units.mm
+    cm = gate.g4_units.cm
+    Bq = gate.g4_units.Bq
+
+    # world
+    world = sim.world
+    world.size = [3 * m, 3 * m, 3 * m]
+    world.material = "G4_AIR"
+
+    # linac
+    sad = 1000 * mm
+    linac = versa.add_linac(sim, "linac_box", sad)
+    linac.material = "G4_AIR"
+    linac.color = [0.78, 0.129, 0.92, 0.3]
+
+    small_linac_box = versa.add_patient_dependent_linac_box(sim, linac.name)
+    mlc_leaves = versa.add_mlc(sim,linac.name,small_linac_box.name)
+    mlc = sim.volume_manager.get_volume(f"{small_linac_box.name}_mlc")
+    jaws = versa.add_jaws(sim, linac.name,small_linac_box.name)
+
+    if shielding:
+        versa.add_linac_shielding(sim, linac.name,small_linac_box.name, type="all", lead_thickness=lead_thickness)
+
+
+    rt_plan_parameters = rtplan.read(dcm_RP, arc_id=arc_id)
+    cp_list = [None]
+    if cp_list[0] != None:
+        l_cp = cp_list
+        new_rt_plan_parameters = {}
+        for key in rt_plan_parameters.keys():
+            value = rt_plan_parameters[key][l_cp]
+            new_rt_plan_parameters[key] = value
+        rt_plan_parameters = new_rt_plan_parameters
+
+    else:
+        l_cp = np.arange(0, len(rt_plan_parameters["jaws 1"]), 1)
+
+    versa.set_time_intervals_from_rtplan(sim, rt_plan_parameters)
+
+    versa.set_linac_head_motion(sim, linac.name, jaws, mlc_leaves, rt_plan_parameters, sad=sad)
+
+    plan = sim.add_volume("Box", "plan")
+    plan.material = "G4_AIR"
+    plan.mother = linac.name
+    plan.size = [linac.size[0], linac.size[1], 0.1 * nm]
+    plan.translation = [0, 0, linac.size[2] / 2 - 30.01 * cm + 0.5 * nm]
+    source = versa.add_phase_space_source(sim, plan.name)
+    source.position_key = "PrePositionLocal"
+    source.direction_key = "PreDirectionLocal"
+    source.weight_key = "Weight"
+    source.PDGCode_key = "PDGCode"
+
+    if mode == "Normal":
+        add_patient_image(sim, world.name, path_img,img,rt_plan_parameters, do_tle=False)
+    if mode == "TLE" :
+        print("lol")
+        add_patient_image(sim, world.name, path_img,img,rt_plan_parameters, do_tle=True,tle_type = tle_type, tle_threshold=tle_threshold)
+
+    # physics
+    sim.physics_manager.physics_list_name = "G4EmStandardPhysics_option3"
+    sim.physics_manager.set_production_cut("world", "electron", 1000*m)
+    volume_list = sim.volume_manager.volumes.keys()
+    if "patient" in volume_list:
+        sim.physics_manager.set_production_cut("patient", "electron", 0.05* mm)
+    # add stat actor
+    s = f"/process/em/UseGeneralProcess false"
+    sim.g4_commands_before_init.append(s)
+    if mode == "TLE" :
+        s = f"/process/eLoss/CSDARange true"
+        sim.g4_commands_before_init.append(s)
+    s = sim.add_actor("SimulationStatisticsActor", "stats")
+    s.track_types_flag = True
+
+
+
+    # print results
+    return sim,rt_plan_parameters
+
+
---
+++ opengate/tests/src/advanced_tests/linacs/comparison_deposited_dose_MC_TPS.py
@@ -0,0 +1,269 @@
+import numpy as np
+import os,glob
+from scipy import ndimage
+from pathlib import Path
+import pydicom
+from opengate.contrib.linacs import dicomrtplan as rtplan
+from opengate.tests import utility
+import itk
+from box import Box
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import json
+import pymedphys
+
+
+class voxelised_dose():
+
+    def __init__(self,img,**kwargs):
+        x, y, z, arrayD, voxel_size, nb_voxel, offset_voxel = self.open_itk_dose_image(img)
+        img_dose = itk.imread(img)
+        self.x = x
+        self.y = y
+        self.z = z
+        self.dose = arrayD
+        self.information = Box({"spacing":voxel_size,"number_of_voxels":nb_voxel,"offset":offset_voxel,"errors" : False,"img":img_dose})
+        self.information["label"] = img.split("./")[-1]
+        for key in kwargs.keys():
+            if key == "label":
+                self.information["label"] = kwargs[key]
+            if key == "field":
+                self.information["field"] = kwargs[key]
+
+
+        l_err_img = [img[:-4] + "-uncertainty.mhd",img[:-4] + "_uncertainty.mhd"]
+        for err_img in l_err_img:
+            if os.path.exists(err_img):
+                x, y, z, err_arrayD, voxel_size, nb_voxel, offset_voxel = self.open_itk_dose_image(err_img)
+                self.err_dose_on_dose = err_arrayD
+                self.err_dose = err_arrayD * self.dose
+                self.information["errors"] = True
+                break
+
+
+
+    def __str__(self):
+        return self.information.label
+
+    def open_itk_dose_image(self,img):
+        img_dose = itk.imread(img)
+        voxel_size = np.array(img_dose.GetSpacing())
+        nb_voxel = np.array(img_dose.GetLargestPossibleRegion().GetSize())
+        offset_voxel = np.array(img_dose.GetOrigin())
+
+        arrayD = itk.GetArrayFromImage(img_dose)
+        numpy_column_order = [2, 1, 0]
+        arrayD = np.transpose(arrayD, numpy_column_order)
+        arrayD = arrayD[:, :, ::-1]
+
+        x = np.arange(0, nb_voxel[0] * voxel_size[0], voxel_size[0])
+        x = x + offset_voxel[0]
+
+        y = np.arange(0, nb_voxel[1] * voxel_size[1], voxel_size[1])
+        y = y + offset_voxel[1]
+
+        z = np.arange(0, nb_voxel[2] * voxel_size[2], voxel_size[2])
+        z = z + offset_voxel[2]
+
+        return (x,y,z,arrayD,voxel_size,nb_voxel,offset_voxel)
+
+
+
+
+def vec_equal(a, b, tol=0.01) -> bool:
+    return all(abs(x - y) <= tol for x, y in zip(a, b))
+
+
+def get_single_isocenter(rtplan_path: Path, tol: float = 0.01) -> np.ndarray:
+    ds = pydicom.dcmread(str(rtplan_path), force=True)
+
+    if not hasattr(ds, "BeamSequence"):
+        raise ValueError("RT Plan sans BeamSequence — impossible de lire l'isocentre.")
+
+    iso_ref = None
+    for i, beam in enumerate(ds.BeamSequence):
+        if not hasattr(beam, "ControlPointSequence"):
+            raise ValueError(f"Le faisceau #{i+1} n'a pas de ControlPointSequence.")
+
+        iso = None
+        # lecture du CP0 en priorité
+        cp0 = beam.ControlPointSequence[0]
+        if hasattr(cp0, "IsocenterPosition"):
+            iso = np.array(cp0.IsocenterPosition, dtype=float)
+        else:
+            for cp in beam.ControlPointSequence:
+                if hasattr(cp, "IsocenterPosition"):
+                    iso = np.array(cp.IsocenterPosition, dtype=float)
+                    break
+
+        if iso is None:
+            raise ValueError(f"Aucun IsocenterPosition trouvé pour le faisceau #{i+1}.")
+
+        if iso_ref is None:
+            iso_ref = iso
+        elif not vec_equal(iso_ref, iso, tol):
+            raise ValueError(
+                f"Plus d'un isocentre détecté (écart > {tol} mm).\n"
+                f"Référence: {iso_ref} vs Faisceau #{i+1}: {iso}"
+            )
+
+    if iso_ref is None:
+        raise ValueError("Aucun isocentre trouvé dans le RT Plan.")
+
+    return iso_ref  # numpy.ndarray [x, y, z] en mm
+
+
+
+def estimate_nb_of_event(dicom_file,median_nb_part):
+    rt_plan_parameters = rtplan.read(dicom_file, "all_cp")
+    cp_weight= rt_plan_parameters["weight"]
+    cp_weight = cp_weight/np.median(cp_weight)
+    total_nb_event = np.sum(cp_weight)*median_nb_part
+    return total_nb_event
+
+plt.rc('font', family = 'sans-serif', serif = 'DejaVu Sans')
+plt.rcParams.update({'font.size' : 20})
+plt.rc('text',usetex = True)
+plt.rcParams['text.latex.preamble'] = r'\boldmath\usepackage{amsmath}\usepackage{bm}'
+
+
+############# simulation parameters to modify ##############
+stats_ratio_according_to_PC_to_cluster_speed_ratio = 1
+speed_ratio = 15
+sampling = 4
+
+path =  f"./data"
+path_output = f"./output"
+
+os.chdir(path)
+folder_list = []
+folder_list.append(path)
+
+# ############# path and file name to modify ##############
+#
+for pname in folder_list:
+
+    tle = True
+    f = open(f"./header.json")
+    json = json.load(f)
+    CT_name = "CT.mhd"
+    TPS_name = "TPS.mhd"
+    RTplan_name = "rt_plan.dcm"
+    CT_mask_name = "mask.mhd"
+
+    if tle :
+
+        MC_dose_name = "MC-tle-norm-dose.mhd"
+    else :
+        MC_dose_name = "MC-tle-norm-dose.mhd"
+
+    path_output = f"../output"
+    if tle :
+        folder_list = sorted(glob.glob(f"{path_output}/tle-stats_file*",recursive=True))
+    else :
+        folder_list = sorted(glob.glob(f"{path_output}/tle-stats_file*", recursive=True))
+    print(folder_list)
+
+
+
+    #
+    isocenter = get_single_isocenter(f"./{RTplan_name}",0.01)
+    print(isocenter)
+    if tle :
+        img_dose_name_list = sorted(glob.glob("../output/*tle-norm-dose*.mhd",recursive=True))
+    else :
+        img_dose_name_list = sorted(glob.glob("../output/*norm-dose*.mhd", recursive=True))
+    print(img_dose_name_list)
+    CT_mask_path = f"./{CT_mask_name}"
+    CT_mask = voxelised_dose(CT_mask_path)
+    structuring_element = np.ones((3, 3, 3))
+    for i in range(1):
+        CT_mask.dose = ndimage.binary_erosion(CT_mask.dose, structure=structuring_element)
+
+    CT_path = f"./{CT_name}"
+    CT = voxelised_dose(CT_path)
+    CT.dose = CT.dose * CT_mask.dose
+    CT.dose[CT.dose == 0] -= 1024
+
+    TPS_path = f"./{TPS_name}"
+    TPS = voxelised_dose(TPS_path)
+    TPS.dose = TPS.dose * CT_mask.dose
+    spacing = TPS.information.spacing
+    volume = spacing[0] * spacing[1]*spacing[2]
+
+    MC_dose_path = f"../output/{MC_dose_name}"
+    MC_dose = voxelised_dose(MC_dose_path)
+    MC_dose.dose = MC_dose.dose * CT_mask.dose
+
+    MC_dose.z = MC_dose.z[::-1]
+    x = MC_dose.x - isocenter[0]
+    y = MC_dose.y - isocenter[1]
+    z = -(MC_dose.z - isocenter[2])
+    idx = 40
+
+
+    x_ref = TPS.x
+    y_ref = TPS.y
+    z_ref = TPS.z
+
+    x_eval = MC_dose.x
+    y_eval = MC_dose.y
+    z_eval = MC_dose.z
+
+    axes_reference = (MC_dose.x, MC_dose.y, MC_dose.z)
+    axes_evaluation = (MC_dose.x, MC_dose.y, MC_dose.z)
+
+    # print(axes_evaluation,axes_reference)
+    dose_reference = TPS.dose
+    dose_evaluation = MC_dose.dose
+
+    gamma_options = {
+        'dose_percent_threshold': 2,
+        'distance_mm_threshold': 2,
+        'lower_percent_dose_cutoff': 50,
+        'interp_fraction': 10,  # Should be 10 or more for more accurate results
+        'max_gamma': 1.5,
+        'random_subset': None,
+        'local_gamma': True,
+        'ram_available': 40 * 2 ** 29  # 40 * 1/2 GB
+    }
+
+    gamma = pymedphys.gamma(
+        axes_reference, dose_reference,
+        axes_evaluation, dose_evaluation,
+        **gamma_options)
+    valid_gamma = gamma[~np.isnan(gamma)]
+    pass_ratio = np.sum(valid_gamma <= 1) / len(valid_gamma)
+    print(pass_ratio)
+
+    height = 20
+    width= height/2
+    fig1,ax1 = plt.subplots(2,1)
+    fig1.set_size_inches(width, height)
+    fig1.subplots_adjust(left=0,
+                                 bottom=0.086,
+                                 right=0.99,
+                                 top=0.986,
+                                 wspace=0.2,
+                                 hspace=0.193)
+    norm = mcolors.LogNorm(vmin=0.03, vmax=70)
+
+    cax = ax1[0].imshow(np.rot90(CT.dose[:,idx,:]), extent=[x.min(), x.max(),z.max(), z.min()], origin='lower', cmap='gist_gray',aspect='equal')
+    cax2 = ax1[0].imshow(np.rot90(MC_dose.dose[:,idx,:]), extent=[x.min(), x.max(),z.max(), z.min()], origin='lower', cmap='jet',norm= norm, aspect='equal',alpha = 0.6)
+    cbar = fig1.colorbar(cax2,ax = ax1[0])
+    cbar.set_label(label = r'\textbf{Dose[\%]}',fontsize=16)
+
+
+#
+    cax = ax1[1].imshow(np.rot90(CT.dose[:,idx,:]), extent=[x.min(), x.max(),z.max(), z.min()], origin='lower', cmap='gist_gray',aspect='equal')
+    cax2 = ax1[1].imshow(np.rot90(TPS.dose[:,idx,:]), extent=[x.min(), x.max(),z.max(), z.min()], origin='lower', cmap='jet',norm= norm, aspect='equal',alpha = 0.6)
+    cbar = fig1.colorbar(cax2,ax = ax1[1], pad=0.04)
+    cbar.set_label(label = r'\textbf{Dose[\%]}',fontsize=16)
+
+
+    ax1[0].set(ylabel = r'\textbf{Y position [mm]}')
+    ax1[1].set(ylabel=r'\textbf{Y position [mm]}',xlabel=r'\textbf{X position [mm]}' )
+    ax1[0].axes.get_xaxis().set_visible(False)
+    fig1.savefig("../output/MC_vs_TPS_dose.pdf")
+    is_ok = pass_ratio > 0.6
+    utility.test_ok(is_ok)
