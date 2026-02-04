80973d59397067bc4b1176e29e5bc3dfd2792907
clemencegentner <clemence.gentner@gmail.com>
2026-02-04 15:28:38 +0100

    binded G4MagneticField

---
+++ core/opengate_core/g4_bindings/pyG4MagneticField.cpp
@@ -0,0 +1,25 @@
+/* --------------------------------------------------
+   Copyright (C): OpenGATE Collaboration
+   This software is distributed under the terms
+   of the GNU Lesser General  Public Licence (LGPL)
+   See LICENSE.md for further details
+   -------------------------------------------------- */
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+#include "G4MagneticField.hh"
+
+void init_G4MagneticField(py::module &m) {
+  py::class_<G4MagneticField, std::unique_ptr<G4MagneticField, py::nodelete>>(
+      m, "G4MagneticField")
+
+    .def(py::init<>())
+    .def(py::init<G4MagneticField&*>())
+
+    .def("DoesFieldChangeEnergy", &DoesFieldChangeEnergy);
+    .def("GetFieldValue", &GetFieldValue);
+    
+
+}
