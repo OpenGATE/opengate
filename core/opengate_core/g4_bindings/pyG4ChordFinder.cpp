80e5eb29a4f89728a74b445a9292f889bcf7e857
Marc Ballestero Ribó <marconiballestero@gmail.com>
2026-02-04 15:04:00 +0100

    Expose more G4 classes

---
+++ core/opengate_core/g4_bindings/pyG4ChordFinder.cpp
@@ -0,0 +1,21 @@
+/* --------------------------------------------------
+   Copyright (C): OpenGATE Collaboration
+   This software is distributed under the terms
+   of the GNU Lesser General  Public Licence (LGPL)
+   See LICENSE.md for further details
+   -------------------------------------------------- */
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+#include "G4ChordFinder.hh"
+
+void init_G4ChordFinder(py::module &m) {
+  py::class_<G4ChordFinder, std::unique_ptr<G4ChordFinder, py::nodelete>>(
+      m, "G4ChordFinder")
+    .def(py::init<G4VIntegrationDriver *>())
+    .def(py::init<G4MagneticField*, G4double, G4IntegrationStepper*, G4int>())
+
+    // TODO: add the rest of needed methods here
+}
