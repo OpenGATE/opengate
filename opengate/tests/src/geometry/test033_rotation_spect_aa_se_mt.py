f9e5c1c6b8f7beb9335a6121efb93191939e3590
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2026-02-10 10:25:47 +0100

    back to initial tolerance (something is wrong)

--- opengate/tests/src/geometry/test033_rotation_spect_aa_se_mt.py
+++ opengate/tests/src/geometry/test033_rotation_spect_aa_se_mt.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
     sim.run()
 
     # check
-    is_ok = test033.evaluate_test(sim, sources, 28, 5913808)
+    is_ok = test033.evaluate_test(sim, sources, 10, 5913808)
 
     utility.test_ok(is_ok)
