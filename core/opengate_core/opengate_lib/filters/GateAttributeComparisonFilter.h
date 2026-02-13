d1970ed56667950dd494c49767b3946f4be36622
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2026-02-13 18:30:06 +0100

    Add `GateAttributeComparisonFilter`: new templated filter for attribute-based comparisons

---
+++ core/opengate_core/opengate_lib/filters/GateAttributeComparisonFilter.h
@@ -0,0 +1,43 @@
+/* --------------------------------------------------
+   Copyright (C): OpenGATE Collaboration
+   This software is distributed under the terms
+   of the GNU Lesser General  Public Licence (LGPL)
+   See LICENSE.md for further details
+   -------------------------------------------------- */
+
+#ifndef GateAttributeComparisonFilter_h
+#define GateAttributeComparisonFilter_h
+
+#include "../GateHelpersDict.h"
+#include "../digitizer/GateTDigiAttribute.h"
+#include "G4Step.hh"
+#include "GateVFilter.h"
+
+template <typename T> class GateAttributeComparisonFilter : public GateVFilter {
+public:
+  GateAttributeComparisonFilter() : GateVFilter() {
+    DDD("GateAttributeComparisonFilter constructor");
+  }
+
+  void InitializeUserInfo(py::dict &user_info) override;
+
+  // Implementation for G4Step
+  bool Accept(const G4Track *track) const override;
+  bool Accept(G4Step *step) const override;
+
+  std::string fAttributeName;
+  T fValueMin;
+  T fValueMax;
+  bool fIncludeMin = true;
+  bool fIncludeMax = true;
+  std::string fSearchMode{"equal"};
+  GateTDigiAttribute<T> *fAttribute{nullptr};
+};
+
+// Typedefs for common use cases in Gate 10
+using GateAttributeFilterDouble = GateAttributeComparisonFilter<double>;
+using GateAttributeFilterInt = GateAttributeComparisonFilter<int>;
+
+#include "GateAttributeComparisonFilter.txx"
+
+#endif // GateAttributeComparisonFilter_h
