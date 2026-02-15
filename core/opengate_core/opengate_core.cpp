306e67c2aba961148acf01ae54c967f0a03e71f8
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2026-02-15 15:12:38 +0100

    Refactor: Remove unused filter initializations (`GateKineticEnergyFilter`, `GateParticleFilter`, `GateThresholdAttributeFilter`) and associated bindings for a cleaner codebase

--- core/opengate_core/opengate_core.cpp
+++ core/opengate_core/opengate_core.cpp
@@ -297,15 +297,9 @@ void init_GateActorManager(py::module &);
 // Gate filters
 void init_GateVFilter(py::module &);
 
-void init_GateParticleFilter(py::module &);
-
 void init_GatePrimaryScatterFilter(py::module &);
 
-void init_GateThresholdAttributeFilter(py::module &);
-
 void init_GateTrackCreatorProcessFilter(py::module &);
-
-void init_GateKineticEnergyFilter(py::module &);
 
 void init_GateBooleanFilter(py::module &);
 
@@ -582,11 +576,8 @@ PYBIND11_MODULE(opengate_core, m) {
   init_GateWeightedEdepActor(m);
   init_GateActorManager(m);
   init_GateVFilter(m);
-  init_GateParticleFilter(m);
   init_GatePrimaryScatterFilter(m);
   init_GateTrackCreatorProcessFilter(m);
-  init_GateKineticEnergyFilter(m);
-  init_GateThresholdAttributeFilter(m);
   init_GateBooleanFilter(m);
   init_GateAttributeComparisonFilter(m);
   init_itk_image(m);
