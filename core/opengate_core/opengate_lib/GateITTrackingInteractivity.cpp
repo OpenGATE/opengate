d736b2cc610efb3f82d67d619f8eeb2548aad962
Nils Krah <krah.nils@gmail.com>
2026-04-13 00:49:08 +0200

    Expose missing chemistry tracking interactivity hooks

--- core/opengate_core/opengate_lib/GateITTrackingInteractivity.cpp
+++ core/opengate_core/opengate_lib/GateITTrackingInteractivity.cpp
@@ -11,11 +11,32 @@ GateITTrackingInteractivity::GateITTrackingInteractivity()
     : G4ITTrackingInteractivity() {}
 
 void GateITTrackingInteractivity::RegisterActor(GateVChemistryActor *actor) {
+  if (actor->HasAction("InitializeChemistryTracking")) {
+    fInitializeTrackingActors.push_back(actor);
+  }
+  if (actor->HasAction("AppendChemistryStep")) {
+    fAppendStepActors.push_back(actor);
+  }
   if (actor->HasAction("StartChemistryTracking")) {
     fStartTrackingActors.push_back(actor);
   }
   if (actor->HasAction("EndChemistryTracking")) {
     fEndTrackingActors.push_back(actor);
+  }
+  if (actor->HasAction("FinalizeChemistryTracking")) {
+    fFinalizeTrackingActors.push_back(actor);
+  }
+}
+
+void GateITTrackingInteractivity::Initialize() {
+  for (auto actor : fInitializeTrackingActors) {
+    actor->InitializeChemistryTracking();
+  }
+}
+
+void GateITTrackingInteractivity::AppendStep(G4Track *track, G4Step *step) {
+  for (auto actor : fAppendStepActors) {
+    actor->AppendChemistryStep(track, step);
   }
 }
 
@@ -28,5 +49,11 @@ void GateITTrackingInteractivity::StartTracking(G4Track *track) {
 void GateITTrackingInteractivity::EndTracking(G4Track *track) {
   for (auto actor : fEndTrackingActors) {
     actor->EndChemistryTracking(track);
+  }
+}
+
+void GateITTrackingInteractivity::Finalize() {
+  for (auto actor : fFinalizeTrackingActors) {
+    actor->FinalizeChemistryTracking();
   }
 }
--- core/opengate_core/opengate_lib/pyGateVChemistryActor.cpp
+++ core/opengate_core/opengate_lib/pyGateVChemistryActor.cpp
@@ -15,12 +15,25 @@ class PyGateVChemistryActor : public GateVChemistryActor {
 public:
   using GateVChemistryActor::GateVChemistryActor;
 
+  void InitializeChemistryTracking() override {
+    PYBIND11_OVERLOAD(void, GateVChemistryActor, InitializeChemistryTracking);
+  }
+
+  void AppendChemistryStep(G4Track *track, G4Step *step) override {
+    PYBIND11_OVERLOAD(void, GateVChemistryActor, AppendChemistryStep, track,
+                      step);
+  }
+
   void StartChemistryTracking(G4Track *track) override {
     PYBIND11_OVERLOAD(void, GateVChemistryActor, StartChemistryTracking, track);
   }
 
   void EndChemistryTracking(G4Track *track) override {
     PYBIND11_OVERLOAD(void, GateVChemistryActor, EndChemistryTracking, track);
+  }
+
+  void FinalizeChemistryTracking() override {
+    PYBIND11_OVERLOAD(void, GateVChemistryActor, FinalizeChemistryTracking);
   }
 };
 
@@ -29,9 +42,14 @@ void init_GateVChemistryActor(py::module &m) {
              std::unique_ptr<GateVChemistryActor, py::nodelete>, GateVActor>(
       m, "GateVChemistryActor")
       .def(py::init<py::dict &>())
+      .def("InitializeChemistryTracking",
+           &GateVChemistryActor::InitializeChemistryTracking)
+      .def("AppendChemistryStep", &GateVChemistryActor::AppendChemistryStep)
       .def("StartChemistryTracking",
            &GateVChemistryActor::StartChemistryTracking)
       .def("EndChemistryTracking", &GateVChemistryActor::EndChemistryTracking)
+      .def("FinalizeChemistryTracking",
+           &GateVChemistryActor::FinalizeChemistryTracking)
       .def("StartProcessing", &GateVChemistryActor::StartProcessing)
       .def("UserPreTimeStepAction", &GateVChemistryActor::UserPreTimeStepAction)
       .def("UserPostTimeStepAction",
