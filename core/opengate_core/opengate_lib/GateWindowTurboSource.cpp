07694e81abfe58a8def2b67d871273df7bda94a7
LiKun <likun@dotuai.com>
2026-05-13 12:02:52 +0800

    in the middle

--- core/opengate_core/opengate_lib/GateWindowTurboSource.cpp
+++ core/opengate_core/opengate_lib/GateWindowTurboSource.cpp
@@ -1,3 +1,5 @@
+// TODO: 需要自行清除
+// ang->fGlobalRotation，如果复用GateGenericSource::PrepareNextRun()的话
 #include "GateWindowTurboSource.h"
 // #include "GateWindowTurboSourceMessenger.hh"
 #include "GateRandomEngine.hh"
@@ -46,150 +48,86 @@ GateWindowTurboSource::GateWindowTurboSource(G4String name)
     : GateVSource(name) {
   m_sourceMessenger = new GateWindowTurboSourceMessenger(this);
 }
-G4bool GateWindowTurboSource::CheckPosDirValid(const G4ThreeVector &pos,
-                                               const G4ThreeVector &dir) const {
-  // compare theta with cos2, to avoid complex calculation
-  //   G4double cot2theta = std::copysign(1.0, dir.z()) * dir.z() * dir.z() /
-  //                        (dir.x() * dir.x() + dir.y() * dir.y());
 
-  G4double x0 = pos.x() * cos_plane_phi + pos.y() * sin_plane_phi;
-  G4double y0 = -pos.x() * sin_plane_phi + pos.y() * cos_plane_phi;
-  G4double a1_rel = a1 - y0;
-  G4double a2_rel = a2 - y0;
-  G4double b1_rel = b1 - pos.z();
-  G4double b2_rel = b2 - pos.z();
-  G4double d_rel = plane_distance - x0;
-  G4double dir_x_rotated = dir.x() * cos_plane_phi + dir.y() * sin_plane_phi;
-  G4double dir_y_rotated = -dir.x() * sin_plane_phi + dir.y() * cos_plane_phi;
+// void verify_one(const G4ThreeVector start, const G4ThreeVector end,
+//                 G4double theta_max, G4double theta_min, G4double phi_max,
+//                 G4double phi_min) {
+//   G4ThreeVector dir = end - start;
+//   G4double theta = M_PI - acos(dir.z() / dir.mag());
+//   G4double epsilon = 1e-10;
+//   G4double phi = atan2(dir.y(), dir.x()) + M_PI;
+//   G4bool theta_valid =
+//       theta <= theta_max + epsilon && theta >= theta_min - epsilon;
+//   G4bool phi_valid = phi <= phi_max + epsilon && phi >= phi_min - epsilon;
+//   G4bool phi_valid_p2pi = phi + 2 * M_PI <= phi_max + epsilon &&
+//                           phi + 2 * M_PI >= phi_min - epsilon;
+//   if (!theta_valid || (!phi_valid && !phi_valid_p2pi)) {
+//     G4cerr << (theta > theta_max + epsilon) << (theta < theta_min - epsilon)
+//            << (phi > phi_max + epsilon) << (phi < phi_min - epsilon) <<
+//            G4endl;
+//     G4cerr << "pos at " << start << " end at " << end << " theta " << theta
+//            << " phi " << phi << G4endl;
+//     fprintf(stderr, "theta %0.53lf\n phi %0.53lf\n", theta, phi);
+//     G4cerr << "pos up at" << start + G4ThreeVector(0, 0, 100) << " pos down
+//     at "
+//            << start + G4ThreeVector(0, 0, -100) << G4endl;
+//     G4cerr << "max theta radius is " << 100 * tan(theta_max)
+//            << " min theta radius is " << 100 * tan(theta_min) << G4endl;
+//     fprintf(stderr, "max theta+epsilon %0.53lf\n min theta-epsilon
+//     %0.53lf\n",
+//             theta_max + epsilon, theta_min - epsilon);
+//     fprintf(stderr, "max phi+epsilon %0.53lf\n min phi-epsilon %0.53lf\n",
+//             phi_max + epsilon, phi_min - epsilon);
+//     // G4cerr << "theta_max " << theta_max << " theta_min " << theta_min << "
+//     // phi_max " << phi_max << " phi_min " << phi_min << G4endl;
+//     fflush(stderr);
+//     G4Exception("GateWindowTurboSource::VerifyPhiTheta",
+//     "VerifyPhiThetaError",
+//                 FatalException, "phi or theta not in range");
+//   }
+//   return;
+// }
 
-  G4double intersect_b = d_rel / dir_x_rotated * dir.z() + pos.z();
-  G4double intersect_a = d_rel / dir_x_rotated * dir_y_rotated + y0;
-  return intersect_a <= a2 && intersect_a >= a1 && intersect_b <= b2 &&
-         intersect_b >= b1;
-}
-
-void verify_one(const G4ThreeVector start, const G4ThreeVector end,
-                G4double theta_max, G4double theta_min, G4double phi_max,
-                G4double phi_min) {
-  G4ThreeVector dir = end - start;
-  G4double theta = M_PI - acos(dir.z() / dir.mag());
-  G4double epsilon = 1e-10;
-  G4double phi = atan2(dir.y(), dir.x()) + M_PI;
-  G4bool theta_valid =
-      theta <= theta_max + epsilon && theta >= theta_min - epsilon;
-  G4bool phi_valid = phi <= phi_max + epsilon && phi >= phi_min - epsilon;
-  G4bool phi_valid_p2pi = phi + 2 * M_PI <= phi_max + epsilon &&
-                          phi + 2 * M_PI >= phi_min - epsilon;
-  if (!theta_valid || (!phi_valid && !phi_valid_p2pi)) {
-    G4cerr << (theta > theta_max + epsilon) << (theta < theta_min - epsilon)
-           << (phi > phi_max + epsilon) << (phi < phi_min - epsilon) << G4endl;
-    G4cerr << "pos at " << start << " end at " << end << " theta " << theta
-           << " phi " << phi << G4endl;
-    fprintf(stderr, "theta %0.53lf\n phi %0.53lf\n", theta, phi);
-    G4cerr << "pos up at" << start + G4ThreeVector(0, 0, 100) << " pos down at "
-           << start + G4ThreeVector(0, 0, -100) << G4endl;
-    G4cerr << "max theta radius is " << 100 * tan(theta_max)
-           << " min theta radius is " << 100 * tan(theta_min) << G4endl;
-    fprintf(stderr, "max theta+epsilon %0.53lf\n min theta-epsilon %0.53lf\n",
-            theta_max + epsilon, theta_min - epsilon);
-    fprintf(stderr, "max phi+epsilon %0.53lf\n min phi-epsilon %0.53lf\n",
-            phi_max + epsilon, phi_min - epsilon);
-    // G4cerr << "theta_max " << theta_max << " theta_min " << theta_min << "
-    // phi_max " << phi_max << " phi_min " << phi_min << G4endl;
-    fflush(stderr);
-    G4Exception("GateWindowTurboSource::VerifyPhiTheta", "VerifyPhiThetaError",
-                FatalException, "phi or theta not in range");
-  }
-  return;
-}
-
-void GateWindowTurboSource::VerifyPhiTheta(G4int number_pos,
-                                           G4double interval) const {
-  G4ThreeVector pos;
-  G4ThreeVector window_vt1, window_vt2, window_vt3, window_vt4;
-  GetWindowVertex(window_vt1, window_vt2, window_vt3, window_vt4);
-  G4ThreeVector delta12 = window_vt2 - window_vt1;
-  G4ThreeVector norm12 = delta12.unit();
-  G4double num_interval_12 = delta12.mag() / interval;
-  G4ThreeVector delta13 = window_vt3 - window_vt1;
-  G4ThreeVector norm13 = delta13.unit();
-  G4double num_interval_13 = delta13.mag() / interval;
-  G4cout << "plane distancce" << plane_distance << " plane_phi " << plane_phi
-         << G4endl;
-  G4cout << "a1 " << a1 << " a2 " << a2 << " b1 " << b1 << " b2 " << b2
-         << G4endl;
-  G4cout << "window vertexs: " << window_vt1 << " " << window_vt2 << " "
-         << window_vt3 << " " << window_vt4 << G4endl;
-  for (G4int i = 0; i < number_pos; i++) {
-    pos = m_posSPS->GenerateOne();
-    SetPhiTheta(pos);
-    G4double theta_max = m_angSPS->GetMaxTheta();
-    G4double theta_min = m_angSPS->GetMinTheta();
-    G4double phi_max = m_angSPS->GetMaxPhi();
-    G4double phi_min = m_angSPS->GetMinPhi();
-    for (G4int j = 0; j < num_interval_12; j++) {
-      G4ThreeVector end = window_vt1 + norm12 * interval * j;
-      verify_one(pos, end, theta_max, theta_min, phi_max, phi_min);
-      end = window_vt3 + norm12 * interval * j;
-      verify_one(pos, end, theta_max, theta_min, phi_max, phi_min);
-    }
-    for (G4int j = 0; j < num_interval_13; j++) {
-      G4ThreeVector end = window_vt1 + norm13 * interval * j;
-      verify_one(pos, end, theta_max, theta_min, phi_max, phi_min);
-      end = window_vt2 + norm13 * interval * j;
-      verify_one(pos, end, theta_max, theta_min, phi_max, phi_min);
-    }
-  }
-  G4cout << "VerifyPhiTheta passed for " << number_pos << " positions"
-         << G4endl;
-}
-
-void GateWindowTurboSource::SetPhiTheta(const G4ThreeVector &pos) const {
-  // compare theta with cos2, to avoid complex calculation
-  //   G4double cot2theta = std::copysign(1.0, dir.z()) * dir.z() * dir.z() /
-  //                        (dir.x() * dir.x() + dir.y() * dir.y());
-  // relationship between angular vector and theta and phi in Geant4
-  // px = -sintheta * cosphi;
-  // py = -sintheta * sinphi;
-  // pz = -costheta;
-
-  G4double x0 = pos.x() * cos_plane_phi + pos.y() * sin_plane_phi;
-  G4double y0 = -pos.x() * sin_plane_phi + pos.y() * cos_plane_phi;
-  G4double a1_rel = a1 - y0;
-  G4double a2_rel = a2 - y0;
-  G4double b1_rel = b1 - pos.z();
-  G4double b2_rel = b2 - pos.z();
-  G4double d_rel = plane_distance - x0;
-
-  G4double aamax = std::max(a1_rel * a1_rel, a2_rel * a2_rel);
-  G4double aamin = std::min(a1_rel * a1_rel, a2_rel * a2_rel);
-  G4double thetamax, thetamin;
-
-  if (a1_rel < 0 and a2_rel > 0 and b2_rel > 0)
-    thetamax = M_PI - atan2(d_rel, b2_rel);
-  else
-    thetamax = M_PI - atan2(sqrt((b2_rel > 0 ? aamin : aamax) + d_rel * d_rel),
-                            b2_rel);
-
-  if (a1_rel < 0 and a2_rel > 0 and b1_rel < 0)
-    // in this case, need to check minmum/maxmum of the hyperbola
-    thetamin = M_PI - atan2(d_rel, b1_rel);
-  else
-    thetamin = M_PI - atan2(sqrt((b1_rel > 0 ? aamax : aamin) + d_rel * d_rel),
-                            b1_rel);
-
-  m_angSPS->SetMinTheta(thetamin);
-  m_angSPS->SetMaxTheta(thetamax);
-  G4double phimin = atan2(a1_rel, d_rel) + plane_phi;
-  G4double phimax = atan2(a2_rel, d_rel) + plane_phi;
-
-  m_angSPS->SetMinPhi(phimin + M_PI);
-  m_angSPS->SetMaxPhi(phimax + M_PI);
-}
-
-G4double solid_angle_pyramid(G4double a, G4double b, G4double d) {
-  return 4 * atan(a * b / (2 * d * sqrt(a * a + b * b + 4 * d * d)));
-}
+// void GateWindowTurboSource::VerifyPhiTheta(G4int number_pos,
+//                                            G4double interval) const {
+//   G4ThreeVector pos;
+//   G4ThreeVector window_vt1, window_vt2, window_vt3, window_vt4;
+//   GetWindowVertex(window_vt1, window_vt2, window_vt3, window_vt4);
+//   G4ThreeVector delta12 = window_vt2 - window_vt1;
+//   G4ThreeVector norm12 = delta12.unit();
+//   G4double num_interval_12 = delta12.mag() / interval;
+//   G4ThreeVector delta13 = window_vt3 - window_vt1;
+//   G4ThreeVector norm13 = delta13.unit();
+//   G4double num_interval_13 = delta13.mag() / interval;
+//   G4cout << "plane distancce" << plane_distance << " plane_phi " << plane_phi
+//          << G4endl;
+//   G4cout << "a1 " << a1 << " a2 " << a2 << " b1 " << b1 << " b2 " << b2
+//          << G4endl;
+//   G4cout << "window vertexs: " << window_vt1 << " " << window_vt2 << " "
+//          << window_vt3 << " " << window_vt4 << G4endl;
+//   for (G4int i = 0; i < number_pos; i++) {
+//     pos = m_posSPS->GenerateOne();
+//     SetPhiTheta(pos);
+//     G4double theta_max = m_angSPS->GetMaxTheta();
+//     G4double theta_min = m_angSPS->GetMinTheta();
+//     G4double phi_max = m_angSPS->GetMaxPhi();
+//     G4double phi_min = m_angSPS->GetMinPhi();
+//     for (G4int j = 0; j < num_interval_12; j++) {
+//       G4ThreeVector end = window_vt1 + norm12 * interval * j;
+//       verify_one(pos, end, theta_max, theta_min, phi_max, phi_min);
+//       end = window_vt3 + norm12 * interval * j;
+//       verify_one(pos, end, theta_max, theta_min, phi_max, phi_min);
+//     }
+//     for (G4int j = 0; j < num_interval_13; j++) {
+//       G4ThreeVector end = window_vt1 + norm13 * interval * j;
+//       verify_one(pos, end, theta_max, theta_min, phi_max, phi_min);
+//       end = window_vt2 + norm13 * interval * j;
+//       verify_one(pos, end, theta_max, theta_min, phi_max, phi_min);
+//     }
+//   }
+//   G4cout << "VerifyPhiTheta passed for " << number_pos << " positions"
+//          << G4endl;
+// }
 
 void GateWindowTurboSource::CheckMotherVolumeIsNotRotated() const {
   // TODO: reimplement
--- core/opengate_core/opengate_lib/GateWindowTurboSource.h
+++ core/opengate_core/opengate_lib/GateWindowTurboSource.h
@@ -1,5 +1,6 @@
 #pragma once
-#include "GateVSource.h"
+#include "GateGenericSource.h"
+#include "GateSingleParticleSource.h"
 #include <G4Point3D.hh>
 #include <G4String.hh>
 #include <G4ThreeVector.hh>
@@ -19,29 +20,17 @@ azimuthal angles of source point seeing pphi1 and pphi2.
 
 */
 
-class GateWindowTurboSource : public GateVSource {
+class GateWindowTurboSource : public GateGenericSource {
 public:
   GateWindowTurboSource(G4String name);
-  ~GateWindowTurboSource() {
-    G4cout << "GateWindowTurboSource destructs" << G4endl;
-  };
+  ~GateWindowTurboSource() = default;
 
   void GeneratePrimaries(G4Event *event,
                          double current_simulation_time) override;
 
   virtual double PrepareNextTime(double current_simulation_time,
-                                 double NumberOfGeneratedEvents);
+                                 double NumberOfGeneratedEvents) override;
 
-  // should be reimplemented
-  G4double GetNextTime(G4double timeStart) override;
-  void SetActRatio(G4double actRatio) {
-    act_ratio = actRatio;
-    act_ratio_set = true;
-  };
-  void SetMaxSolidAngle(G4double maxSolidAngle) {
-    max_solid_angle = maxSolidAngle;
-    max_solid_angle_set = true;
-  };
   void Initialize(G4int samplingCount);
   void LoadVoxelizedPhantom(G4String filename);
   void SetPhantomPosition(G4ThreeVector pos);
@@ -49,18 +38,5 @@ public:
                        G4ThreeVector &pos3, G4ThreeVector &pos4) const;
 
 private:
-  // static G4bool random_engine_initialized;
-  void SetPhiTheta(const G4ThreeVector &pos) const;
-  G4bool CheckPosDirValid(const G4ThreeVector &pos,
-                          const G4ThreeVector &dir) const;
   void CheckMotherVolumeIsNotRotated() const;
-  G4double act_ratio = 1;
-  G4double max_solid_angle = 0;
-  G4bool act_ratio_set;
-  G4bool max_solid_angle_set;
-  void VerifyPhiTheta(G4int number_pos, G4double interval) const;
-  // G4ThreeVector mPth1;
-  // G4ThreeVector mPth2;
-  // G4ThreeVector mPphi1;
-  // G4ThreeVector mPphi2;
 };
