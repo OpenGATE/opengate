16ff959d1f802d160d26107210bc0b790491ac99
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2026-06-11 16:44:45 +0200

    refactor lastvertexsource

--- opengate/sources/lastvertexsources.py
+++ opengate/sources/lastvertexsources.py
@@ -11,21 +11,22 @@ from ..utility import g4_units
 from ..exception import fatal, warning
 
 
-class LastVertexSource(SourceBase, g4.GateLastVertexSource):
+class LastVertexSource(SourceBase):
     """
     The source used to replay position, energy, direction and weight of last vertex particles actor
     """
 
     def __init__(self, *args, **kwargs):
         SourceBase.__init__(self, *args, **kwargs)
-        self.__initcpp__()
 
-    def __initcpp__(self):
-        g4.GateLastVertexSource.__init__(self)
+    def create_g4_source(self):
+        return g4.GateLastVertexSource()
 
-    def initialize(self, run_timing_intervals):
+    def initialize_g4_source(self, g4_source, run_timing_intervals):
         self.user_info.n = np.zeros(len(run_timing_intervals)) + 1
-        SourceBase.initialize(self, run_timing_intervals)
+        self.initialize_start_end_time(run_timing_intervals)
+        self.check_ui_activity(self.user_info)
+        g4_source.InitializeUserInfo(self.user_info)
 
 
 process_cls(LastVertexSource)
