df0f7a2ed2dae3b6d18bc0677adf784e9d9ef03e
Nils Krah <krah.nils@gmail.com>
2026-06-15 11:46:02 +0200

    Add process_cls() statement for chemistry classes

--- opengate/managers.py
+++ opengate/managers.py
@@ -2281,6 +2281,7 @@ def create_sim_from_json(path):
 
 
 process_cls(PhysicsManager)
+process_cls(ChemistryManager)
 process_cls(VolumeManager)
 process_cls(ActorManager)
 process_cls(PostProcessingManager)
