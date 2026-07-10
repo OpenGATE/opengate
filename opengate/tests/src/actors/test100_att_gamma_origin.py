9722c26522584afd574de4bdc5518cee24a2a902
anetxe <ane.etxebeste@creatis.insa-lyon.fr>
2026-07-10 11:19:31 +0200

    Test added for gammaAncestor

--- opengate/tests/src/actors/test100_att_gamma_origin.py
+++ opengate/tests/src/actors/test100_att_gamma_origin.py
@@ -1,15 +1,112 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import os
-import subprocess
-import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import uproot
 import opengate as gate
-from opengate.contrib.root_helpers import *
 from opengate.tests import utility
+
+
+def test100_test(df, output_dir=None):
+    overall_valid = True
+
+    allowedE = {0.510999, 1.274497} #MeV,
+    #validation rules  for position of gamma ancestors : annihilation or isomeric transition
+    validation_rules = {
+        0.510999: {"mean_max": 1.0, "max_max": 8.0},
+        1.274497: {"mean_max": 0.0, "max_max": 0.0}
+    }
+    tolE = 0.001
+    energies = df["GammaVertexKineticEnergy"]
+    origin_x = df["GammaPosition_X"]
+    origin_y = df["GammaPosition_Y"]
+    origin_z = df["GammaPosition_Z"]
+    n_entries = len(energies)
+
+    # Masks for each energy
+    masks = {E: energies.apply(lambda x: abs(x - E) < tolE) for E in allowedE}
+
+    # Compute Euclidean distances for each energy group
+    distances = {
+        E: np.sqrt(origin_x[masks[E]] ** 2 + origin_y[masks[E]] ** 2 + origin_z[masks[E]] ** 2)
+        for E in allowedE
+    }
+
+    # ----------------------------------------------------
+    # Test for Ratio  of intensities nb(E0)/nb(E1)
+    # ----------------------------------------------------
+    tolf = 0.05
+    fraction = 1.81 #expected intensity ratio for the annhilations photons wrp to 1.274 keV  photon
+    E0, E1 = allowedE
+    n_E0 = masks[E0].sum()
+    n_E1 = masks[E1].sum()
+    if n_E0 == 0 or n_E1 == 0:
+        overall_valid = False
+    else:
+        ratio_E0_E1 = n_E0 / n_E1
+        print(f"Ratio nb(E0)/nb(E1) = {ratio_E0_E1:.6f}")
+        if ratio_E0_E1 < fraction-tolf or ratio_E0_E1 > fraction:
+            print(f"[FAIL] Ratio nb(E0)/nb(E1) = {ratio_E0_E1:.6f} (allowed between 1.76 and 1.81)")
+            overall_valid = False
+
+    # ----------------------------------------------------
+    # Test for ancestor position
+    # ---------------------------------------------------
+    for E in allowedE:
+        d = distances[E]
+        mean_d = d.mean()
+        max_d = d.max()
+
+        # -----------------------------
+        # Plotting
+        # -----------------------------
+        plt.figure(figsize=(8, 5))
+        plt.hist(d, bins=60, alpha=0.7, color='steelblue')
+        plt.title(f"Euclidean distance distribution for E = {E} MeV")
+        plt.xlabel("Distance (mm)")
+        plt.ylabel("Counts")
+
+        plt.axvline(mean_d, color='red', linestyle='--', linewidth=2,
+                    label=f"Mean = {mean_d:.2f}")
+        plt.axvline(max_d, color='green', linestyle='--', linewidth=2,
+                    label=f"Max = {max_d:.2f}")
+
+        plt.legend()
+        plt.tight_layout()
+
+        if output_dir is not None:
+            fname = os.path.join(output_dir, f"dist_E_{E:.6f}.png")
+            plt.savefig(fname, dpi=200)
+
+        plt.close()
+
+        # -----------------------------
+        # Validation rules depending on the  photon, annihilation or isomeric trqnsition
+        # -----------------------------
+        rules = validation_rules[E]
+        if mean_d > rules["mean_max"] or max_d > rules["max_max"]:
+            print(f"[FAIL] E={E}: mean={mean_d:.3f} (allowed ≤ {rules['mean_max']}), "
+                  f"max={max_d:.3f} (allowed ≤ {rules['max_max']})")
+            overall_valid = False
+
+    # ----------------------------------------------------
+    #  test for fraction of 'invalid' events (not allowed energies), aroudn 0.3%  due to positron annihilation in the fly ?
+    #-----------------------------------------------------------------------------------------
+    valid = energies.apply(lambda x: any(abs(x - e) < tolE for e in allowedE))
+    mask_invalid = ~energies.isna() & ~valid
+    inv_energies = energies[mask_invalid]
+    inv_particle = df.loc[mask_invalid, "ParticleName"]
+    inv_proces = df.loc[mask_invalid, "TrackCreatorProcess"]
+    n_invalid = len(inv_energies)
+    print("percentage of invalid entries (%): ", n_invalid/n_entries*100)
+    if n_invalid/n_entries> 0.01:
+        print("Invalid energies found:")
+        for e, p, proc in zip(inv_energies, inv_particle, inv_proces):
+            print(f"Energy: {e}, Particle: {p}, Process: {proc}")
+        overall_valid = False
+
+    return overall_valid
 
 
 def main():
@@ -19,9 +116,8 @@ def main():
     )
 
     """
-    This test check the new attribute XXXX.
-
-    This attribute store the position/energy of the first gamma that is parent of the current particle (including   itself).
+    This test check the new attribute GammaAncestorAttribute
+    This attribute store the position/energy of the first gamma that is parent of the current particle (including  itself).
     """
     print(paths)
 
@@ -35,32 +131,38 @@ def main():
     mm = gate.g4_units.mm
     cm = gate.g4_units.cm
     Bq = gate.g4_units.Bq
+    sec = gate.g4_units.s
 
     # world size
     world = sim.world
-    world.size = [1 * m, 1 * m, 1 * m]
+    world.size = [0.5 * m,0.5 * m, 0.5 * m]
 
-    # waterbox
-    waterbox = sim.add_volume("Box", "waterbox")
-    waterbox.size = [10 * cm, 10 * cm, 10 * cm]
-    waterbox.translation = [0, 0, 0]
-    waterbox.material = "G4_WATER"
-    waterbox.color = [0, 0, 1, 1]
+    # water encapsulation for the annihilation of the positron
+    encapsulation = sim.add_volume("Sphere", "encapsulation")
+    encapsulation.rmin = 0 * cm
+    encapsulation.rmax = 1 * cm
+    encapsulation.translation = [0, 0, 0]
+    encapsulation.material = "G4_WATER"
+    encapsulation.color = [0, 0, 1, 1]
 
     # sphere around
     sphere = sim.add_volume("Sphere", "sphere")
-    sphere.rmin = 15 * cm
-    sphere.rmax = 16 * cm
+    sphere.rmin = 10 * cm
+    sphere.rmax = 11 * cm
     sphere.material = "G4_AIR"
 
     # source
-    source = sim.add_source("GenericSource", "source")
-    source.particle = "ion 89 225"
+    source = sim.add_source("GenericSource", "Nas22_source")
+    source.attached_to = encapsulation  # for the annihilation of the positrorn
+    source.particle = "ion 11 22" #Na22
+    source.energy.type= "mono"
     source.energy.mono = 0
-    source.position.type = "sphere"
-    source.position.radius = 5 * mm
+    source.position.type = "point"
     source.direction.type = "iso"
     source.activity = 5000 * Bq
+    source.half_life = 8.205*1e07 * sec  #I need to give this to have results
+
+
 
     # new attribute
     att1 = sim.activate_auxiliary_attribute("GammaAncestorAttribute", "GammaPosition")
@@ -73,7 +175,7 @@ def main():
     # phase space
     phsp = sim.add_actor("PhaseSpaceActor", "phase_space")
     phsp.attached_to = sphere
-    phsp.output_filename = "phase_space.root"
+    phsp.output_filename = "test100_phase_space.root"
     phsp.attributes = [
         "EventID",
         "TrackID",
@@ -81,22 +183,35 @@ def main():
         "ParticleName",
         "PreKineticEnergy",
         "PrePosition",
+        "TrackCreatorProcess",
         att1.name,
         att2.name,
     ]
+    sim.physics_manager.physics_list_name = "G4EmStandardPhysics_option3"
+    sim.physics_manager.set_production_cut("world", "all", 1 * mm)
 
+    sim.run_timing_intervals = [[0, 2.5 * sec]]
     # stats
-    stat = sim.add_actor("SimulationStatisticsActor", "stat")
-    stat.track_types_flag = True
+    #stat = sim.add_actor("SimulationStatisticsActor", "stat")
+    #stat.track_types_flag = True
 
     # physics with decay
     sim.physics_manager.enable_decay = True
 
     # run
     sim.run()
+    #print(stat)
 
-    # todo
-    is_ok = True
+    # test
+    print(phsp.output_filename)
+    print(sim.output_dir)
+    phsp_out = uproot.open(
+        str(paths.output) +"/"+str(phsp.output_filename) + ":phase_space"
+    )
+
+    df = phsp_out.arrays(library="pd")
+    is_ok = test100_test(df, output_dir=paths.output)
+
     utility.test_ok(is_ok)
 
 
